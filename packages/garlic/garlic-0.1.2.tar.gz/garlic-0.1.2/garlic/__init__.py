28014459c59b1558c21cda0e61e9a86fde69b928
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 03:07:57 +0000

    Add package garlic/0.1.2/garlic-0.1.2.tar.gz

---
+++ packages/garlic/garlic-0.1.2.tar.gz/garlic-0.1.2/garlic/__init__.py
@@ -0,0 +1,12 @@
+
+from garlic.base_event import BaseEvent
+from garlic.event_dispatcher import EventDispatcher
+from garlic.event_handler import EventHandler
+from garlic.subscriber import Subscriber
+
+__all__ = [
+    "BaseEvent",
+    "EventDispatcher",
+    "EventHandler",
+    "Subscriber",
+]
