5ac299abd27d27e67a0608e46d44fde8c0c657f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 15:08:20 +0000

    Add package cdm/0.1.10/cdm-0.1.10.tar.gz

---
+++ packages/cdm/cdm-0.1.10.tar.gz/cdm-0.1.10/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+readme = "Charzeh Download Manager"
+
+setup(
+    name='cdm',
+    version='0.1.10',
+    description='Charzeh Download Manager',
+    long_description=readme,
+    author='AmirMohammad Dehghan',
+    author_email='dehghan@mit.edu',
+    url='https://github.com/amirmd76/cdm.git',
+    entry_points={
+        'console_scripts': ['cdm = cdm.cdm:main']
+    },
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[]
+)
