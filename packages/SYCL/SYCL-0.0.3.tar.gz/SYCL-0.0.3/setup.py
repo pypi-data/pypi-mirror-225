fffae997441f6df785af5f72d02079fcfca5ea4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 03:09:00 +0000

    Add package SYCL/0.0.3/SYCL-0.0.3.tar.gz

---
+++ packages/SYCL/SYCL-0.0.3.tar.gz/SYCL-0.0.3/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+setuptools .setup(
+    name="SYCL",#库的名字
+    version="0.0.3",#库的版本号，后续更新的时候只需要改版本号就行了
+    author="SYC",
+    author_email="1831402487@qq.com", #你的邮箱
+    description="hhhhh", #介绍
+    Long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
