9f7185324d5f4bf5f28b07bb2b6e2acab3cf81ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 14:08:18 +0000

    Add package strangeworks/0.4.6/strangeworks-0.4.6-py3-none-any.whl

---
+++ packages/strangeworks/strangeworks-0.4.6-py3-none-any.whl/strangeworks/core/config/env.py
@@ -0,0 +1,43 @@
+"""env.py."""
+import os
+from typing import Optional
+
+from strangeworks.core.config.base import ConfigSource
+from strangeworks.core.utils import is_empty_str
+
+
+class EnvConfig(ConfigSource):
+    """Obtain configuration from environment variables.
+
+    Environment variables are expected to be in the following format:
+        STRANGEWORKS_CONFIG_{profile}_{key}
+    where `profile` and `key` are non-empty strings in uppercase
+    letters.
+    """
+
+    def get(self, key: str, profile: str = "default") -> Optional[str]:
+        """Retrieve the values from environment variables.
+
+        The method will convert all lowercase key or profile values to uppercase.
+        """
+        if is_empty_str(key) or is_empty_str(profile):
+            return None
+
+        env_var = EnvConfig._get_envvar_name(key=key, profile=profile)
+        return os.getenv(env_var)
+
+    def set(self, profile: str = "default", overwrite: bool = False, **params):
+        """Set method for environment variables.
+
+        Existing environment variables will be updated only if overwrite is set
+        to True.
+        """
+        for key, val in params.items():
+            envvar_name = EnvConfig._get_envvar_name(key=key, profile=profile)
+            # overwrite existing envvars iff overwrite is set to True.
+            if envvar_name in os.environ and overwrite:
+                os.environ[envvar_name] = val
+
+    @staticmethod
+    def _get_envvar_name(key: str, profile: str = "default"):
+        return f"STRANGEWORKS_CONFIG_{profile.upper()}_{key.upper()}"
