dc8a90c0f8d4a5a341571146d3f9d2994aca8a88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 14:07:52 +0000

    Add package aesthetic-text/0.1.6/aesthetic_text-0.1.6-py3-none-any.whl

---
+++ packages/aesthetic-text/aesthetic_text-0.1.6-py3-none-any.whl/aesthetic_text/aesthetic_text.py
@@ -0,0 +1,26 @@
+# ------- Style text -------
+class style:
+    bold = "\033[1m"
+    dim = "\033[2m"
+    italic = "\033[3m"
+    underline = "\033[4m"
+    highlight = "\033[7m"
+    hidden = "\033[8m"
+    strikethrough = "\033[9m"
+    
+# ------- Color text -------
+class color:
+    white = "\033[97m"
+    grey = "\033[90m"
+    black = "\033[30m"
+    purple = "\033[95m"
+    blue = "\033[94m"
+    cyan = "\033[96m"
+    green = "\033[92m"
+    yellow = "\033[93m"
+    red = "\033[91m"
+    pink = "\033[38;5;206m"
+    orange = "\033[38;5;208m"
+    brown = "\033[38;5;130m"
+
+reset = "\033[0m"
