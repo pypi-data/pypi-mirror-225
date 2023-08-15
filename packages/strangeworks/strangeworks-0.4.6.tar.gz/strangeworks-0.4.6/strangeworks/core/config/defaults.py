4389edbb94617c739537dbab5cea47d8e87acc58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 14:08:18 +0000

    Add package strangeworks/0.4.6/strangeworks-0.4.6.tar.gz

---
+++ packages/strangeworks/strangeworks-0.4.6.tar.gz/strangeworks-0.4.6/strangeworks/core/config/defaults.py
@@ -0,0 +1,27 @@
+"""defaults.py."""
+from typing import Optional
+
+from strangeworks.core.config.base import ConfigSource
+
+DEFAULT_URL = "https://api.strangeworks.com"
+
+
+class DefaultConfig(ConfigSource):
+    """Configuration Source for default values."""
+
+    cfg = {"url": DEFAULT_URL}
+
+    def get(self, key: str, profile: str = "default") -> Optional[str]:
+        """Strangeworks SDK Default Configurations
+
+        Serves as a source of default config values.  Any request for a profile other
+        than default will return None.
+        """
+        if profile != "default" or key not in DefaultConfig.cfg:
+            return None
+
+        return DefaultConfig.cfg[key]
+
+    def set(self, profile: str = "default", overwrite: bool = False, **params):
+        """Set (no-op) for default profile."""
+        return
