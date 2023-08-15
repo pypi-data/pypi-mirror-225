736ee135a9f8780f4ea958f0739865c59005b1fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 15:07:33 +0000

    Add package junoplatform/0.0.37/junoplatform-0.0.37.tar.gz

---
+++ packages/junoplatform/junoplatform-0.0.37.tar.gz/junoplatform-0.0.37/src/junoplatform/api/local.py
@@ -0,0 +1,18 @@
+import requests
+import logging
+
+def write(data:list[dict]):
+    """
+    data: list[dict], example:
+          [{'PLC_TAG1': 2.2}, {'PLC_TAG2': 1.2}]
+    """
+    try:
+      api='http://jp-connector/api/write'
+      r = requests.post(api, json=data, timeout=5)
+      if r.status_code != 200:
+         logging.error(r.text)
+         return r.text
+      return ""
+    except Exception as e:
+      logging.error(str(e))
+      return str(e)
