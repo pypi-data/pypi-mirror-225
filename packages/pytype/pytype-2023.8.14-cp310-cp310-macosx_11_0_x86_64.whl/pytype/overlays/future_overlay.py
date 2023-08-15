fd11235d0a6862084a387df4c39c539da712dda7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 15:07:25 +0000

    Add package pytype/2023.8.14/pytype-2023.8.14-cp310-cp310-macosx_11_0_x86_64.whl

---
+++ packages/pytype/pytype-2023.8.14-cp310-cp310-macosx_11_0_x86_64.whl/pytype/overlays/future_overlay.py
@@ -0,0 +1,19 @@
+"""Implementation of special members of the future library."""
+
+from pytype.overlays import metaclass
+from pytype.overlays import overlay
+
+
+class FutureUtilsOverlay(overlay.Overlay):
+  """A custom overlay for the 'future' module."""
+
+  def __init__(self, ctx):
+    member_map = {
+        "with_metaclass": build_with_metaclass,
+    }
+    ast = ctx.loader.import_name("future.utils")
+    super().__init__(ctx, "future.utils", member_map, ast)
+
+
+def build_with_metaclass(ctx):
+  return metaclass.WithMetaclass.make("with_metaclass", ctx, "future.utils")
