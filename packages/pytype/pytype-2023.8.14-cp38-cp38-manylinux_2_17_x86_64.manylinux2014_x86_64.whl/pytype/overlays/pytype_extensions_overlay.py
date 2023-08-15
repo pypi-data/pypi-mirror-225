647ecb7b0f5d9c7a77d1478af807f89da73b10ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 15:07:26 +0000

    Add package pytype/2023.8.14/pytype-2023.8.14-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pytype/pytype-2023.8.14-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pytype/overlays/pytype_extensions_overlay.py
@@ -0,0 +1,20 @@
+"""Implementation of special members of pytype_extensions."""
+
+from pytype.overlays import overlay
+from pytype.overlays import special_builtins
+
+
+class PytypeExtensionsOverlay(overlay.Overlay):
+  """A custom overlay for the 'pytype_extensions' module."""
+
+  def __init__(self, ctx):
+    member_map = {
+        "assert_type": build_assert_type
+    }
+    ast = ctx.loader.import_name("pytype_extensions")
+    super().__init__(ctx, "pytype_extensions", member_map, ast)
+
+
+def build_assert_type(ctx):
+  return special_builtins.AssertType.make_alias("assert_type", ctx,
+                                                "pytype_extensions")
