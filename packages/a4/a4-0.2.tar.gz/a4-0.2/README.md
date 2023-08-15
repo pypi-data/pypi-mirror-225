727de88de9eaa6e908a9eb8d839d05e9df1ac3a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 04:11:38 +0000

    Add package a4/0.2/a4-0.2.tar.gz

---
+++ packages/a4/a4-0.2.tar.gz/a4-0.2/README.md
@@ -0,0 +1,8 @@
+# a4
+Source code for the A4 plugin.
+
+# Download
+Download it at [PyPI](https://pypi.org/project/a4/) or type this command in your terminal:
+```
+pip install a4
+```
---
+++ packages/a4/a4-0.2.tar.gz/a4-0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='a4',
+    version='0.2',
+    description='A Python package for terminal utilities and screenshot capturing.',
+    author='BL1Z33',
+    packages=['a4'],
+    install_requires=['Pillow'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
