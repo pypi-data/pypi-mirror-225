7623235ff5b9e9db129b55ec4f2f69cfb185721f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 03:09:08 +0000

    Add package close-bullet/0.2/close_bullet-0.2.tar.gz

---
+++ packages/close-bullet/close_bullet-0.2.tar.gz/close_bullet-0.2/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+setuptools.setup(
+    name='close_bullet',
+    version='0.2',
+    author='NoBody_cp',
+    description='Privet Library For 01 Team',
+    packages=setuptools.find_packages(),
+)
