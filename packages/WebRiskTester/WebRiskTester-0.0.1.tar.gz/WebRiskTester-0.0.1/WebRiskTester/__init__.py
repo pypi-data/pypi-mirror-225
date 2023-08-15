c61e7f11720ff6124863781e97a1a03df871da29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 15:07:35 +0000

    Add package WebRiskTester/0.0.1/WebRiskTester-0.0.1.tar.gz

---
+++ packages/WebRiskTester/WebRiskTester-0.0.1.tar.gz/WebRiskTester-0.0.1/WebRiskTester/__init__.py
@@ -0,0 +1,4 @@
+from .eip import PLC
+a = 'http://testsafebrowsing.appspot.com/apiv4/ANY_PLATFORM/MALWARE/URL/'
+__version_info__ = (0, 8, 6)
+__version__ = '.'.join(str(x) for x in __version_info__)
---
+++ packages/WebRiskTester/WebRiskTester-0.0.1.tar.gz/WebRiskTester-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='WebRiskTester',
+    version='0.0.1',
+    description='Package to test WebRisk API',
+    author='T',
+    packages=find_packages(),
+)
+
