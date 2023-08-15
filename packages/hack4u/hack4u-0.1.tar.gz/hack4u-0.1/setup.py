5719b668dceb3ac0767088ec3b15daca30e1c9f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 14:08:28 +0000

    Add package hack4u/0.1/hack4u-0.1.tar.gz

---
+++ packages/hack4u/hack4u-0.1.tar.gz/hack4u-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hack4u",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Marcelo Vázquez",
+    description="Una biblioteca para consultar cursos de hack4u.",
+    url="https://hack4u.io",
+)
+
