0713c80905ab90666e0dac6416db0647f67a8963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 15:07:25 +0000

    Add package dxlib/0.3.9/dxlib-0.3.9-py3-none-any.whl

---
+++ packages/dxlib/dxlib-0.3.9-py3-none-any.whl/dxlib/strategies/custom_strategies/__init__.py
@@ -0,0 +1,5 @@
+from .rsi_strategy import RsiStrategy
+from .pair_trading import PairTradingStrategy
+from .systematic_trading import SystematicRandomForest
+from .trend_follow import TrendFollowStrategy
+from .volatility_breakout import VolatilityBreakoutStrategy
