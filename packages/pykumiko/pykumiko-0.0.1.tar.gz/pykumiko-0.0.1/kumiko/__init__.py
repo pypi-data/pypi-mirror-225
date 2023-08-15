3a118650595fbbae6ce6b4e9fde7f6eb5c760259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 04:11:21 +0000

    Add package pykumiko/0.0.1/pykumiko-0.0.1.tar.gz

---
+++ packages/pykumiko/pykumiko-0.0.1.tar.gz/pykumiko-0.0.1/kumiko/__init__.py
@@ -0,0 +1,8 @@
+desc = """
+This is a reserved package name for Kumiko, the multipurpose bot for freedom and control. 
+This package does not do anything. 
+
+For more info, please visit here: https://github.com/No767/Kumiko
+"""
+
+print(desc)
