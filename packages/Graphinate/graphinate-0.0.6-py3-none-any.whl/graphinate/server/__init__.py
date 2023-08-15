1c245665e3200db2153584ccf36c4a81f76674f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 14:07:17 +0000

    Add package Graphinate/0.0.6/graphinate-0.0.6-py3-none-any.whl

---
+++ packages/Graphinate/graphinate-0.0.6-py3-none-any.whl/graphinate/server/__init__.py
@@ -0,0 +1,24 @@
+import webbrowser
+
+import strawberry
+
+from starlette.applications import Starlette
+from strawberry.asgi import GraphQL
+
+from .starlette import routes
+
+
+def run_graphql(graphql_schema: strawberry.Schema, port: int = 8072):
+    def open_url():
+        for app_name in ('voyager', 'graphiql', 'viewer'):
+            webbrowser.open(f'http://localhost:{port}/{app_name}')
+
+    graphql_app = GraphQL(graphql_schema)
+    app = Starlette(
+        routes=routes(),
+        on_startup=[open_url])
+    app.add_route("/graphql", graphql_app)
+    app.add_websocket_route("/graphql", graphql_app)
+
+    import uvicorn
+    uvicorn.run(app, host='0.0.0.0', port=port)
