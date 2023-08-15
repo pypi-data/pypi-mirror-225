ac137ead5514556429fbe945a7f095318e092562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 03:09:46 +0000

    Add package close-bullet/0.5/close_bullet-0.5.tar.gz

---
+++ packages/close-bullet/close_bullet-0.5.tar.gz/close_bullet-0.5/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+setuptools.setup(
+    name='close_bullet',
+    version='0.5',
+    author='NoBody_cp',
+    description='Privet Library For 01 Team',
+    packages=['close_bullet', 'close_bullet.backend', 'close_bullet.Z1', 'venv/Lib/site-packages/onest_captcha'],
+)
