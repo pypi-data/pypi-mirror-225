ec32180af177072c8b3bca293df106f737a57903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 14:08:48 +0000

    Add package openbb-nightly/3.2.0.dev20230815/openbb_nightly-3.2.0.dev20230815-py3-none-any.whl

---
+++ packages/openbb-nightly/openbb_nightly-3.2.0.dev20230815-py3-none-any.whl/openbb_terminal/core/session/constants.py
@@ -0,0 +1,52 @@
+BASE_URL = "https://payments.openbb.co/"
+# BASE_URL = "http://127.0.0.1:8000/"
+
+HUB_URL = "https://my.openbb.co/"
+REGISTER_URL = HUB_URL + "register"
+SUPPORT_URL = HUB_URL + "app/terminal/support"
+SOURCES_URL = HUB_URL + "app/terminal/data-sources"
+KEYS_URL = HUB_URL + "app/terminal/api-keys"
+COLORS_URL = HUB_URL + "app/terminal/theme"
+CHARTS_TABLES_URL = HUB_URL + "app/terminal/theme/charts-tables"
+
+DEFAULT_ROUTINES_URL = "https://openbb-cms.directus.app/items/Routines"
+
+TIMEOUT = 30
+CONNECTION_ERROR_MSG = "[red]Connection error.[/red]"
+CONNECTION_TIMEOUT_MSG = "[red]Connection timeout.[/red]"
+
+SCRIPT_TAGS = [
+    "stocks",
+    "crypto",
+    "etf",
+    "economy",
+    "forex",
+    "fixed income",
+    "alternative",
+    "funds",
+    "bonds",
+    "macro",
+    "mutual funds",
+    "equities",
+    "options",
+    "dark pool",
+    "shorts",
+    "insider",
+    "behavioral analysis",
+    "fundamental analysis",
+    "technical analysis",
+    "quantitative analysis",
+    "forecasting",
+    "government",
+    "comparison",
+    "nft",
+    "on chain",
+    "off chain",
+    "screener",
+    "report",
+    "overview",
+    "rates",
+    "econometrics",
+    "portfolio",
+    "real estate",
+]
