c82918ee78b5d387f04edb874f80fd8055072ef4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 15:06:48 +0000

    Add package deepevals/0.2.0/deepevals-0.2.0.tar.gz

---
+++ packages/deepevals/deepevals-0.2.0.tar.gz/deepevals-0.2.0/tests/test_clients.py
@@ -0,0 +1,36 @@
+"""Test clients
+"""
+import unittest
+from evals import Evaluator
+from evals.pipeline import Pipeline
+from evals.metrics.metric import RandomMetric
+
+
+class TestEvaluator(unittest.TestCase):
+    def test_eval(self):
+        eval = Evaluator()
+        result = eval.add_ground_truth(
+            query="Example",
+            expected_response="Customer success response is here",
+            tags=["sample"],
+        )
+        assert True
+
+
+def result_function(query):
+    return "Custiomers are great!"
+
+
+class TestE2E(unittest.TestCase):
+    def test_e2e(self):
+        eval = Evaluator()
+        pipeline = Pipeline(
+            pipeline_id="textract-pdf-processing", result_function=result_function
+        )
+        metric = RandomMetric()
+        result = eval.evaluate(pipeline=pipeline, metric=metric)
+        assert True
+
+
+if __name__ == "__main__":
+    unittest.main()
