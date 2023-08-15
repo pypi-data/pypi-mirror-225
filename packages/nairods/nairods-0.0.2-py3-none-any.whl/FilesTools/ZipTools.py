0caca328ff8a3aeea7e3120908025111a775b68d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 03:08:30 +0000

    Add package nairods/0.0.2/nairods-0.0.2-py3-none-any.whl

---
+++ packages/nairods/nairods-0.0.2-py3-none-any.whl/FilesTools/ZipTools.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+# -*-coding:utf-8-*-
+import zipfile
+from FileTools.FolderTools import FolderCurd
+
+
+# 压缩文件的增删改查等
+class ZipCurd(object):
+    """
+    arc文件操作
+    """
+
+    def __init__(self, zip_path=""):
+        self.zip_path = zip_path
+
+    def unzip_file(self, zip_file, save_folder):
+        """
+        解压文件到指定文件夹
+        :param zip_file:  需要解压的文件
+        :param save_folder:    保存的本地文件夹
+        :return: 保存成功
+        """
+        FC = FolderCurd()
+        FC.creat_folder(save_folder)
+        with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+            zip_ref.extractall(save_folder)
+        return "解压成功"
+
+
+
+# if __name__ == '__main__':
+#     ZC = ZipCurd()
+
