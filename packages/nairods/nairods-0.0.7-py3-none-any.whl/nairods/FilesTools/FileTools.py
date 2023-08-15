3af7883b931f2bfdf736bbed3a833e7489b73463
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 03:07:03 +0000

    Add package nairods/0.0.7/nairods-0.0.7-py3-none-any.whl

---
+++ packages/nairods/nairods-0.0.7-py3-none-any.whl/nairods/FilesTools/FileTools.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# -*-coding:utf-8-*-
+import os
+
+
+# 文件夹的增删改查
+class FolderCurd(object):
+    """
+    文件夹操作
+    """
+
+    def __init__(self):
+        pass
+
+    def creat_folder(self, file_path=""):
+        """
+        文件不存在则创建
+        :param file_path: 文件地址
+        :return: 创建成功
+        """
+        pass
+
+
+
+
+
+
+# if __name__ == '__main__':
+#     local_folder_path = F'..\\Temp\\HistoryFile\\2023\\'  # 本地保存文件路径,使用os.sep
+#     FC =FolderCurd()
+#     FC.walk_folder_path(local_folder_path)
