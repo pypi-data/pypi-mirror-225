f758a0780282361dbd6f491b268122e4db486fe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 03:08:48 +0000

    Add package Tenhou-Paifu-Logger/0.3.2/Tenhou-Paifu-Logger-0.3.2.tar.gz

---
+++ packages/Tenhou-Paifu-Logger/Tenhou-Paifu-Logger-0.3.2.tar.gz/Tenhou-Paifu-Logger-0.3.2/paifulogger/src/get_paifu.py
@@ -0,0 +1,18 @@
+import xml.etree.ElementTree as ET
+import os
+from .url_request_handler import url_request_handler
+from .Paifu import Paifu
+from .local import local_str
+
+
+def get_paifu(url: str, local_str: local_str):
+    response = url_request_handler(url)
+    root = ET.fromstring(response)
+    paifu = Paifu(url, root)
+    if not os.path.isdir(f'./{local_str.paifu}/{paifu.go_str}/'):
+        os.makedirs(f'./{local_str.paifu}/{paifu.go_str}/')
+
+    url = url.split('=')[1]+'='+url.split('=')[2]
+    with open(f'./{local_str.paifu}/{paifu.go_str}/'+url+'.xml', 'w') as t:
+        t.write(response)
+    return paifu
