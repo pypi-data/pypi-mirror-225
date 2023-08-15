1ee517b3777cd111c41c9d0ad6b85998b4aa4cb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 14:08:20 +0000

    Add package django-email-accounts/0.4.2/django-email-accounts-0.4.2.tar.gz

---
+++ packages/django-email-accounts/django-email-accounts-0.4.2.tar.gz/django-email-accounts-0.4.2/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+extras_require = {
+    "rest_framework": ["djangorestframework>=3.12"],
+    "filters": ["django-filter>=2.4"],
+}
+
+setup(
+    name='django-email-accounts',
+    version='0.4.2',
+    author='Fathi Abdelmalek',
+    author_email='abdelmalek.fathi.2001@gmail.com',
+    description='A Django app for user management with email-based authentication.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/fathiabdelmalek/django-email-accounts',
+    packages=find_packages(),
+    package_data={
+        'email_accounts': ['templates/**/*', 'static/**/*'],
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Framework :: Django",
+        "Framework :: Django :: 3.2",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        'Django>=3.2.0',
+    ],
+    extras_require=extras_require,
+)
