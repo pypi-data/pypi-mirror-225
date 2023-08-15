0b1072db825f97f07c2110cbb53977c0cfba3e78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 15:07:22 +0000

    Add package pydantic-client/0.1.12/pydantic_client-0.1.12.tar.gz

---
+++ packages/pydantic-client/pydantic_client-0.1.12.tar.gz/pydantic_client-0.1.12/pydantic_client/__init__.py
@@ -0,0 +1,12 @@
+from .decorators import delete, get, patch, post, put, rest
+from .group import Group
+
+__all__ = [
+    "patch",
+    "rest",
+    "get",
+    "post",
+    "put",
+    "delete",
+    "Group",
+]
