0d7d1bc8933d55aa0e8feab716daf82c73a76aa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 03:07:21 +0000

    Add package pyoomysql/0.2.10/pyoomysql-0.2.10.tar.gz

---
+++ packages/pyoomysql/pyoomysql-0.2.10.tar.gz/pyoomysql-0.2.10/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyoomysql",
+    version="0.2.10",
+    author="Jesus Alejandro Sanchez Davila",
+    author_email="jsanchez.consultant@gmail.com",
+    description="Python Object-Orinted MySQL interface",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/jsanchezd/pyoomysql",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'mysql-connector-python'
+    ]
+)
