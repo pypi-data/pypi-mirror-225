4947cd3f5c058eaa578eb412c49dd3e1ca41f808
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 15:07:08 +0000

    Add package vcorelib/2.5.0/vcorelib-2.5.0.tar.gz

---
+++ packages/vcorelib/vcorelib-2.5.0.tar.gz/vcorelib-2.5.0/vcorelib/__init__.py
@@ -0,0 +1,17 @@
+# =====================================
+# generator=datazen
+# version=3.1.2
+# hash=5a962998947a8e1e4971b7cce911f5e3
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "A collection of core Python utilities."
+PKG_NAME = "vcorelib"
+VERSION = "2.5.0"
+
+# vcorelib-specific content.
+DEFAULT_INDENT = 2
+DEFAULT_ENCODING = "utf-8"
