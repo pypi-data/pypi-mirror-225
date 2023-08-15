a78c96608b26221a096822fd2a3ed7efa6a7ca50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 03:07:11 +0000

    Add package blaster-server/0.0.442/blaster-server-0.0.442.tar.gz

---
+++ packages/blaster-server/blaster-server-0.0.442.tar.gz/blaster-server-0.0.442/blaster/tools/sanitize_html.py
@@ -0,0 +1,165 @@
+import html
+from functools import partial
+
+# custom containers ##########
+# SanitizedList and SanitizedDict are used for HTML safe operation
+# the idea is to wrap them to sanitizeContainers, and escapt them while retrieving
+# rather than during inserting/parsing stage
+def _sanitize(val, escape_html=True, escape_quotes=False):
+	if(isinstance(val, str)):
+		if(escape_html):
+			return html.escape(val, quote=escape_quotes)
+	elif(isinstance(val, list)):
+		return SanitizedList(val)
+	elif(isinstance(val, dict)):
+		return SanitizedDict(val)
+	return val
+
+
+class SanitizedObject:
+	entries = None
+
+	def __getitem__(self, key):
+		return _sanitize(self.entries[key])
+
+	def __setitem__(self, key, val):
+		self.entries[key] = val
+
+	def __delitem__(self, key):
+		del self.entries[key]
+
+	def __getattr__(self, attr):
+		return getattr(self.entries, attr)
+
+	def __contains__(self, key):
+		return key in self.entries
+
+	def __iter__(self):
+		return map(_sanitize, self.entries.__iter__())
+
+	def __len__(self):
+		return len(self.entries)
+
+	def __str__(self):
+		return f"sanitized_{self.entries}"
+
+	def __repr__(self):
+		return repr(self.entries)
+
+	def __eq__(self, other):
+		if(isinstance(other, SanitizedObject)):
+			return self.entries == other.entries
+		return self.entries == other
+
+	def __ne__(self, other):
+		return not self.__eq__(other)
+
+	def __add__(self, other):
+		return self.entries + other
+
+	def __sub__(self, other):
+		return self.entries - other
+
+
+class SanitizedDict(SanitizedObject):
+
+	def __init__(self, entries=None, **kwargs):
+		self.entries = entries if entries is not None else {}
+		if(kwargs):
+			self.entries.update(kwargs)
+
+	@property
+	def __class__(self):
+		return dict
+
+	def get(self, key, default=None):
+		v = self.entries.get(key, _sanitize)  # just sentinel
+		if(v is _sanitize):
+			return default
+		return _sanitize(v)
+
+	def items(self):
+		return map(lambda kv: (kv[0], _sanitize(kv[1])), self.entries.items())
+
+
+class SanitizedList(SanitizedObject):
+	# list
+	def __init__(self, entries=None):
+		if(isinstance(entries, SanitizedList)):
+			self.entries = entries.entries
+		else:
+			self.entries = entries if entries is not None else []
+
+	@property
+	def __class__(self):
+		return list
+
+	def at(self, i, escape_html=True, escape_quotes=False):
+		return _sanitize(self.entries[i], escape_html=escape_html, escape_quotes=escape_quotes)
+
+
+class LowerKeyDict(dict):
+	def __getitem__(self, k):
+		return super().__getitem__(k.lower())
+
+	def get(self, k, default=None):
+		return super().get(k.lower(), default)
+
+
+#  MONKEY PATCHING
+def default_json_encoder(enc, obj):
+	if(isinstance(obj, SanitizedObject)):
+		return obj.entries
+	return JSONEncoder.default(enc, obj)
+
+
+try:
+	from json import JSONEncoder
+	JSONEncoder._olddefault = JSONEncoder.default
+	JSONEncoder.default = default_json_encoder
+except ImportError:
+	pass
+
+
+def default_ujson_encoder(obj):
+	if(isinstance(obj, SanitizedObject)):
+		return obj.entries
+	return TypeError(f"Object of type {type(obj).__name__} is not JSON serializable" )
+
+
+try:
+	import ujson
+	ujson.dumps = partial(ujson.dumps, default=default_ujson_encoder)
+except ImportError:
+	pass
+
+try:
+	import simplejson
+	simplejson.dumps = partial(simplejson.dumps, default=default_ujson_encoder)
+except ImportError:
+	pass
+
+# bson patching
+try:
+	from bson.codec_options import TypeEncoder, DEFAULT_CODEC_OPTIONS
+
+	class SanitizedListBsonTypeEncoder(TypeEncoder):
+		python_type = SanitizedList  # the Python type acted upon by this type codec
+		bson_type = list  # the BSON type acted upon by this type codec
+
+		def transform_python(self, value: SanitizedList):
+			return value.entries
+
+	class SanitizedDictBsonTypeEncoder(TypeEncoder):
+		python_type = SanitizedDict
+		bson_type = dict
+
+		def transform_python(self, value: SanitizedDict):
+			return value.entries
+
+	DEFAULT_CODEC_OPTIONS.type_registry._encoder_map[SanitizedList] = SanitizedListBsonTypeEncoder()
+	for enc in [SanitizedListBsonTypeEncoder(), SanitizedDictBsonTypeEncoder()]:
+		DEFAULT_CODEC_OPTIONS.type_registry._encoder_map[enc.python_type] = enc.transform_python
+
+except ImportError:
+	pass
---
+++ packages/blaster-server/blaster-server-0.0.442.tar.gz/blaster-server-0.0.442/test/test_tools.py
@@ -0,0 +1,195 @@
+import unittest
+from blaster import tools
+import time
+import ujson as json
+from blaster.tools import get_time_overlaps, retry,\
+	ExpiringCache, create_signed_value, decode_signed_value
+from blaster.tools.sanitize_html import SanitizedDict, SanitizedList
+from datetime import datetime
+from blaster.utils.data_utils import parse_string_to_units,\
+	parse_currency_string
+
+
+class TestSanitization(unittest.TestCase):
+	def test_sanitization(self):
+		sd = SanitizedDict(a="<a>", b="<b>")
+		sd["c"] = "<c>"
+		sd["d"] = {"e": "<e>", "f": "<f>"}
+
+		self.assertTrue(sd.get("a") == sd["a"] == "&lt;a&gt;")
+		self.assertTrue(sd["d"]["e"] == "&lt;e&gt;")
+
+		# test iterator
+		for k, v in sd.items():
+			if(isinstance(v, SanitizedDict)):
+				for k1, v1 in v.items():
+					self.assertTrue(">" not in v1)
+			else:
+				self.assertTrue(">" not in v)
+
+		self.assertTrue(isinstance(sd, dict))
+		sl = SanitizedList(["<a>", "<b>"])
+		sl.append({"c": "<c>", "d": "<d>"})
+		sl.extend(["<e>", "<f> <>><><><<<>>"])
+		sl.append(["<g>", "<h>"])
+		self.assertTrue(sl[0] == "&lt;a&gt;")
+		self.assertTrue(sl[2]["c"] == "&lt;c&gt;")  # test dict in a list
+		# test iterator
+		for i in sl:
+			if(isinstance(i, SanitizedList)):
+				for j in i:
+					self.assertTrue(">" not in j)
+			else:
+				self.assertTrue(">" not in i)
+
+		# test serialization deserialization
+		self.assertTrue(isinstance(sl, list))
+
+		print(json.dumps(sl))
+
+
+class TestAuth(unittest.TestCase):
+	def test(self):
+		secret = "ijkl"
+		val = create_signed_value("abcd", "efgh", secret).decode('utf-8')
+		self.assertEqual(decode_signed_value("abcd", val, secret), b"efgh")
+
+		val = create_signed_value("abcd", "efgh2", secret, -1).decode('utf-8')
+		self.assertEqual(decode_signed_value("abcd", val, secret), None)
+
+		self.assertEqual(decode_signed_value("abcd", "asdasda", secret), None)
+		print("Auth tests passed")
+
+
+class TestTools(unittest.TestCase):
+	def test_overlaps(self):
+		self.assertEqual(
+			get_time_overlaps(
+				datetime(year=2021, month=10, day=1),
+				datetime(year=2021, month=10, day=20),
+				["Monday 10:30 - 12:30|100EUR", "Tuesday 10:30|200EUR"],
+				exclude=["05/10/2021 10:30"]
+			),
+			[
+				(datetime(2021, 10, 4, 10, 30), datetime(2021, 10, 4, 12, 30), ['100EUR']), 
+				(datetime(2021, 10, 11, 10, 30), datetime(2021, 10, 11, 12, 30), ['100EUR']),
+				(datetime(2021, 10, 12, 10, 30), datetime(2021, 10, 12, 11, 30), ['200EUR']),
+				(datetime(2021, 10, 18, 10, 30), datetime(2021, 10, 18, 12, 30), ['100EUR']),
+				(datetime(2021, 10, 19, 10, 30), datetime(2021, 10, 19, 11, 30), ['200EUR'])
+			]
+		)
+		self.assertEqual(
+			get_time_overlaps(
+				datetime(year=2021, month=10, day=1),
+				datetime(year=2021, month=10, day=20),
+				["5/12/2021 10:30 - 12:30", "07/12/2021 10:30"]
+			),
+			[]
+		)
+		self.assertEqual(
+			get_time_overlaps(
+				datetime(year=2021, month=10, day=1),
+				datetime(year=2021, month=10, day=20),
+				["5/10/2021 10:30 - 6/10/2021 11:30"]
+			),
+			[(datetime(2021, 10, 5, 10, 30), datetime(2021, 10, 6, 11, 30), [])]
+		)
+
+		self.assertEqual(
+			get_time_overlaps(
+				datetime(year=2022, month=7, day=27, hour=23),
+				datetime(year=2022, month=7, day=28, hour=2),
+				["23:30 - 23:45", "00:00 - 00:15", "Wednesday - Thursday"],
+				partial=True
+			),
+			[
+				(datetime(2022, 7, 27, 23, 0), datetime(2022, 7, 28, 2, 0), []),  # web-thursday
+				(datetime(2022, 7, 27, 23, 30), datetime(2022, 7, 27, 23, 45), []),  # 23:30 - 23:45
+				(datetime(2022, 7, 28, 0, 0), datetime(2022, 7, 28, 0, 15), [])  # 00:00 - 00:15
+			]
+		)
+
+		self.assertEqual(
+			get_time_overlaps(
+				datetime(year=2022, month=7, day=27, hour=23),
+				datetime(year=2022, month=7, day=28, hour=2),
+				["23:30 - 23:45", "00:00 - 00:15", "Wednesday 00:00 - Thursday 1:30"],
+				partial=True
+			),
+			[
+				(datetime(2022, 7, 27, 23, 0), datetime(2022, 7, 28, 1, 30), []),  # web-thursday
+				(datetime(2022, 7, 27, 23, 30), datetime(2022, 7, 27, 23, 45), []),  # 23:30 - 23:45
+				(datetime(2022, 7, 28, 0, 0), datetime(2022, 7, 28, 0, 15), [])  # 00:00 - 00:15
+			]
+		)
+
+		self.assertEqual(
+			get_time_overlaps(
+				datetime(year=2021, month=10, day=1),
+				datetime(year=2021, month=10, day=20),
+				["5/10/2021 10:30 - 6/10/2021 11:30"],
+				milliseconds=True
+			),
+			[(1633422600000, 1633512600000, [])]
+		)
+		self.assertEqual(
+			get_time_overlaps(
+				datetime(year=2022, month=6, day=19),
+				datetime(year=2022, month=6, day=27),
+				"Monday 2:30 p.m - 6:30 p.m , Tuesday 9:30 a.m-12:30 p.m,  Tuesday 2:30 p.m-6:30 p.m, Wednesday 9:30 a.m - 12:30 p.m, Wednesday 2:30 p.m- 6:30 p.m, Thursday 9:30 a.m -12:30 p.m, Thursday 2:30 p.m-6:30 p.m, Friday 9:30 a.m- 12:30p.m, Friday 2:30 p.m-6:30 p.m, Saturday 9:30 a.m-12:30 p.m",
+			),
+			[
+				(datetime(2022, 6, 20, 14, 30), datetime(2022, 6, 20, 18, 30), []),
+				(datetime(2022, 6, 21, 9, 30), datetime(2022, 6, 21, 12, 30), []),
+				(datetime(2022, 6, 21, 14, 30), datetime(2022, 6, 21, 18, 30), []),
+				(datetime(2022, 6, 22, 9, 30), datetime(2022, 6, 22, 12, 30), []),
+				(datetime(2022, 6, 22, 14, 30), datetime(2022, 6, 22, 18, 30), []),
+				(datetime(2022, 6, 23, 9, 30), datetime(2022, 6, 23, 12, 30), []),
+				(datetime(2022, 6, 23, 14, 30), datetime(2022, 6, 23, 18, 30), []),
+				(datetime(2022, 6, 24, 9, 30), datetime(2022, 6, 24, 12, 30), []), 
+				(datetime(2022, 6, 24, 14, 30), datetime(2022, 6, 24, 18, 30), []),
+				(datetime(2022, 6, 25, 9, 30), datetime(2022, 6, 25, 12, 30), [])
+			]
+
+		)
+
+	@retry(2)
+	def test_can_retry(self):
+		raise Exception
+
+	def test_string_to_units(self):
+		print(parse_string_to_units(".9 units"))
+		print(parse_string_to_units("0.9 units"))
+		print(parse_string_to_units("rs -1.9"))
+		print(parse_currency_string("INR 2000"))
+
+	def remove_duplicates(self):
+		l2 = tools.remove_duplicates([1, 2, 3, 5, 3])
+		self.assertEqual(l2, [1, 2, 3, 5])
+
+		l2 = tools.remove_duplicates(["a", "a", "b", "c", "a", "b"])
+		self.assertEqual(l2, ["a", "b", "c"])
+
+		l2 = tools.remove_duplicates([{"a": 1}, {"a":1}, {"a": 2}, {"a": 2}], key=lambda x:x.get("a"))
+		self.assertEqual(l2, [{"a": 1}, {"a": 2}])
+
+	def test_expiring_cache(self):
+		c = ExpiringCache(10, ttl=1000)
+		c.set(1, 2)
+		c.set(2, 2)
+		time.sleep(1)
+		c.set(3, 3)
+		self.assertIsNone(c.get(1))
+		self.assertIsNone(c.get(2))
+		self.assertEqual(c.get(3), 3)
+		time.sleep(1) # all items expired
+		self.assertIsNone(c.get(3))
+		for i in range(9):
+			time.sleep(0.2)
+			c.set(i, i) 
+		# when last item is added, it expired first 4 items
+		self.assertEqual(len(c.to_son()), 5)
+
+
+if __name__ == "__main__":
+	unittest.main()
