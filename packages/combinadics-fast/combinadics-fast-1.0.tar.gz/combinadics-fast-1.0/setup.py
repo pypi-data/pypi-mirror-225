f3906b80dde8b520618856506ec9c26d9c85bda8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 14:08:13 +0000

    Add package combinadics-fast/1.0/combinadics-fast-1.0.tar.gz

---
+++ packages/combinadics-fast/combinadics-fast-1.0.tar.gz/combinadics-fast-1.0/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='combinadics-fast',
+      version='1.0',
+      description='Fast tool for converting unique numbers <-> unique k-combinations.',
+      author='Andrew Healey',
+      author_email='doolie.healey@gmail.com',
+      url='https://github.com/andrew-healey/combinadics',
+      py_modules=['combinadics_fast']
+     )
