0d3561d51d0a5f5dcdb7fd467c3f226dc3bd604b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 04:11:33 +0000

    Add package JsonDF/1.0.0/jsondf-1.0.0-py3-none-any.whl

---
+++ packages/JsonDF/jsondf-1.0.0-py3-none-any.whl/src/Data.py
@@ -0,0 +1,43 @@
+import JsonDF.src.utils.Dict.Dict as d
+import JsonDF.src.utils.List.List as l
+
+class Data:
+    def __init__(self, prefix='root', data=dict()) -> None:
+        self.data = data
+        self.prefix = prefix
+        self.rows = {}
+        self.rows_flatten = {}
+    
+    def childs(self):
+        if type(self.data) == dict: 
+            if len(self.data.keys()) >= 1:
+                self.childsType(list(self.data.keys()))
+        if type(self.data) == list:
+            if len(self.data) >= 1:
+                self.data = self.data[0]
+                self.childsType(self.data)
+    
+    def childsType(self, keys):
+        for key in keys:
+            if type(self.data[key]) == list or type(self.data[key]) == dict:
+                self.rows[f"{self.prefix}_{key}"] = self.child(data=self.data[key], prefix=f"{self.prefix}_{key}")
+            else:
+                self.rows[f"{self.prefix}_{key}"] = self.data[key]
+    
+    def tabelize(self, rows):
+        for key, value in rows.items():
+            if type(value) == dict:
+                yield from self.tabelize(value)
+            else:
+                yield(key, value)
+    
+    def flatten(self):
+        for key, value in self.tabelize(self.rows):
+            self.rows_flatten[key] = value
+
+
+    def child(self, data, prefix):
+        if type(data) == list:
+            return l.List(data=data, prefix=prefix).rows
+        elif type(data) == dict:
+            return d.Dict(data=data, prefix=prefix).rows
---
+++ packages/JsonDF/jsondf-1.0.0-py3-none-any.whl/src/utils/List/List.py
@@ -0,0 +1,33 @@
+import JsonDF.src.utils.Dict.Dict as d
+
+class List:
+    def __init__(self, prefix, data):
+        self.data = data
+        self.prefix = prefix
+        self.rows = {}
+        self.childs = self.process()
+
+    
+    def process(self):
+        if len(self.data) > 1:
+            self.data = self.data[0]
+            self.childs(self.data)
+        elif len(self.data) == 1:
+            if type(self.data[0]) == dict:
+                self.data = self.data[0]
+                return self.childs(list(self.data.keys()))
+            else:
+                return self.data[0]
+    
+    def childs(self, keys):
+        for key in keys:
+            if type(self.data[key]) == list or type(self.data[key]) == dict:
+                self.rows[f"{self.prefix}_{key}"] = self.childType(data=self.data[key], prefix=f"{self.prefix}_{key}")
+            else:
+                self.rows[f"{self.prefix}_{key}"] = self.data[key]
+    
+    def childType(self, data, prefix):
+        if type(data) == list:
+            return List(data=data, prefix=prefix).rows
+        elif type(data) == dict:
+            return d.Dict(data=data, prefix=prefix).rows
