08ec5c7a80772ab8f6e982302aeb25e87e5f5fd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 03:07:21 +0000

    Add package django-rework/0.6.0/django-rework-0.6.0.tar.gz

---
+++ packages/django-rework/django-rework-0.6.0.tar.gz/django-rework-0.6.0/setup.py
@@ -0,0 +1,52 @@
+from setuptools import find_packages, setup
+from rework import __version__
+
+
+def read(f):
+    return open(f, 'r', encoding='utf-8').read()
+
+
+# version Operators
+# https://pip.pypa.io/en/latest/user_guide/#fixing-conflicting-dependencies
+INSTALL_REQUIREMENTS = read('requirements.txt').splitlines()
+
+setup(
+    name='django-rework',
+    version=__version__,
+    description='Rapid develop framework base on Django',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    url='https://github.com/rework-union/django-rework',
+    author='rework union',
+    author_email='josh.yu_8@live.com',
+    license='MIT',
+    entry_points={
+        'console_scripts': [
+            'rework = rework.core.management:execute_from_command_line',
+        ]
+    },
+    install_requires=INSTALL_REQUIREMENTS,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4.0',
+        'Framework :: Django :: 4.1',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+)
