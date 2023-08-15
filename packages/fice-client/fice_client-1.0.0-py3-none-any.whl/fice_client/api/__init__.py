e05e357a375e8669710225999e9eb5f2abda4dee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 03:09:15 +0000

    Add package fice-client/1.0.0/fice_client-1.0.0-py3-none-any.whl

---
+++ packages/fice-client/fice_client-1.0.0-py3-none-any.whl/fice_client/api/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+
+# import apis into api package
+from fice_client.api.agents_api import AgentsApi
+from fice_client.api.authorization_api import AuthorizationApi
+from fice_client.api.compliance_api import ComplianceApi
+from fice_client.api.compliance_policy_api import CompliancePolicyApi
+from fice_client.api.devices_api import DevicesApi
+from fice_client.api.miscellaneous_api import MiscellaneousApi
+from fice_client.api.tenats_api import TenatsApi
+
