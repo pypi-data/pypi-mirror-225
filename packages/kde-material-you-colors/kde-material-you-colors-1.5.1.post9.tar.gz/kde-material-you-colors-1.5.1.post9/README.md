841203ba8bea2b97b74a497a341672bf45e58dfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 03:07:02 +0000

    Add package kde-material-you-colors/1.5.1.post9/kde-material-you-colors-1.5.1.post9.tar.gz

---
+++ packages/kde-material-you-colors/kde-material-you-colors-1.5.1.post9.tar.gz/kde-material-you-colors-1.5.1.post9/README.md
@@ -0,0 +1,173 @@
+<div align="center">
+
+# 🎨 KDE Material You Colors
+
+## Automatic Material You Colors Generator from your wallpaper for the Plasma Desktop
+
+This is a Python program that uses the [Python implementation](https://github.com/avanishsubbiah/material-color-utilities-python) of Google's [Material Color Utilities](https://github.com/material-foundation/material-color-utilities) to generate a Material Design 3 color scheme.
+Which is used to generate Light and Dark Color Themes for KDE (and pywal if installed) from your current wallpaper, automatically.
+
+![](https://img.shields.io/static/v1?style=for-the-badge&label=Made%20with&message=Python&color=1f425f&logo=python&labelColor=2d333b)
+![](https://img.shields.io/github/v/release/luisbocanegra/kde-material-you-colors?include_prereleases&style=for-the-badge&color=1f425f&labelColor=2d333b)
+</div>
+
+<div>
+<img src="https://user-images.githubusercontent.com/15076387/188578458-8171e42b-f36c-44c1-9eb0-506c301d4f16.gif"  alt="Screenshot">
+</div>
+
+# Features
+
+## Plasma specific
+
+- Update automatically on wallpaper change
+- Multiple wallpaper [plugins supported](#working-wallpaper-plugins)
+- Support for selecting Wallpaper plugin from secondary monitors
+- Dark and Light Icon theme
+- Support Plasma 5.26+ dark wallpaper variants
+- Start automatically on login
+- Make titlebar darker to match specified applications like terminals, code editors and other programs themed by pywal
+- Follow Plasma Material You Dark/Light change to work with theme schedulers like [Koi](https://github.com/baduhai/Koi)
+- **Plasma addons**
+  - Tint [SierraBreeze](https://github.com/kay0u/SierraBreeze) window decoration buttons
+  - TitleBar opacity control for [Klassy](https://github.com/paulmcauley/klassy) and [SierraBreezeEnhanced](https://github.com/kupiqu/SierraBreezeEnhanced) window decorations
+  - ToolBar opacity control for [Lightly](https://github.com/Luwx/Lightly) Application style
+  - Tint [Klassy](https://github.com/paulmcauley/klassy) window decoration outline
+
+## Themeable programs
+
+- Konsole color scheme
+  - opacity control
+- **[Pywal](https://github.com/dylanaraps/pywal) support to theme other programs using Material You Colors**
+- Basic KSyntaxHighlighting support (Kate, KWrite, KDevelop...)
+
+## Theming options
+
+- Alternative Material You color selection if the wallpaper provides more than one
+- Use your favorite color to generate Material You color schemes
+- Custom colors list used for konsole/pywal
+- Custom amount for background color tint
+- Dark/light Color schemes (Plasma and pywal/konsole independently)
+- Set a script/command that will be executed on start or wallpaper/dark/light/settings change
+- Configuration file
+
+# Installing
+
+## Using pypi
+
+```sh
+pip install kde-material-you-colors
+# Optional
+# pywal to theme other programs using Material You Colors
+pip install pywal
+# Colr to display colored palette and seed colors from terminal (approximate)
+pip install colr
+```
+
+## Arch Linux
+
+- [AUR](https://aur.archlinux.org/packages/kde-material-you-colors) use your preferred AUR helper
+
+## openSUSE Build Service packages by [marknefedov](https://github.com/marknefedov)
+
+### Fedora
+
+Add repository and install as root:
+
+```sh
+dnf config-manager --add-repo https://download.opensuse.org/repositories/home:MarkNefedov/Fedora_37/home:MarkNefedov.repo
+dnf install kde-material-you-colors
+```
+
+### openSUSE
+
+For **openSUSE Tumbleweed** add repository and install as root:
+
+```sh
+zypper addrepo https://download.opensuse.org/repositories/home:MarkNefedov/openSUSE_Tumbleweed/home:MarkNefedov.repo
+zypper refresh
+zypper install kde-material-you-colors
+```
+
+For **openSUSE 15.4** add repository and install as root:
+
+```sh
+zypper addrepo https://download.opensuse.org/repositories/home:MarkNefedov/15.4/home:MarkNefedov.repo
+zypper refresh
+zypper install kde-material-you-colors
+```
+
+## Optional features
+
+- Install the [Colr](https://pypi.org/project/Colr/) python module to display colored palette and seed colors from terminal
+- Install the [pywal](https://pypi.org/project/pywal/) python module to theme other programs using Material You Colors
+  - Check [pywal Customization Wiki](https://github.com/dylanaraps/pywal/wiki/Customization) to theme supported programs
+
+# Running from terminal to debug your configuration
+
+- Run `kde-material-you-colors`
+
+- Flags take precedence over sonfiguration file, run `kde-material-you-colors -h` to see the list of available options
+
+## Starting/Stopping Desktop entries
+
+Run `kde-material-you-colors -cl` to copy desktop entries to ~/.local/share/applications/
+
+- To start the program launch **KDE Material You Colors** from your applications list
+- To stop it launch **Stop KDE Material You Colors** from your applications list
+
+# Running on Startup
+
+After finishing the setup, you can make it run automatically on boot
+
+1. Copy the default configuration to ~/.config/kde-material-you-colors/config.conf:
+
+    `kde-material-you-colors -c`
+
+2. Set the program to automatically start with Plasma:
+
+    `kde-material-you-colors -a`
+
+3. Reboot or logout/login and test the changes
+
+## Removing from autostart
+
+1. Open `System Settings` > `Startup and Shutdown`
+2. Remove `kde-material-you-colors` by clicking on the `-` button.
+
+# Supported Wallpaper plugins
+
+**Wallpaper plugins must store the current wallpaper in `~/.config/plasma-org.kde.plasma.desktop-appletsrc`**
+
+Confirmed working Plasma Wallpaper Plugins:
+
+| Name | ID |
+| ----------- | ----------- |
+| Image (default) | `org.kde.image` |
+| Picture of the day | `org.kde.potd` |
+| Slideshow | `org.kde.slideshow` |
+| Plain color | `org.kde.color` |
+| Conway's Game of Life (cell color) | `org.kde.plasma.gameoflife` |
+| Active blur | `a2n.blur` |
+
+# Configuration file
+
+- Copy default configuration: run `kde-material-you-colors -c`
+- Edit ~/.config/kde-material-you-colors/config.conf
+- Run `kde-material-you-colors` with no arguments from terminal to test it.
+- **You can view the sample configuration file [here](https://github.com/luisbocanegra/kde-material-you-colors/blob/plasmoid/src/kde_material_you_colors/data/sample_config.conf)**
+
+# Notes
+
+- To update color with `plasma-apply-colorscheme` (utility provided by plasma developers), the file containing the new color scheme must have a different name than the current one, to workaround this the program creates two scheme files with different names, then applies one after the other. As a result you end up with duplicated color schemes and maybe some lag while updating schemes.
+
+## Bug reporting / Feature requests / Contributing
+
+Please read the [Contributing guidelines in this repository](CONTRIBUTING.md)
+
+# Thanks & Credits
+
+- [Avanish Subbiah](https://github.com/avanishsubbiah) for the [Python implementation](https://github.com/avanishsubbiah/material-color-utilities-python) of Material Color Utilities required by this project.
+- [This comment by throwaway6560192 on Reddit](https://www.reddit.com/r/kde/comments/mg6wr4/comment/gssbtqe/?utm_source=share&utm_medium=web2x&context=3) and [@pashazz  (Pavel Borisov) ksetwallpaper](https://github.com/pashazz/ksetwallpaper) for the script to get the current Wallpaper that served me as starting point.
+- Everyone that made [material-color-utilities](https://github.com/material-foundation/material-color-utilities) possible.
+- [Pywal](https://github.com/dylanaraps/pywal) developers
+- [Albert Ragány-Németh](https://github.com/albi005) for the [C# implementation](https://github.com/albi005/MaterialColorUtilities) of Material Color Utilities (used until v0.8.0).
---
+++ packages/kde-material-you-colors/kde-material-you-colors-1.5.1.post9.tar.gz/kde-material-you-colors-1.5.1.post9/src/kde_material_you_colors/utils/plasma_utils.py
@@ -0,0 +1,219 @@
+import configparser
+import logging
+import os
+import subprocess
+from .. import settings
+from . import kwin_utils
+from . import file_utils
+
+
+def make_scheme(schemes=None):
+    # Make sure the schemes path exists
+    if not os.path.exists(settings.USER_SCHEMES_PATH):
+        os.makedirs(settings.USER_SCHEMES_PATH)
+    light_scheme = schemes.get_light_scheme()
+    dark_scheme = schemes.get_dark_scheme()
+    # plasma-apply-colorscheme doesnt allow to apply the same theme twice to reload
+    # since I don't know how to reaload it with code lets make a copy and switch between them
+    # sadly color settings will show copies too
+
+    with open(
+        settings.THEME_LIGHT_PATH + "2.colors", "w", encoding="utf8"
+    ) as light_scheme_file:
+        light_scheme_file.write(light_scheme)
+    with open(
+        settings.THEME_LIGHT_PATH + ".colors", "w", encoding="utf8"
+    ) as light_scheme_file:
+        light_scheme_file.write(light_scheme)
+    with open(
+        settings.THEME_DARK_PATH + "2.colors", "w", encoding="utf8"
+    ) as dark_scheme_file:
+        dark_scheme_file.write(dark_scheme)
+    with open(
+        settings.THEME_DARK_PATH + ".colors", "w", encoding="utf8"
+    ) as dark_scheme_file:
+        dark_scheme_file.write(dark_scheme)
+
+    plasma_darker_header(schemes)
+
+
+def apply_color_schemes(light=False):
+    if light == None:
+        light = False
+    if light != None:
+        if light == True:
+            color_scheme = settings.THEME_LIGHT_PATH
+        elif light == False:
+            color_scheme = settings.THEME_DARK_PATH
+        kwin_utils.blend_changes()
+        subprocess.run(
+            "plasma-apply-colorscheme " + color_scheme + "2.colors",
+            shell=True,
+            stderr=subprocess.DEVNULL,
+            stdout=subprocess.DEVNULL,
+        )
+        colorscheme_out = subprocess.check_output(
+            "plasma-apply-colorscheme " + color_scheme + ".colors",
+            shell=True,
+            stderr=subprocess.PIPE,
+            universal_newlines=True,
+        ).strip()
+        logging.info(colorscheme_out)
+        # Get hash of the updated theme
+        hash = file_utils.get_file_sha1(color_scheme + ".colors")
+        # Update ColorScheme hash in kdeglobals file
+        if os.path.exists(settings.KDE_GLOBALS) and hash is not None:
+            kdeglobals = configparser.ConfigParser()
+            kdeglobals.optionxform = str
+            try:
+                kdeglobals.read(settings.KDE_GLOBALS)
+                if "General" not in kdeglobals:
+                    kdeglobals.add_section("General")
+
+                general = kdeglobals["General"]
+                general["ColorSchemeHash"] = hash
+
+                with open(settings.KDE_GLOBALS, "w") as configfile:
+                    kdeglobals.write(configfile, space_around_delimiters=False)
+            except Exception as e:
+                logging.error(f"Error:\n{e}")
+
+
+def set_icons(icons_light, icons_dark, light=False):
+    """Set icon theme with plasma-changeicons for light and dark schemes
+
+    Args:
+        icons_light (str): Light mode icon theme
+        icons_dark (str): Dark mode icon theme
+        light (bool): wether using light or dark mode
+    """
+    if light and icons_light != None:
+        icons = icons_light
+    elif not light and icons_dark != None:
+        icons = icons_dark
+    else:
+        icons = None
+    if icons != None:
+        changeicons_error = subprocess.check_output(
+            "/usr/lib/plasma-changeicons " + icons,
+            shell=True,
+            stderr=subprocess.STDOUT,
+            universal_newlines=True,
+        ).strip()
+        logging.info(f"{icons} {changeicons_error}")
+
+
+def kde_globals_light():
+    kdeglobals = configparser.ConfigParser()
+    if os.path.exists(settings.KDE_GLOBALS):
+        try:
+            kdeglobals.read(settings.KDE_GLOBALS)
+            if "General" in kdeglobals:
+                general = kdeglobals["General"]
+                if "ColorScheme" in general:
+                    if "MaterialYouDark" in general["ColorScheme"]:
+                        return False
+                    elif "MaterialYouLight" in general["ColorScheme"]:
+                        return True
+            else:
+                return None
+        except Exception as e:
+            logging.error(f"Error:\n{e}")
+            return None
+    else:
+        return None
+
+
+def get_initial_mode():
+    """Try to get the initial theme mode based based on the theme name,
+    if failed try to get it from the stored hash and the current
+    generated schemes.
+
+    Returns:
+        bool: Current mode light=True, dark=False
+    """
+
+    theme_from_name = kde_globals_light()
+
+    if theme_from_name is not None:
+        logging.info("Using theme from stored name")
+        return theme_from_name
+    else:
+        logging.info(
+            "Couldn't find theme by name, trying to resolve theme from last stored hash..."
+        )
+
+        current_theme_hash = None
+        kdeglobals = configparser.ConfigParser()
+        kdeglobals.optionxform = str
+        try:
+            kdeglobals.read(settings.KDE_GLOBALS)
+            if "General" in kdeglobals and "ColorSchemeHash" in kdeglobals["General"]:
+                current_theme_hash = kdeglobals["General"]["ColorSchemeHash"]
+        except Exception as e:
+            logging.error(f"Error:\n{e}")
+
+        if current_theme_hash is not None:
+            logging.debug(f"Config file hash:  {current_theme_hash}")
+            dark_scheme_hash = file_utils.get_file_sha1(
+                settings.THEME_DARK_PATH + ".colors"
+            )
+            logging.debug(f"Dark scheme hash:  {dark_scheme_hash}")
+            light_scheme_hash = file_utils.get_file_sha1(
+                settings.THEME_LIGHT_PATH + ".colors"
+            )
+            logging.debug(f"Light scheme hash: {light_scheme_hash}")
+            if current_theme_hash == dark_scheme_hash:
+                return False
+            if current_theme_hash == light_scheme_hash:
+                return True
+
+        logging.warning("Couldn't find previus theme, falling back to dark mode...")
+        return False
+
+
+def plasma_darker_header(schemes):
+    """Make a copy of the generated plasma themes but with darker headers
+
+    Args:
+        schemes (ThemeConfig): generated color schemes
+    """
+    light_color = schemes.get_wal_light_scheme()["special"]["background"]
+    dark_color = schemes.get_wal_dark_scheme()["special"]["background"]
+    color_scheme = configparser.ConfigParser()
+    color_scheme.optionxform = str
+    try:
+        # Edit titlebar of dark scheme
+        color_scheme.read(settings.THEME_DARK_PATH + ".colors")
+        color_scheme["Colors:Header][Inactive"]["BackgroundNormal"] = dark_color
+        color_scheme["Colors:Header"]["BackgroundNormal"] = dark_color
+        color_scheme["General"]["Name"] = "Material You Dark (darker titlebar)"
+
+        with open(
+            settings.THEME_DARK_PATH + "_darker_titlebar.colors", "w"
+        ) as configfile:
+            color_scheme.write(configfile, space_around_delimiters=False)
+        color_scheme["General"]["Name"] = "Material You Dark (darker titlebar2)"
+        with open(
+            settings.THEME_DARK_PATH + "_darker_titlebar2.colors", "w"
+        ) as configfile:
+            color_scheme.write(configfile, space_around_delimiters=False)
+
+        # Edit titlebar of light scheme
+        color_scheme.read(settings.THEME_LIGHT_PATH + ".colors")
+        color_scheme["Colors:Header][Inactive"]["BackgroundNormal"] = light_color
+        color_scheme["Colors:Header"]["BackgroundNormal"] = light_color
+        color_scheme["General"]["Name"] = "Material You Light (darker titlebar)"
+
+        with open(
+            settings.THEME_LIGHT_PATH + "_darker_titlebar.colors", "w"
+        ) as configfile:
+            color_scheme.write(configfile, space_around_delimiters=False)
+        color_scheme["General"]["Name"] = "Material You Light (darker titlebar2)"
+        with open(
+            settings.THEME_LIGHT_PATH + "_darker_titlebar2.colors", "w"
+        ) as configfile:
+            color_scheme.write(configfile, space_around_delimiters=False)
+
+    except Exception as e:
+        logging.error(f"Error:\n{e}")
