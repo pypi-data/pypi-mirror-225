cd320e76e4b9cf08d346fe1fa59f98e5cbf15060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 14:08:20 +0000

    Add package giger/0.1.4/giger-0.1.4.tar.gz

---
+++ packages/giger/giger-0.1.4.tar.gz/giger-0.1.4/src/giger/commands/prompt.py
@@ -0,0 +1,53 @@
+import logging
+import sys
+
+from ..services.prompt import PromptService
+
+_logger = logging.getLogger(__name__)
+
+
+class PromptCommand:
+    def __init__(self, parser):
+        self.parser = parser.add_parser("prompt", help="Generate prompt")
+        self.parser.add_argument("description", nargs="*")
+        self.parser.add_argument("--time", choices=PromptService.times())
+        self.parser.add_argument("--type", choices=PromptService.types())
+        self.parser.add_argument("--background_color")
+        self.parser.add_argument(
+            "--art_style", choices=PromptService.art_styles(), nargs="*"
+        )
+        self.parser.add_argument("--artist", choices=PromptService.artists(), nargs="*")
+        self.parser.add_argument(
+            "--realism", choices=PromptService.realisms(), nargs="*"
+        )
+        self.parser.add_argument(
+            "--rendering_engine", choices=PromptService.rendering_engines(), nargs="*"
+        )
+        self.parser.add_argument(
+            "--lightning_angle", choices=PromptService.lightning_angles(), nargs="*"
+        )
+        self.parser.add_argument(
+            "--lightning_style", choices=PromptService.lightning_styles(), nargs="*"
+        )
+        self.parser.add_argument(
+            "--camera_position", choices=PromptService.camera_positions(), nargs="*"
+        )
+        self.parser.add_argument("--camera", choices=PromptService.cameras(), nargs="*")
+        self.parser.add_argument("--style", choices=PromptService.styles(), nargs="*")
+        self.parser.add_argument(
+            "--composition", choices=PromptService.compositions(), nargs="*"
+        )
+        self.parser.add_argument("--iso", choices=PromptService.isos())
+        self.parser.add_argument(
+            "--resolution", choices=PromptService.resolutions(), nargs="*"
+        )
+        self.parser.add_argument(
+            "--compel_style", choices=PromptService.compel_styles()
+        )
+
+    def run(self, args):
+        if not sys.stdin.isatty():
+            args.description.extend(sys.stdin.read().splitlines())
+        args.description = ", ".join(args.description)
+        _logger.info("Creating prompt for '{0}'".format(args.description))
+        print(PromptService().generate(args))
