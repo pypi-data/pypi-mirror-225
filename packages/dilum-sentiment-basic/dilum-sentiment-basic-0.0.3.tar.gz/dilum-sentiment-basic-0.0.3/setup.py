5fe4d3cf841bb339e45f6e15708534396b2527f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 03:09:15 +0000

    Add package dilum-sentiment-basic/0.0.3/dilum-sentiment-basic-0.0.3.tar.gz

---
+++ packages/dilum-sentiment-basic/dilum-sentiment-basic-0.0.3.tar.gz/dilum-sentiment-basic-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+import setuptools
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name="dilum-sentiment-basic",
+    version="0.0.3",
+    author="Dilum Sanjaya",
+    author_email="info@dilums.com",
+    description="Streamlit custom component to display sentiment analysis data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://dilums.com",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.7",
+    install_requires=[
+        "streamlit >= 0.63",
+    ],
+)
