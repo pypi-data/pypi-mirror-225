65fe77dc9da5710afbe01d1f57b5d3c2cd8e542d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 15:06:48 +0000

    Add package deepeval/0.2.0/deepeval-0.2.0.tar.gz

---
+++ packages/deepeval/deepeval-0.2.0.tar.gz/deepeval-0.2.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from evals._version import __version__
+
+setup(
+    name="deepeval",
+    version=__version__,
+    url="https://github.com/mr-gpt/evals",
+    author="Twilix",
+    author_email="jacky@twilix.io",
+    description="Eval",
+    packages=find_packages(),
+    # TODO - make pandas an 'extra' requirement in the future
+    install_requires=[
+        "requests",
+        "tqdm",
+        "transformers",
+        "pytest",
+        "tabulate",
+    ],
+)
