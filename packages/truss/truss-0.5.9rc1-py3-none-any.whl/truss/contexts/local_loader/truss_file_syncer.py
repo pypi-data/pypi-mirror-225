42ef39e2baccac4f3efaad07733249643e81e48e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 14:08:28 +0000

    Add package truss/0.5.9rc1/truss-0.5.9rc1-py3-none-any.whl

---
+++ packages/truss/truss-0.5.9rc1-py3-none-any.whl/truss/contexts/local_loader/truss_file_syncer.py
@@ -0,0 +1,35 @@
+import logging
+from pathlib import Path
+from threading import Thread
+
+
+class TrussFilesSyncer(Thread):
+    """Daemon thread that watches for changes in the user's Truss and syncs to running service."""
+
+    def __init__(
+        self,
+        watch_path: Path,
+        remote,
+    ) -> None:
+        from truss.util.path import is_ignored, load_trussignore_patterns
+
+        super().__init__(daemon=True)
+        self._logger = logging.Logger(__name__)
+        self.watch_path = watch_path
+        self.remote = remote
+        self.watch_filter = lambda _, path: not is_ignored(
+            Path(path),
+            load_trussignore_patterns(),
+        )
+
+    def run(self) -> None:
+        """Watch for files in background and apply appropriate patches."""
+        from watchfiles import watch
+
+        # disable watchfiles logger
+        logging.getLogger("watchfiles.main").disabled = True
+
+        for _ in watch(
+            self.watch_path, watch_filter=self.watch_filter, raise_interrupt=False
+        ):
+            self.remote.patch(self.watch_path, self._logger)
