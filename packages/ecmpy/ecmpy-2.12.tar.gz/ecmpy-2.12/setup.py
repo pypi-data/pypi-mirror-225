398be0c5ff2aa553ac8ae2c8f6c46ba991d13023
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 15:07:17 +0000

    Add package ecmpy/2.12/ecmpy-2.12.tar.gz

---
+++ packages/ecmpy/ecmpy-2.12.tar.gz/ecmpy-2.12/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ecmpy',
+    version='2.12',
+    packages=find_packages(),
+    package_data={
+        "ecmpy": [
+            "*",
+            "*/*",
+            "_cache/*",
+            "analysis/*",
+            "data/*",
+            "model/*",
+            "script/*"
+            "ipynb_checkpoints/*"
+            "__pacache__/*"            
+        ]
+    },
+    license='MIT',
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+        'cobra==0.21.0',
+        'openpyxl',
+        'requests',
+        'pebble',
+        'xlsxwriter',
+        'Bio',
+        'Require',
+        'quest',
+        'scikit-learn',
+        'RDKit',
+        'seaborn',
+        'pubchempy',
+        'torch',
+        'ipykernel',
+        'bioservices==1.10.4',
+        'pyprobar',
+        'xmltodict',
+        'plotly',
+        'kaleido',
+        'nbformat',
+        ],
+    author='Zhitao Mao',
+    author_email='mao_zt@tib.cas.cn',
+    description='Automated construction of enzyme constraint models using ECMpy workflow.',
+    url='https://github.com/tibbdc/ECMpy2.0',
+)
