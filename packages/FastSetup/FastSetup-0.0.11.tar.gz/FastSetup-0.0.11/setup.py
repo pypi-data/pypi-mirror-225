aa897898869ca29bd3b7ce3f73ec72654ca8591f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 03:07:52 +0000

    Add package FastSetup/0.0.11/FastSetup-0.0.11.tar.gz

---
+++ packages/FastSetup/FastSetup-0.0.11.tar.gz/FastSetup-0.0.11/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+ 
+
+classifiers = [
+  'Development Status :: 1 - Planning',
+  'Intended Audience :: Developers',
+  'License :: OSI Approved :: MIT License',
+  'Operating System :: Microsoft :: Windows :: Windows 11',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='FastSetup',
+  version='0.0.11',
+  description='Static code in python scripts',
+  long_description = open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Tareq Abeda',
+  author_email='TareqAbeda@outlook.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='FastSetup', 
+  packages=find_packages(),
+  install_requires=['logging', 'pandas', 'datetime'] 
+)
