a568d5e441d6df22e3db3c93543c2fc618cb5c30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 03:08:06 +0000

    Add package onenonly/0.1/onenonly-0.1.tar.gz

---
+++ packages/onenonly/onenonly-0.1.tar.gz/onenonly-0.1/onenonly/__init__.py
@@ -0,0 +1,27 @@
+r"""
+One-n-Only, a library for Python
+================================
+
+Modules:
+-------
+`areas`
+`const`
+`data`
+`LinkedList`
+`List`
+`maths`
+`matrix`
+`perimeter`
+`queue`
+`search`
+`sort`
+`stack`
+`stats`
+`utils`
+"""
+
+VERSION = "0.1"
+AUTHOR = "Sahil Rajwar"
+HOMEPAGE = "https://github.com/Sahil-Rajwar-2004/One-n-Only"
+
+
