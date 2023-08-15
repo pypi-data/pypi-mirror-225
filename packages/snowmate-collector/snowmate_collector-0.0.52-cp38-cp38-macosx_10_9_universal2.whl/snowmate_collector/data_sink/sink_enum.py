92536165dae5ab9a1f5ce08c8f335331874ae6e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-15 14:07:20 +0000

    Add package snowmate-collector/0.0.52/snowmate_collector-0.0.52-cp38-cp38-macosx_10_9_universal2.whl

---
+++ packages/snowmate-collector/snowmate_collector-0.0.52-cp38-cp38-macosx_10_9_universal2.whl/snowmate_collector/data_sink/sink_enum.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+from snowmate_collector.data_sink.debug_print_sink import StdoutSink
+from snowmate_collector.data_sink.http_sink import HTTPSink
+from snowmate_collector.data_sink.sanity_sink import SanitySink
+
+
+class DataSinks(Enum):
+    """
+    This is an enum containing all the available data sinks.
+    """
+
+    PRINT = StdoutSink()
+    SANITY = SanitySink()
+    HTTP = HTTPSink()
