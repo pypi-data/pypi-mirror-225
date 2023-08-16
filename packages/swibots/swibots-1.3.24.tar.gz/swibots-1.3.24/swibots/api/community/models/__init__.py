24ae4dd0bc1d4e9004079fcd73fbd3a932e9fe05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 04:12:57 +0000

    Add package swibots/1.3.24/swibots-1.3.24.tar.gz

---
+++ packages/swibots/swibots-1.3.24.tar.gz/swibots-1.3.24/swibots/api/community/models/__init__.py
@@ -0,0 +1,13 @@
+from .channel import Channel
+from .community import Community
+from .group import Group
+from .role import Role
+from .rolepermission import RolePermission
+from .rolemember import RoleMember
+from .baninfo import BanInfo
+from .community_member import CommunityMember
+from .restricteduser import RestrictedUser
+from .instantmessaging import InstantMessaging
+
+__all__ = ["Channel", "Community", "Group", "Role", "RolePermission", "RoleMember", "BanInfo", "CommunityMember", "RestrictedUser",
+           "InstantMessaging"]
