474d3f39afe4c182abfc278e5d753101cc144036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 03:11:20 +0000

    Add package atproto/0.0.24/atproto-0.0.24.tar.gz

---
+++ packages/atproto/atproto-0.0.24.tar.gz/atproto-0.0.24/atproto/xrpc_client/models/com/atproto/sync/get_commit_path.py
@@ -0,0 +1,29 @@
+##################################################################
+# THIS IS THE AUTO-GENERATED CODE. DON'T EDIT IT BY HANDS!
+# Copyright (C) 2023 Ilya (Marshal) <https://github.com/MarshalX>.
+# This file is part of Python atproto SDK. Licenced under MIT.
+##################################################################
+
+
+import typing as t
+from dataclasses import dataclass
+
+from atproto.xrpc_client.models import base
+
+
+@dataclass
+class Params(base.ParamsModelBase):
+
+    """Parameters model for :obj:`com.atproto.sync.getCommitPath`."""
+
+    did: str  #: The DID of the repo.
+    earliest: t.Optional[str] = None  #: The earliest commit to start from.
+    latest: t.Optional[str] = None  #: The most recent commit.
+
+
+@dataclass
+class Response(base.ResponseModelBase):
+
+    """Output data model for :obj:`com.atproto.sync.getCommitPath`."""
+
+    commits: t.List[str]  #: Commits.
