09e89f563ef79db3050c5699f697e1d1f1a0e5ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 04:14:59 +0000

    Add package PyDAO-9000/1.2.2/PyDAO_9000-1.2.2-py3-none-any.whl

---
+++ packages/PyDAO-9000/PyDAO_9000-1.2.2-py3-none-any.whl/SqltDAO/__init__.py
@@ -0,0 +1,14 @@
+'''
+Supporting namespace for generating Soft9000.com PyDAO projects.
+
+Enjoy!
+
+   -- Randall
+'''
+if True:
+   from SqltDAO.CodeGen01.Meta import Meta
+   from SqltDAO.main import Main as App
+   print(f'Starting "{Meta.Title()}" ...')
+   App.mainloop()
+   print("Done!")
+   quit()
