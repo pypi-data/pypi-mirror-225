2f5a5deda42e17d13b9d70d713adb88786340bb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 03:08:36 +0000

    Add package hides/0.0.2/hides-0.0.2-py3-none-any.whl

---
+++ packages/hides/hides-0.0.2-py3-none-any.whl/hides/__init__.py
@@ -0,0 +1,8 @@
+__author__="Atharva Bhandvalkar"
+__email__="<atharv.bhandvalkar@gmail.com>"
+
+__license__="MIT"
+__name__="hides"
+__version__="0.0.2"
+__title__="hides"
+__description__="Tool to scan encript and sign files"
