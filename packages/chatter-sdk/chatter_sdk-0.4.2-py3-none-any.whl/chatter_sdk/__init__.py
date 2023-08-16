02d261d775046329a699f0785bcc58bdebf72424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 03:08:27 +0000

    Add package chatter-sdk/0.4.2/chatter_sdk-0.4.2-py3-none-any.whl

---
+++ packages/chatter-sdk/chatter_sdk-0.4.2-py3-none-any.whl/chatter_sdk/__init__.py
@@ -0,0 +1,18 @@
+from .chatter import chatter
+
+
+def set_api_key(key: str):
+    chatter.api_key = key
+
+
+def get_api_key():
+    return chatter.api_key
+
+
+api_key = property(get_api_key, set_api_key)
+
+
+def render_prompt(id: str, variables: dict, debug=False):
+    return chatter.render_prompt(id, variables, debug)
+
+
