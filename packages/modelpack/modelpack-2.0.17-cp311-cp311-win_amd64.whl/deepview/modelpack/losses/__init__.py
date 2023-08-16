b8b194b29dedbb1f2e9fd8810e1d37800724f703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 15:10:11 +0000

    Add package modelpack/2.0.17/modelpack-2.0.17-cp311-cp311-win_amd64.whl

---
+++ packages/modelpack/modelpack-2.0.17-cp311-cp311-win_amd64.whl/deepview/modelpack/losses/__init__.py
@@ -0,0 +1,12 @@
+# Copyright 2022 by Au-Zone Technologies.  All Rights Reserved.
+#
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential.
+#
+# This source code is provided solely for runtime interpretation by Python.
+# Modifying or copying any source code is explicitly forbidden.
+
+from deepview.modelpack.losses.core import \
+    ModelPackLoss
+from deepview.modelpack.losses.utils import \
+    bbox_iou, bbox_giou, bbox_generic_iou
