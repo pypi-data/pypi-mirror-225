6f8184e255f0fe038f40a53246561cade5bd0fa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 04:16:01 +0000

    Add package csle-cluster/0.2.20/csle_cluster-0.2.20.tar.gz

---
+++ packages/csle-cluster/csle_cluster-0.2.20.tar.gz/csle_cluster-0.2.20/tests/test_cluster_manager.py
@@ -0,0 +1,14 @@
+
+
+class TestClusterManagerSuite:
+    """
+    Test suite for cluster_manager.py
+    """
+
+    def test_get_cluster_manager_status(self) -> None:
+        """
+        Tests the _get_cluster_manager_status function
+
+        :return: None
+        """
+        assert True
