bd7ae9e991d6674327cd1166a77d7d04f3f01f58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 03:07:49 +0000

    Add package opensr-degradation/0.1.3/opensr_degradation-0.1.3.tar.gz

---
+++ packages/opensr-degradation/opensr_degradation-0.1.3.tar.gz/opensr_degradation-0.1.3/opensr_degradation/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for opensr_degradation."""
+
+__author__ = """Cesar Aybar"""
+__email__ = "csaybar@gmail.com"
+__version__ = "0.1.3"
+
+
+from opensr_degradation.main import blur_model, reflectance_model, noise_model
---
+++ packages/opensr-degradation/opensr_degradation-0.1.3.tar.gz/opensr_degradation-0.1.3/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import find_packages, setup
+
+with open("README.rst") as readme_file:
+    readme = readme_file.read()
+
+with open("HISTORY.rst") as history_file:
+    history = history_file.read()
+
+requirements = ["torch", "numpy", "sklearn", "scikit-image"]
+
+test_requirements = []
+
+setup(
+    author="Cesar Aybar",
+    author_email="csaybar@gmail.com",
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    description="A real-world HR degradation",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + "\n\n" + history,
+    include_package_data=True,
+    keywords="opensr_degradation",
+    name="opensr_degradation",
+    packages=find_packages(include=["opensr_degradation", "opensr_degradation.*"]),
+    test_suite="tests",
+    tests_require=test_requirements,
+    url="https://github.com/csaybar/opensr_degradation",
+    data_files=[("models", ["models/model_reflectance.pt", "models/model_noise.pt"])],
+    version="0.1.3",
+    zip_safe=False,
+)
