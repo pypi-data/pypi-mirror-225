116c75d833ad33d9f6da58196e7368cb903012a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 03:10:29 +0000

    Add package rggrader/0.1.1/rggrader-0.1.1.tar.gz

---
+++ packages/rggrader/rggrader-0.1.1.tar.gz/rggrader-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rggrader',
+    version='0.1.1',
+    description='Submisiion tool for REA course',
+    author='Aditira Jamhuri',
+    author_email='aditira.jamhuri@ruangguru.com',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
