dcb338ff5299be665eb5c3475c0f9dd02fdb2e5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 03:11:12 +0000

    Add package sync-env/1.0.2/sync_env-1.0.2.tar.gz

---
+++ packages/sync-env/sync_env-1.0.2.tar.gz/sync_env-1.0.2/batch/upload_test.bat
@@ -0,0 +1,3 @@
+call build.bat
+
+py -m twine upload --repository testpypi dist/* --config-file .pypirc
---
+++ packages/sync-env/sync_env-1.0.2.tar.gz/sync_env-1.0.2/tests/module/init.bat
@@ -0,0 +1,5 @@
+
+py -3.11 -m venv venv
+venv\Scripts\activate.bat
+
+pip install -i https://test.pypi.org/simple/ sync-env
