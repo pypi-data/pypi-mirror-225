26e393c6cd880d718521666fa7c8f31ae3b74537
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 04:13:15 +0000

    Add package fusion-engine/1.1.1/fusion_engine-1.1.1-cp311-cp311-macosx_13_0_x86_64.whl

---
+++ packages/fusion-engine/fusion_engine-1.1.1-cp311-cp311-macosx_13_0_x86_64.whl/fusionengine/__init__.py
@@ -0,0 +1,33 @@
+__author__ = "Dimkauzh"
+__version__ = "1.1.1"
+
+from fusionengine.files.imports import *
+import fusionengine.files.systems as sysconfig
+
+
+class Main:
+    def __init__(self, *args) -> None:
+        """A class that contains all the functions for the engine."""
+        pg.init()
+
+        self.window = window.Window()
+        self.color = color.Colors()
+        self.colortools = color.ColorTools()
+        self.event = event.Event()
+        self.keys = event.Keys()
+        self.draw = draw.Draw()
+        self.image = image.Image()
+        self.body = body
+        self.system = sysconfig.System()
+        self.shape = shape.Shapes()
+        self.ui = ui.UI()
+        self.fonts = fonts.Fonts()
+        self.debug = debug.DebugFiles()
+        self.vector = vector.Vectors()
+
+        if args != ("message=False",):
+            python_version = sys.version.split()[0]
+            print(
+                f"Fusion Engine {__version__} (Pygame-ce {pg.version.ver}, Python {python_version})"
+            )
+            print("Welcome to Fusion Engine! https://github.com/dimkauzh/fusion-engine")
