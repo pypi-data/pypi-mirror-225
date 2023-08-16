2cafc9e8b93587bfb053ab9d2d65cdd5614de288
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 16:12:01 +0000

    Add package aiogitignore/1.0.1/aiogitignore-1.0.1-py3-none-any.whl

---
+++ packages/aiogitignore/aiogitignore-1.0.1-py3-none-any.whl/core/help.py
@@ -0,0 +1,9 @@
+VERSION = "1.0.1"
+
+def help() -> None:
+    """Prints the help message.
+    """
+    print("Usage: python -m gitignore [OPTIONS] <ARGS>...")
+    print("Options:")
+    print("  -h, --help\t\t\tPrints this help message.")
+    print("  -v, --version\t\t\tPrints the version of the program.")
