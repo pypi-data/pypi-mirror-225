428d58c02d3686bda6929e0eb92e176bc7efc2e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 15:11:37 +0000

    Add package openssm-dev/0.1.6.12/openssm_dev-0.1.6.12-py3-none-any.whl

---
+++ packages/openssm-dev/openssm_dev-0.1.6.12-py3-none-any.whl/openssm/integrations/openai/ssm.py
@@ -0,0 +1,32 @@
+"""OpenAI-based SSMs."""
+
+
+from openssm.core.ssm.base_ssm import BaseSSM
+from openssm.core.adapter.abstract_adapter import AbstractAdapter
+from openssm.core.backend.abstract_backend import AbstractBackend
+from openssm.integrations.openai.slm import GPT3CompletionSLM, GPT3ChatCompletionSLM, GPT4ChatCompletionSLM
+
+
+class GPT3CompletionSSM(BaseSSM):
+    """GPT3-based Completion SSMs."""
+
+    def __init__(self,
+                 adapter: AbstractAdapter = None,
+                 backends: list[AbstractBackend] = None):
+        super().__init__(GPT3CompletionSLM(), adapter, backends)
+
+
+class GPT3ChatCompletionSSM(BaseSSM):
+    """GPT3-based Chat Completion SSMs."""
+
+    def __init__(self,
+                 adapter: AbstractAdapter = None,
+                 backends: list[AbstractBackend] = None):
+        super().__init__(GPT3ChatCompletionSLM(), adapter, backends)
+
+
+class GPT4ChatCompletionSSM(BaseSSM):
+    def __init__(self,
+                 adapter: AbstractAdapter = None,
+                 backends: list[AbstractBackend] = None):
+        super().__init__(GPT4ChatCompletionSLM(), adapter, backends)
