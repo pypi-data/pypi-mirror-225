107fb0a165f26dc1b6f80a9db7bd662ffaa9fd43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 15:10:26 +0000

    Add package hnu-quant-db/0.0.13/hnu_quant_db-0.0.13.tar.gz

---
+++ packages/hnu-quant-db/hnu_quant_db-0.0.13.tar.gz/hnu_quant_db-0.0.13/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hnu_quant_db',
+    version='0.0.13',
+    description='HNU Quant Association Database Query Tool',
+    author='rikkaka',
+    author_email='793329010@qq.com',
+    
+    packages=find_packages(),
+    
+    exclude_package_data={
+        '': ['*.pyc', '*.pyo', '*.pyd'],
+        'private': ["token"]
+    },
+    
+    install_requires=[
+        'pandas >= 2.0',
+        'sqlalchemy >= 2.0',
+        'psycopg2-binary >= 2.9',
+    ],
+    
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
+    
