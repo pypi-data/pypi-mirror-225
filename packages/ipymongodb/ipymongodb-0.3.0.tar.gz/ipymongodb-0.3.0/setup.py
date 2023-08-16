f06db056f75d6a6def4cd6db45b49ea4fe3da7c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 03:08:30 +0000

    Add package ipymongodb/0.3.0/ipymongodb-0.3.0.tar.gz

---
+++ packages/ipymongodb/ipymongodb-0.3.0.tar.gz/ipymongodb-0.3.0/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ipymongodb",
+    version="0.3.0",
+    author="innovata sambong",
+    author_email="iinnovata@gmail.com",
+    description='pymongo 패키지 wrapper',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/innovata/iPyMongo",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"":"src"},
+    packages=setuptools.find_packages("src"),
+    python_requires=">=3.8",
+    install_requires=['pymongo', 'pandas', 'ipylib'],
+)
