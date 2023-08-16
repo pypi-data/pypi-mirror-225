a7dd37d6b67bdcef04c3ffc4da8137c5aa634a33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 14:10:08 +0000

    Add package proconip/1.3.0/proconip-1.3.0.tar.gz

---
+++ packages/proconip/proconip-1.3.0.tar.gz/proconip-1.3.0/src/setup.py
@@ -0,0 +1,10 @@
+"""Simple setup script for proconip package."""
+
+from setuptools import setup, find_packages
+
+install_requires = [
+    "aiohttp>=3.8",
+    "yarl>=1.8",
+]
+
+setup(name="proconip", version="1.3.0", packages=find_packages())
