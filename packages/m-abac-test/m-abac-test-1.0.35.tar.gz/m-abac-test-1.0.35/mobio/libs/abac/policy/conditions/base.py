ac9adfcbafa237d22c1a7b0b5ea236ecdb217f81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 16:12:01 +0000

    Add package m-abac-test/1.0.35/m-abac-test-1.0.35.tar.gz

---
+++ packages/m-abac-test/m-abac-test-1.0.35.tar.gz/m-abac-test-1.0.35/mobio/libs/abac/policy/conditions/base.py
@@ -0,0 +1,29 @@
+"""
+    Operation base class
+"""
+
+from abc import ABCMeta, abstractmethod
+
+# from py_abac.context import EvaluationContext
+# what is value of field, values is list value policy
+MapAnyValue = ""
+
+class ConditionBase(metaclass=ABCMeta):
+    """
+        Base class for conditions
+    """
+
+    # @abstractmethod
+    # def is_satisfied(self, ctx: EvaluationContext) -> bool:
+    #     """
+    #         Is conditions satisfied?
+    #
+    #         :param ctx: evaluation context
+    #         :return: True if satisfied else False
+    #     """
+    #     raise NotImplementedError()
+
+    class Qualifier:
+        ForAnyValue = "ForAnyValue"
+        ForAllValues = "ForAllValues"
+        ALL = [ForAnyValue, ForAllValues]
---
+++ packages/m-abac-test/m-abac-test-1.0.35.tar.gz/m-abac-test-1.0.35/mobio/libs/abac/policy/conditions/collection/base.py
@@ -0,0 +1,59 @@
+"""
+    Collection conditions base class
+"""
+
+import logging
+
+from marshmallow import Schema, fields, EXCLUDE
+
+from ..base import ConditionBase, ABCMeta, abstractmethod
+
+LOG = logging.getLogger(__name__)
+
+
+def is_collection(value) -> bool:
+    """
+        Check if value is a collection
+    """
+    return any([isinstance(value, list), isinstance(value, set), isinstance(value, tuple)])
+
+
+class CollectionCondition(ConditionBase, metaclass=ABCMeta):
+    """
+        Base class for collection conditions
+
+        :param values: collection of values to compare during policy evaluation
+    """
+
+    def __init__(self, values, what, **kwargs):
+        self.values = values
+        self.what = what
+
+
+    @abstractmethod
+    def _is_satisfied(self) -> bool:
+        """
+            Is collection conditions satisfied
+
+            :param what: collection to check
+            :return: True if satisfied else False
+        """
+        raise NotImplementedError()
+
+
+class CollectionConditionSchema(Schema):
+    """
+        Base JSON schema class for collection conditions
+    """
+    values = fields.List(
+        fields.Raw(required=True, allow_none=False),
+        required=True,
+        allow_none=False
+    )
+    what = fields.List(
+        fields.Raw(allow_none=False),
+        allow_none=False
+    )
+
+    class Meta:
+        unknown = EXCLUDE
