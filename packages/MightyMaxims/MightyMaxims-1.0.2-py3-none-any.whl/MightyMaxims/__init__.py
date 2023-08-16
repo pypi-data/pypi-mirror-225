8181a8e12a8581368f5e4e1fbdf00c04dba72134
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 14:10:31 +0000

    Add package MightyMaxims/1.0.2/MightyMaxims-1.0.2-py3-none-any.whl

---
+++ packages/MightyMaxims/MightyMaxims-1.0.2-py3-none-any.whl/MightyMaxims/__init__.py
@@ -0,0 +1,17 @@
+'''
+Might Maxims: Short & Memorable Quotations.
+Project: https://github.com/soft9000/DoctorQuote
+Paperback: https://www.amazon.com/dp/B09H9DV8KV
+Group: https://www.facebook.com/groups/mightymaxims
+
+To use:
+
+>>> import MightyMaxims.GetRandom as app
+
+>>> app.get_random()
+
+'''
+
+import MightyMaxims.GetRandom as app
+
+app.get_random()
