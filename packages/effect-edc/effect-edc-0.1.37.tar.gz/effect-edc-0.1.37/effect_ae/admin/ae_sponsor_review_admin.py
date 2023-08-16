94764186afce84d1932824d5f567ee75e9892fa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 15:10:17 +0000

    Add package effect-edc/0.1.37/effect-edc-0.1.37.tar.gz

---
+++ packages/effect-edc/effect-edc-0.1.37.tar.gz/effect-edc-0.1.37/effect_ae/admin/ae_sponsor_review_admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from edc_model_admin.history import SimpleHistoryAdmin
+
+from ..admin_site import effect_ae_admin
+from ..forms import AeSponsorReviewForm
+from ..models import AeSponsorReview
+from .modeladmin_mixins import AeReviewModelAdminMixin
+
+
+@admin.register(AeSponsorReview, site=effect_ae_admin)
+class AeSponsorReviewAdmin(AeReviewModelAdminMixin, SimpleHistoryAdmin):
+    form = AeSponsorReviewForm
