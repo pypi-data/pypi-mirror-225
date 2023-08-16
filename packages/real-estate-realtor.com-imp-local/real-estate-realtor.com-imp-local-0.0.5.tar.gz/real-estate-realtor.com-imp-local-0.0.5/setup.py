cf1882e952d3421c0b8000d2069b371226205ca6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 16:11:40 +0000

    Add package real-estate-realtor.com-imp-local/0.0.5/real-estate-realtor.com-imp-local-0.0.5.tar.gz

---
+++ packages/real-estate-realtor.com-imp-local/real-estate-realtor.com-imp-local-0.0.5.tar.gz/real-estate-realtor.com-imp-local-0.0.5/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+# used by python -m build
+# python -m build needs pyproject.toml or setup.py
+# The need of setup.py is changing as of poetry 1.1.0 (including current pre-release) as we have moved away from needing to generate a setup.py file to enable editable installs - We might able to delete this file in the near future
+setuptools.setup(
+     # TODO: Please update the name and delete this line i.e. XXX-local or XXX-remote (without the -python-package suffix). Only lower case
+     name='real-estate-realtor.com-imp-local',  
+     version='0.0.5',
+     author="Circles",
+     author_email="info@circles.life",
+     # TODO: Please update the description and delete this line
+     description="PyPI Package for Circles <project-name> Local/Remote Python",
+     # TODO: Please update the long description and delete this line    
+     long_description="This is a package for sharing common XXX function used in different repositories",
+     long_description_content_type="text/markdown",
+     url="https://github.com/circles",
+
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: Other/Proprietary License",
+         "Operating System :: OS Independent",
+     ],
+ )
