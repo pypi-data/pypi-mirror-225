000229552cf53afcd939cc423f166b5b73e90c77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 03:09:05 +0000

    Add package ecs-files-composer/1.4.0/ecs_files_composer-1.4.0.tar.gz

---
+++ packages/ecs-files-composer/ecs_files_composer-1.4.0.tar.gz/ecs_files_composer-1.4.0/ecs_files_composer/ecs_files_composer.py
@@ -0,0 +1,156 @@
+# SPDX-License-Identifier: MPL-2.0
+# Copyright 2020-2022 John Mille<john@compose-x.io>
+
+"""Main module."""
+
+from __future__ import annotations
+
+import os
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .input import Model
+
+import json
+from os import environ, path
+from tempfile import TemporaryDirectory
+
+import yaml
+from yaml import Loader
+
+from ecs_files_composer import input
+from ecs_files_composer.aws_mgmt import S3Fetcher
+from ecs_files_composer.certbot_aws_store import process_certbot_aws_store_certificates
+from ecs_files_composer.certificates_mgmt import process_x509_certs
+from ecs_files_composer.common import LOG
+from ecs_files_composer.files_mgmt import File
+
+
+def init_config(
+    raw=None,
+    file_path=None,
+    env_var=None,
+    ssm_parameter=None,
+    s3_config=None,
+    secret_config=None,
+    role_arn=None,
+    external_id=None,
+    decode_base64=False,
+    context=None,
+    override_folder: str = None,
+):
+    """
+    Function to initialize the configuration
+
+    :param raw: The raw content of a content
+    :param str file_path: The path to a job configuration file
+    :param str env_var:
+    :param str ssm_parameter:
+    :param str s3_config:
+    :param str secret_config:
+    :param str role_arn:
+    :param str external_id:
+    :param bool decode_base64:
+    :return: The ECS Config description
+    :rtype: dict
+    """
+    iam_override = {"SessionName": "FilesComposerInit"}
+    if ssm_parameter or s3_config or secret_config:
+        role_arn = environ.get("CONFIG_IAM_ROLE_ARN", role_arn)
+        external_id = environ.get("CONFIG_IAM_EXTERNAL_ID", external_id)
+        if role_arn:
+            iam_override.update({"RoleArn": role_arn})
+        if external_id:
+            iam_override.update({"ExternalId": external_id})
+    if ssm_parameter:
+        initial_config = {"source": {"Ssm": {"ParameterName": ssm_parameter}}}
+    elif s3_config:
+        if not S3Fetcher.bucket_re.match(s3_config):
+            raise ValueError(
+                "The value for S3 URI is not valid.",
+                s3_config,
+                "Expected to match",
+                S3Fetcher.bucket_re.pattern,
+            )
+        initial_config = {
+            "source": {
+                "S3": {
+                    "BucketName": S3Fetcher.bucket_re.match(s3_config).group("bucket"),
+                    "Key": S3Fetcher.bucket_re.match(s3_config).group("key"),
+                }
+            }
+        }
+    elif secret_config:
+        initial_config = {"source": {"Secret": {"SecretId": secret_config}}}
+    elif file_path:
+        with open(path.abspath(file_path)) as file_fd:
+            config_content = file_fd.read()
+        initial_config = {"content": config_content}
+    elif raw:
+        initial_config = {"content": raw}
+    elif env_var:
+        initial_config = {"content": environ.get(env_var, None)}
+    else:
+        raise ValueError("No input source was provided")
+    if not initial_config:
+        raise ImportError("Failed to import a configuration content")
+    LOG.debug(initial_config)
+    if not override_folder:
+        temp_dir = TemporaryDirectory()
+        config_path = f"{temp_dir.name}/init.conf"
+    else:
+        config_path = f"{override_folder}/init.conf"
+    jobs_input_def = {
+        "files": {config_path: initial_config},
+        "IamOverride": iam_override,
+    }
+    if decode_base64:
+        initial_config["encoding"] = "base64"
+    if context:
+        initial_config["context"] = context
+    start_jobs(jobs_input_def)
+    try:
+        with open(config_path) as config_fd:
+            try:
+                config = yaml.load(config_fd.read(), Loader=Loader)
+                LOG.info(f"Successfully loaded YAML config {config_path}")
+                return config
+            except yaml.YAMLError:
+                config = json.loads(config_fd.read())
+                LOG.info(f"Successfully loaded JSON config {config_path}")
+                return config
+            except Exception:
+                LOG.error("Input content is neither JSON nor YAML formatted")
+                raise
+    except OSError as error:
+        LOG.exception(error)
+        LOG.error(f"Failed to read input file from {config_path}")
+
+
+def process_files(job: Model, override_session=None) -> None:
+    files: list = []
+    for file_path, file in job.files.items():
+        config = json.loads(file.json(by_alias=True))
+        file_redef = File(**config)
+        file_redef.path = file_path
+        files.append(file_redef)
+    for file in files:
+        file.handler(job.iam_override, override_session)
+        LOG.info(f"Tasks for {file.path} completed.")
+
+
+def start_jobs(config: dict, override_session=None):
+    """
+    Starting point to run the files job
+
+    :param config:
+    :param override_session:
+    :return:
+    """
+    job = input.Model(**config)
+    if job.certificates:
+        process_x509_certs(job)
+    if job.certbot_store:
+        process_certbot_aws_store_certificates(job)
+    if job.files:
+        process_files(job, override_session)
---
+++ packages/ecs-files-composer/ecs_files_composer-1.4.0.tar.gz/ecs_files_composer-1.4.0/ecs_files_composer/jinja2_filters/__init__.py
@@ -0,0 +1,235 @@
+# SPDX-License-Identifier: MPL-2.0
+# Copyright 2020-2022 John Mille<john@compose-x.io>
+
+"""
+Package allowing to expand the Jinja filters to use.
+"""
+import json
+import re
+from base64 import b64decode, b64encode
+from os import environ
+
+import requests
+import yaml
+from aws_cfn_custom_resource_resolve_parser import handle
+from boto3.session import Session
+from flatdict import FlatDict, FlatterDict
+
+from .aws_filters import msk_bootstrap
+
+
+def env_override(value, key):
+    """
+    Function to use in new Jinja filter
+    :param value:
+    :param key:
+    :return:
+    """
+    return environ.get(key, value)
+
+
+def define_metadata(for_task=False):
+    meta_v4 = "ECS_CONTAINER_METADATA_URI_V4"
+    meta_v3 = "ECS_CONTAINER_METADATA_URI"
+
+    if environ.get(meta_v3, None):
+        meta_url = environ.get(meta_v3)
+    elif environ.get(meta_v4, None):
+        meta_url = environ.get(meta_v4)
+    else:
+        raise OSError("No ECS Metadata URL provided. This filter only works on ECS")
+    if for_task:
+        return requests.get(f"{meta_url}/task")
+    else:
+        return requests.get(meta_url)
+
+
+def from_list_to_dict(top_key, new_mapping, to_convert):
+    """
+
+    :param str top_key:
+    :param dict new_mapping:
+    :param list to_convert:
+    :return:
+    """
+    for count, item in enumerate(to_convert):
+        list_key = f"{top_key}_{count}"
+        if isinstance(item, dict):
+            from_dict_to_simple_keys(list_key, new_mapping, item)
+        elif isinstance(item, list):
+            from_list_to_dict(list_key, new_mapping, item)
+
+
+def from_dict_to_simple_keys(top_key, new_mapping, to_convert):
+    """
+
+    :param str top_key:
+    :param dict new_mapping:
+    :param dict to_convert:
+    :return:
+    """
+    for key, value in to_convert.items():
+        if isinstance(value, str):
+            new_mapping[f"{top_key}_{key}"] = value
+        elif isinstance(value, dict):
+            from_dict_to_simple_keys(f"{top_key}_{key}", new_mapping, value)
+        elif isinstance(value, list):
+            from_list_to_dict(f"{top_key}_{key}", new_mapping, value)
+
+
+def from_metadata_to_flat_keys(metadata):
+    """
+    Function to transform the metadata into simplified structure
+    :param dict metadata:
+    :return:
+    """
+    new_metadata = {}
+    for key, value in metadata.items():
+        if isinstance(value, str):
+            new_metadata[key] = value
+        elif isinstance(value, dict):
+            from_dict_to_simple_keys(key, new_metadata, value)
+        elif isinstance(value, list):
+            from_list_to_dict(key, new_metadata, value)
+    return new_metadata
+
+
+def get_property(metadata, property_key, separator: str = None):
+    if separator is None:
+        separator = r"::"
+    metadata_mapping = FlatterDict(metadata)
+    metadata_mapping.set_delimiter(separator)
+    if property_key in metadata_mapping:
+        return metadata_mapping[property_key]
+
+    metadata_mapping = from_metadata_to_flat_keys(metadata)
+    property_re = re.compile(property_key)
+    for key, value in metadata_mapping.items():
+        if property_re.findall(key):
+            return value
+    return None
+
+
+def ecs_container_metadata(property_key=None, fallback_value=None):
+    metadata_raw = define_metadata()
+    metadata = metadata_raw.json()
+    if property_key:
+        value = get_property(metadata, property_key)
+        if value is None:
+            print(f"No task property found matching {property_key}")
+            return fallback_value
+        return value
+    return metadata
+
+
+def ecs_task_metadata(property_key=None, fallback_value=None):
+    metadata_raw = define_metadata(for_task=True)
+    metadata = metadata_raw.json()
+    if property_key:
+        value = get_property(metadata, property_key)
+        if value is None:
+            print(f"No task property found matching {property_key}")
+            return fallback_value
+        return value
+    return metadata
+
+
+def to_yaml(value):
+    """
+    Filter to render input to YAML formatted content
+    :return:
+    """
+    return yaml.dump(value, Dumper=yaml.Dumper)
+
+
+def to_json(value, indent=2):
+    return json.dumps(value, indent=indent)
+
+
+def base64encode(value: str):
+    """Return value base64 encoded"""
+    try:
+        return b64encode(value).decode("utf-8")
+    except TypeError:
+        return b64encode(value.encode("utf-8")).decode("utf-8")
+
+
+def base64decode(value) -> bytes:
+    """Decodes base64 encoded value"""
+    return b64decode(value)
+
+
+def env_var(key, value=None):
+    return environ.get(key, value)
+
+
+def from_ssm(parameter_name: str) -> str:
+    """
+    Function to retrieve an SSM parameter value
+
+    :param parameter_name: Name of the parameter
+    """
+    return (
+        Session()
+        .client("ssm")
+        .get_parameter(Name=parameter_name, WithDecryption=True)["Parameter"]["Value"]
+    )
+
+
+def from_ssm_json(parameter_name: str) -> dict:
+    """
+    Function to retrieve an SSM parameter value
+
+    :param parameter_name: Name of the parameter
+    """
+    value_str = (
+        Session()
+        .client("ssm")
+        .get_parameter(Name=parameter_name, WithDecryption=True)["Parameter"]["Value"]
+    )
+    try:
+        return json.loads(value_str)
+    except json.JSONDecodeError:
+        return {}
+
+
+def hostname(alternative_value: str = None) -> str:
+    try:
+        import platform
+
+        return str(platform.node())
+    except Exception as error:
+        print("Error with platform", error)
+        try:
+            import socket
+
+            return str(socket.gethostname())
+        except Exception as error:
+            print("Error with socket", error)
+            pass
+    if alternative_value:
+        return alternative_value
+
+
+def using_resolve(resolve_string: str) -> str:
+    return handle(resolve_string)
+
+
+JINJA_FUNCTIONS = {
+    "ecs_container_metadata": ecs_container_metadata,
+    "ecs_task_metadata": ecs_task_metadata,
+    "env_var": env_var,
+    "from_ssm": from_ssm,
+    "from_ssm_json": from_ssm_json,
+    "from_resolve": using_resolve,
+    "msk_bootstrap": msk_bootstrap,
+    "hostname": hostname,
+}
+
+JINJA_FILTERS = {
+    "to_yaml": to_yaml,
+    "to_json": to_json,
+    "env_override": env_override,
+    "base64encode": base64encode,
+    "base64decode": base64decode,
+}
