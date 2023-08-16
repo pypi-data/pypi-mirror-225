4a9c87703f600d0e54bd1d76c4b5b1ed42a34f2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 16:11:40 +0000

    Add package mediafiregrabber/1.2/mediafiregrabber-1.2.tar.gz

---
+++ packages/mediafiregrabber/mediafiregrabber-1.2.tar.gz/mediafiregrabber-1.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mediafiregrabber",
+    version="1.2",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'mediafiregrabber = mediafiregrabber.cli:download'
+        ],
+    },
+    install_requires=[
+        "requests",
+        "beautifulsoup4",
+        "tqdm",
+        "urllib3",
+        "asyncio",
+        "aiohttp"
+    ],
+    author="Swargaraj Bhowmik",
+    author_email="contact@swargarajbhowmik.me",
+    description="Simple Python Package for MediaFire File Download and Information Retrieval",
+    license="MIT",
+    url="https://github.com/swargarajbhowmik/mediafiregrabber",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
