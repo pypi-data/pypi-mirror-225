723f6ce5001a6d12d08fa7408ca66def9de99d5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 15:11:41 +0000

    Add package odp-sdk-python-ingest/0.4.7/odp_sdk_python_ingest-0.4.7-py3-none-any.whl

---
+++ packages/odp-sdk-python-ingest/odp_sdk_python_ingest-0.4.7-py3-none-any.whl/odp/compute/blocks/azure-key-vault.py
@@ -0,0 +1,40 @@
+import re
+from typing import Optional
+
+from azure.identity import ClientSecretCredential
+from azure.keyvault.keys import KeyClient
+from prefect.blocks.core import Block
+from pydantic import AnyHttpUrl
+
+from odp.compute.blocks.aad_client_credentials import AadClientCredentials
+
+
+class AzureKeyVault(Block):
+    """Azure Key Vault block that allows users to retrieve secrets from an Azure Key Vault."""
+
+    _block_type_name = "Azure Key Vault"
+    _logo_url = "https://stprododpcmscdnendpoint.azureedge.net/assets/icons/azure-storage.png"  # type: ignore
+
+    aad_client_credentials: AadClientCredentials
+    key_vault_url: AnyHttpUrl
+    client: Optional[KeyClient]
+
+    def block_initialization(self) -> None:
+        """Validate the block setup and create an authenticated client for the key vault."""
+
+        try:
+            tenant_id = re.search(
+                "https://login.microsoftonline.com/(.+?)/oauth2/v2.0/token", self.aad_client_credentials.token_url
+            ).group(1)
+        except AttributeError:
+            raise ValueError("Invalid token URL for the AadClientCredentials Block.")
+        azure_creds = ClientSecretCredential(
+            tenant_id=tenant_id,
+            client_id=self.aad_client_credentials.client_id.get_secret_value(),
+            client_secret=self.aad_client_credentials.client_secret.get_secret_value(),
+        )
+        self.client = KeyClient(self.key_vault_url, azure_creds)
+
+    def get_key(self, key_name: str):
+        """Return key from Azure Key Vault."""
+        return self.client.get_key(key_name)
