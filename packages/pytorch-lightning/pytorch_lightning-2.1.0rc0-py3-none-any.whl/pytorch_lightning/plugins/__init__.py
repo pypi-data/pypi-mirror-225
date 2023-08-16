1b9b96bc6773156f65b8d1fef55167a112b36ba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 16:11:40 +0000

    Add package pytorch-lightning/2.1.0rc0/pytorch_lightning-2.1.0rc0-py3-none-any.whl

---
+++ packages/pytorch-lightning/pytorch_lightning-2.1.0rc0-py3-none-any.whl/pytorch_lightning/plugins/__init__.py
@@ -0,0 +1,32 @@
+from typing import Union
+
+from lightning_fabric.plugins import CheckpointIO, ClusterEnvironment, TorchCheckpointIO, XLACheckpointIO
+from pytorch_lightning.plugins.io.async_plugin import AsyncCheckpointIO
+from pytorch_lightning.plugins.layer_sync import LayerSync, TorchSyncBatchNorm
+from pytorch_lightning.plugins.precision.amp import MixedPrecisionPlugin
+from pytorch_lightning.plugins.precision.deepspeed import DeepSpeedPrecisionPlugin
+from pytorch_lightning.plugins.precision.double import DoublePrecisionPlugin
+from pytorch_lightning.plugins.precision.fsdp import FSDPMixedPrecisionPlugin, FSDPPrecisionPlugin
+from pytorch_lightning.plugins.precision.half import HalfPrecisionPlugin
+from pytorch_lightning.plugins.precision.precision_plugin import PrecisionPlugin
+from pytorch_lightning.plugins.precision.xla import XLAPrecisionPlugin
+
+PLUGIN = Union[PrecisionPlugin, ClusterEnvironment, CheckpointIO, LayerSync]
+PLUGIN_INPUT = Union[PLUGIN, str]
+
+__all__ = [
+    "AsyncCheckpointIO",
+    "CheckpointIO",
+    "TorchCheckpointIO",
+    "XLACheckpointIO",
+    "DeepSpeedPrecisionPlugin",
+    "DoublePrecisionPlugin",
+    "HalfPrecisionPlugin",
+    "MixedPrecisionPlugin",
+    "PrecisionPlugin",
+    "FSDPMixedPrecisionPlugin",
+    "FSDPPrecisionPlugin",
+    "XLAPrecisionPlugin",
+    "LayerSync",
+    "TorchSyncBatchNorm",
+]
