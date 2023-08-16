bd1c48504c11b436b9ce1bc9574a5c2caaa7b4f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 14:08:00 +0000

    Add package nusuGraph/1.1/nusuGraph-1.1.tar.gz

---
+++ packages/nusuGraph/nusuGraph-1.1.tar.gz/nusuGraph-1.1/setup.py
@@ -0,0 +1,54 @@
+"""
+@author: Nusab Taha
+@contact: https://nusab19.pages.dev/
+@license: MIT License, see LICENSE file
+
+Copyright (C) 2023 - Present
+"""
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+version = "1.1"
+
+
+with open("README.md") as f:
+    long_description = f.read()
+    long_description = "Will be added..."
+
+
+setup(
+    name="nusuGraph",
+    version=version,
+
+    author="Nusab Taha",
+
+    author_email="telegraph@Nusab Taha.pw",
+    url="https://github.com/Nusab19/nusuGraph",
+
+    description="Telegraph API wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    download_url="https://github.com/Nusab19/nusuGraph/archive/v{}.zip".format(
+        version
+    ),
+    license="MIT",
+
+    packages=["nusugraph"],
+    install_requires=["httpx"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ]
+)
