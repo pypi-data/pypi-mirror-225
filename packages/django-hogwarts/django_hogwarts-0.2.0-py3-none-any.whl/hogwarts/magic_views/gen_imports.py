2c3979d7b56c5ad70e47ea5d5cc8345c65b5a4c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 04:15:09 +0000

    Add package django-hogwarts/0.2.0/django_hogwarts-0.2.0-py3-none-any.whl

---
+++ packages/django-hogwarts/django_hogwarts-0.2.0-py3-none-any.whl/hogwarts/magic_views/gen_imports.py
@@ -0,0 +1,55 @@
+from typing import Tuple
+
+Imports = list[Tuple[str, str]]
+
+
+class ImportsGenerator:
+    def __init__(self):
+        self.imports: Imports = []
+
+    def add(self, module, obj):
+        if not self.exists(obj):
+            self.imports.append((module, obj))
+
+    def exists(self, obj: str):
+        return any(i[1] == obj for i in self.imports)
+
+    def add_bulk(self, module, objs: list[str]):
+        for obj in objs:
+            self.add(module, obj)
+
+    def gen(self):
+        merged_imports = self.get_merge_imports()
+        result = ""
+        for module, obj in merged_imports.items():
+            result += f"from {module} import {', '.join(obj)}\n"
+
+        return result
+
+    def get_merge_imports(self):
+        merged_imports = {}
+        for module, obj in self.imports:
+            if module not in merged_imports.keys():
+                merged_imports[module] = [obj]
+            else:
+                merged_imports[module].append(obj)
+        return merged_imports
+
+    @property
+    def imported_classes(self):
+        for _import in self.imports:
+            yield _import[1]
+
+
+class ViewImportsGenerator(ImportsGenerator):
+    def add_login_required(self):
+        self.add("django.contrib.auth.mixins", "LoginRequiredMixin")
+
+    def add_user_test(self):
+        self.add("django.contrib.auth.mixins", "UserPassesTestMixin")
+
+    def add_reverse(self):
+        self.add("django.shortcuts", "reverse")
+
+    def add_reverse_lazy(self):
+        self.add("django.shortcuts", "reverse_lazy")
