8e0cab5a0cf6f3ab752f19799b17d3fb3ba000c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 04:13:28 +0000

    Add package twpl/0.1.0/twpl-0.1.0.tar.gz

---
+++ packages/twpl/twpl-0.1.0.tar.gz/twpl-0.1.0/setup.py
@@ -0,0 +1,42 @@
+import setuptools
+from types import SimpleNamespace
+from twpl import __version__
+
+
+meta = SimpleNamespace(
+    __name__ = "twpl",
+    __version__ = __version__,
+    __author__ = "Kirill Grigorev",
+    __git_id__ = "LankyCyril",
+    __license__ = "GPLv3",
+)
+
+
+def get_readme(filename):
+    with open(filename) as readme_handle:
+        return readme_handle.read()
+
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name = meta.__name__,
+        version = meta.__version__,
+        packages = [meta.__name__],
+        url = f"https://github.com/{meta.__git_id__}/{meta.__name__}/",
+        author = meta.__author__,
+        license = meta.__license__,
+        zip_safe = True,
+        description = "Two-phase locking via lockfiles",
+        long_description = get_readme("readme.md"),
+        long_description_content_type = "text/markdown",
+        classifiers = [
+            "Development Status :: 3 - Alpha",
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+            "Intended Audience :: Developers",
+            "Topic :: Software Development",
+            "Topic :: System",
+        ],
+        python_requires = ">=3.7",
+        install_requires = ["filelock"],
+    )
