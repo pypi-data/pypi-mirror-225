ecce08c62cc74af22e7abf774a9fbaabed917504
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 15:10:11 +0000

    Add package llmx/0.0.9a0/llmx-0.0.9a0-py3-none-any.whl

---
+++ packages/llmx/llmx-0.0.9a0-py3-none-any.whl/llmx/datamodel.py
@@ -0,0 +1,55 @@
+from dataclasses import asdict
+from typing import Any, Optional, Union, List
+from pydantic.dataclasses import dataclass
+
+
+@dataclass
+class Message:
+    role: str
+    content: str
+
+    def __post_init__(self):
+        self._fields_dict = asdict(self)
+
+    def __getitem__(self, key: Union[str, int]) -> Any:
+        return self._fields_dict.get(key)
+
+    def to_dict(self):
+        return self._fields_dict
+
+
+@dataclass
+class TextGenerationConfig:
+    n: int = 1
+    temperature: float = 0.1
+    max_tokens: Union[int, None] = None
+    top_p: float = 1.0
+    top_k: int = 50
+    frequency_penalty: float = 0.0
+    presence_penalty: float = 0.0
+    provider: str = "openai"
+    model: Optional[str] = None
+    stop: Union[List[str], str, None] = None
+    use_cache: bool = True
+
+    def __post_init__(self):
+        self._fields_dict = asdict(self)
+
+    def __getitem__(self, key: Union[str, int]) -> Any:
+        return self._fields_dict.get(key)
+
+
+@dataclass
+class TextGenerationResponse:
+    """Response from a text generation"""
+
+    text: List[Message]
+    config: Any
+    logprobs: Optional[Any] = None
+    usage: Optional[Any] = None
+
+    def __post_init__(self):
+        self._fields_dict = asdict(self)
+
+    def __getitem__(self, key: Union[str, int]) -> Any:
+        return self._fields_dict.get(key)
---
+++ packages/llmx/llmx-0.0.9a0-py3-none-any.whl/llmx/version.py
@@ -0,0 +1,2 @@
+VERSION = "0.0.9a"
+APP_NAME = "llmx"
