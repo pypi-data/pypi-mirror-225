e11f544b07ee6cb3868aaf8b48917511c779a828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 14:08:51 +0000

    Add package ms-mint-app/0.3.1.2/ms-mint-app-0.3.1.2.tar.gz

---
+++ packages/ms-mint-app/ms-mint-app-0.3.1.2.tar.gz/ms-mint-app-0.3.1.2/setup.py
@@ -0,0 +1,59 @@
+from setuptools import setup, find_packages
+
+import versioneer
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+install_requires = [
+    "ms-mint",
+    "xlsxwriter",
+    "waitress",
+    "dash",
+    "dash_extensions",
+    "dash_bootstrap_components",
+    "flask_login",
+    "urllib3",
+    "dash_tabulator",
+    "dash_uploader",
+    "beautifulsoup4",
+    "dask[diskcache]",
+    "h5py",
+    "hdf5plugin",
+    "werkzeug==2.2.3"
+]
+
+
+config = {
+    "name": "ms-mint-app",
+    "version": versioneer.get_version(),
+    "cmdclass": versioneer.get_cmdclass(),
+    "description": "Metabolomics Integrator (Mint)",
+    "long_description": long_description,
+    "long_description_content_type": "text/markdown",
+    "author": "Soren Wacker",
+    "url": "https://github.com/LewisResearchGroup/ms-mint-app",
+    "author_email": "swacker@ucalgary.ca",
+    "scripts": ["scripts/Mint.py"],
+    "packages": find_packages(),
+    "data_files": [("scripts", ["scripts/Mint.py"])],
+    "classifiers": [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    "python_requires": ">=3.8",
+    "install_requires": install_requires,
+    "include_package_data": True,
+    "package_data": {
+        "ms_mint_app.static": [
+            "Standard_Peaklist.csv",
+            "ChEBI-Chem.parquet",
+            "ChEBI-Groups.parquet",
+        ]
+    },
+}
+
+setup(**config)
