df66f88ba49e9b37500e8a03815a2669c28fc98c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 04:13:25 +0000

    Add package cancerontogpt/0.2.14/cancerontogpt-0.2.14-py3-none-any.whl

---
+++ packages/cancerontogpt/cancerontogpt-0.2.14-py3-none-any.whl/cancerontogpt/engines/resolver.py
@@ -0,0 +1,22 @@
+"""Resolver engine."""
+from typing import Optional, Type, Union
+
+from class_resolver import ClassResolver
+
+from cancerontogpt.engines.enrichment import EnrichmentEngine
+from cancerontogpt.engines.halo_engine import HALOEngine
+from cancerontogpt.engines.knowledge_engine import KnowledgeEngine
+from cancerontogpt.engines.spires_engine import SPIRESEngine
+
+resolver = ClassResolver([SPIRESEngine, HALOEngine, EnrichmentEngine], base=KnowledgeEngine)
+
+
+def create_engine(
+    template: str, engine: Optional[Union[str, Type]] = None, **kwargs
+) -> KnowledgeEngine:
+    """Create a knowledge engine."""
+    if engine is None:
+        engine = SPIRESEngine
+    if isinstance(engine, str):
+        engine = resolver.get_class(engine)(**kwargs)
+    return engine(template, **kwargs)
