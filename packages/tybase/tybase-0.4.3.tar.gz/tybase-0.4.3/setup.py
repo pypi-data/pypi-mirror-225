6ee8f9b434e6b4e65c7558b6fd96b3464570eb44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 16:12:10 +0000

    Add package tybase/0.4.3/tybase-0.4.3.tar.gz

---
+++ packages/tybase/tybase-0.4.3.tar.gz/tybase-0.4.3/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tybase',
+    version='0.4.3',
+    include_package_data=True,
+    description='image_compressor, 图片简单的无损压缩方法',
+    long_description=open('README.md', 'r', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',  # 版本描述
+    author='Tuya',
+    author_email='353335447@qq.com',
+    url='https://github.com/yourusername/your_package',
+    packages=find_packages(),
+    install_requires=[
+        'setuptools',
+        "opencv-python",
+        "loguru",
+        "openpyxl",
+        'requests',
+        "python-dotenv",
+        "retrying",
+        "pymysql",
+        "mysql-connector-python",
+        "sqlalchemy",
+        "pandas",
+        "langchain",
+        "openai",
+        'python-dotenv',
+        "oss2"
+        ""
+
+        # List your package dependencies here
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
