01b9db80067c5aabaf23beacfd97336779385cd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 14:08:58 +0000

    Add package path4gmns/0.9.5/path4gmns-0.9.5-py3-none-any.whl

---
+++ packages/path4gmns/path4gmns-0.9.5-py3-none-any.whl/path4gmns/consts.py
@@ -0,0 +1,18 @@
+""" global constants """
+# for shortest path calculation
+MAX_LABEL_COST = 2147483647
+# in case where the divisor/denominator is ZERO
+# to do: combine SMALL_DIVISOR and MIN_OD_VOL
+SMALL_DIVISOR = 0.00001
+# for column generation
+MIN_OD_VOL = 0.000001
+# for accessibility evaluation
+MIN_TIME_BUDGET = 10
+MAX_TIME_BUDGET = 240
+BUDGET_TIME_INTVL = 5
+# unit
+MILE_TO_METER = 1609
+MPH_TO_KPH = 1.609
+# simulation
+SECONDS_IN_MINUTE = 60
+SECONDS_IN_HOUR = 3600
