c7a7e6d07215a3819396002f8bc48bb12e685866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 15:10:22 +0000

    Add package movefilestd/0.0.1/movefilestd-0.0.1.tar.gz

---
+++ packages/movefilestd/movefilestd-0.0.1.tar.gz/movefilestd-0.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+VERSION = '0.0.1'
+DESCRIPTION = 'task_1 from gnanendra'
+LONG_DESCRIPTION = 'A package that allows to build a basic file moving'
+
+# Setting up
+setup(
+    name="movefilestd",
+    version=VERSION,
+    author="Subhajit",
+    author_email="singha111subhajit@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python',],
+    url='https://github.com/singha111subhajit/movefilestd.git',
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
