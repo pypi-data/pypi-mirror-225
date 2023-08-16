5211ef75c68158abc0dab36996dff25a6f57a7e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 03:08:30 +0000

    Add package rtsf/2.9.6/rtsf-2.9.6-py2.py3-none-any.whl

---
+++ packages/rtsf/rtsf-2.9.6-py2.py3-none-any.whl/rtsf/__about__.py
@@ -0,0 +1,11 @@
+#! python3
+# -*- encoding: utf-8 -*-
+
+__title__ = "rtsf"
+__short_desc__ = "rock4 test service framework"
+__version__ = "2.9.6"
+__autor__ = u"罗科峰"
+__author_email__ = "lkf20031988@163.com"
+__license__ = 'MIT'
+
+HOME_PAGE = "https://github.com/RockFeng0/rtsf"
