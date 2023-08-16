079b22521f933e6476892d58236e4a8ee5c05c12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 04:15:20 +0000

    Add package trendup-storage/0.0.3/trendup_storage-0.0.3.tar.gz

---
+++ packages/trendup-storage/trendup_storage-0.0.3.tar.gz/trendup_storage-0.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="trendup_storage",
+    version="0.0.3",
+
+    author="JerryLin",
+    author_email="jerry.lin@keeptossinglab.com",
+    packages=find_packages(where="trendup_storage"),
+    package_dir={"": "trendup_storage"},
+    include_package_data=True,
+    url="http://pypi.python.org/pypi/MyApplication_v010/",
+    description="Useful towel-related stuff.",
+    install_requires=[
+        "attrs",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
