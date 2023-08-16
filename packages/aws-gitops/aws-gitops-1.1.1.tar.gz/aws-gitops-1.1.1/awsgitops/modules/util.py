0f305596b4dadef095b0a51dc1fe1b4b64554445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 15:09:41 +0000

    Add package aws-gitops/1.1.1/aws-gitops-1.1.1.tar.gz

---
+++ packages/aws-gitops/aws-gitops-1.1.1.tar.gz/aws-gitops-1.1.1/awsgitops/modules/util.py
@@ -0,0 +1,45 @@
+# Report an error and exit the program
+def error(contents):
+    print(f"ERROR: {contents}\nExiting program!")
+    exit(1)
+
+# Read a value from a yaml object with a variable number of keys
+def read(yaml, *keys):
+    if len(keys) == 1:
+        return yaml[keys[0]]
+    else:
+        return read(yaml[keys[0]], *keys[1:])
+
+# Check if a key is present in a yaml object
+def is_present(yaml, *keys):
+    if len(keys) == 1:
+        if keys[0] in yaml: return True
+        return False
+    else:
+        if keys[0] not in yaml: return False
+        return is_present(yaml[keys[0]], *keys[1:])
+
+# Find the path to a key in a yaml object
+def find(yaml, key, path=[]):
+    matches = []
+    if type(yaml) != dict and type(yaml) != list:
+        return matches 
+    for x, item in enumerate(yaml):
+        if item == key:
+            matches.append(path + [key])
+            return matches 
+        if type(yaml) == dict:
+            result = find(yaml[item], key, path=path+[item])
+        else:
+            result = find(yaml[x], key, path=path+[x])
+        matches += result
+    return matches
+
+# Write a value to a yaml object with a variable number of keys
+def write(yaml, value, *keys):
+    if len(keys) == 1:
+        yaml[keys[0]] = value
+        return yaml
+    else:
+        yaml[keys[0]] = write(yaml[keys[0]], value, *keys[1:])
+        return yaml
