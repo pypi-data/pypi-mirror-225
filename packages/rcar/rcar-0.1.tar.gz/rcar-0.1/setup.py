0977e0456341dc043459fd07d4ac8e6e3d797397
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 04:11:51 +0000

    Add package rcar/0.1/rcar-0.1.tar.gz

---
+++ packages/rcar/rcar-0.1.tar.gz/rcar-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rcar",
+    version="0.1",
+    packages=find_packages(),
+    author="Robert Valassi",
+    description="This is a class for cars2",
+    install_requires=[
+        # List the dependencies your package requires
+    ],
+)
