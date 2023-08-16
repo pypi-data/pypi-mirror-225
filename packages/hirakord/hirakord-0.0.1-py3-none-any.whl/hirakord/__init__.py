63fb6e9eb3968700edae17dbf13b5b973918c2c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 14:09:30 +0000

    Add package hirakord/0.0.1/hirakord-0.0.1-py3-none-any.whl

---
+++ packages/hirakord/hirakord-0.0.1-py3-none-any.whl/hirakord/__init__.py
@@ -0,0 +1,15 @@
+"""
+Hirakord: Build Bots faster
+~~~~~~~~~~~~~~~~~~~~~
+
+A command-line interface for building Discord bots.
+
+:copyright: (c) 2023-present ExHiraku
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = "hirakord"
+__author__ = "ExHiraku"
+__license__ = "MIT"
+__copyright__ = "Copyright 2023-present ExHiraku"
+__version__ = "0.0.1"
