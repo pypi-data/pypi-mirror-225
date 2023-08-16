f1bc75c08b6fa8829c543be89e83272d2b4f3406
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 15:11:24 +0000

    Add package goofis-ardihikaru/0.0.38/goofis_ardihikaru-0.0.38-py3-none-any.whl

---
+++ packages/goofis-ardihikaru/goofis_ardihikaru-0.0.38-py3-none-any.whl/goofis_ardihikaru/enums/currency_unit.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class CurrencyUnit(Enum):
+    """ Currency Unit enums """
+
+    # indonesians specific currency unit
+    ID_JT = "jt"
+    ID_RB = "rb"
+
+    T = "T"
+    B = "B"
+    M = "M"
