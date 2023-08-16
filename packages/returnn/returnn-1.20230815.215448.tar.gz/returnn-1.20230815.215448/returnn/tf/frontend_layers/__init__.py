25b9c085a7cf24dc18d0d29c3d384b463d871740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 05:07:34 +0000

    Add package returnn/1.20230815.215448/returnn-1.20230815.215448.tar.gz

---
+++ packages/returnn/returnn-1.20230815.215448.tar.gz/returnn-1.20230815.215448/returnn/tf/frontend_layers/__init__.py
@@ -0,0 +1,19 @@
+"""
+TensorFlow frontend.
+"""
+
+from ._backend import ReturnnLayersBackend
+
+# Sorted by name.
+from .cond import *
+from .config_entry_points import *
+from .debug_eager_mode import *
+from .dims import *
+from .layer import *
+from .make_layer import *
+from .prev_tensor_ref import *
+
+
+# https://returnn.readthedocs.io/en/latest/configuration_reference/behavior_version.html
+# Need Dim.is_equal to be more restrictive (v16).
+min_returnn_behavior_version = 16
