c2dab12089fdd23e9ae388218bda8dc6be8ec017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 03:08:46 +0000

    Add package mailpad/1.0.0/mailpad-1.0.0.tar.gz

---
+++ packages/mailpad/mailpad-1.0.0.tar.gz/mailpad-1.0.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.0'
+DESCRIPTION = 'Python Mailing Library'
+LONG_DESCRIPTION = 'A package that allows to send mails from python environment'
+
+setup(
+    name="mailpad",
+    version=VERSION,
+    author="Shivansh Dwivedi",
+    author_email="<me@shivanshdwivedi.tech>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['langchain'],
+    keywords=['mailpad','python', 'mail', 'email', 'email client', 'smtp'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    license="MIT",
+)
