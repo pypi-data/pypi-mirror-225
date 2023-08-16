970bf66e3709673364851b103f7c694478e3ed3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 14:09:31 +0000

    Add package hspylib-setman/0.10.4/hspylib_setman-0.10.4-py3-none-any.whl

---
+++ packages/hspylib-setman/hspylib_setman-0.10.4-py3-none-any.whl/setman/__init__.py
@@ -0,0 +1,12 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-setman v0.10.4
+#
+# Package: main.build.lib.setman
+"""Package initialization."""
+
+__all__ = [
+    'core', 
+    'settings'
+]
+__version__ = '0.10.4'
