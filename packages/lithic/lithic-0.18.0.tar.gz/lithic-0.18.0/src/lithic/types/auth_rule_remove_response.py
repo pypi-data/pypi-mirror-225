6fc99bddaa932f2f23f6d60175af0623b9af9249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 14:08:37 +0000

    Add package lithic/0.18.0/lithic-0.18.0.tar.gz

---
+++ packages/lithic/lithic-0.18.0.tar.gz/lithic-0.18.0/src/lithic/types/auth_rule_remove_response.py
@@ -0,0 +1,15 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from typing import List, Optional
+
+from .._models import BaseModel
+
+__all__ = ["AuthRuleRemoveResponse"]
+
+
+class AuthRuleRemoveResponse(BaseModel):
+    account_tokens: Optional[List[str]]
+
+    card_tokens: Optional[List[str]]
+
+    program_level: Optional[bool]
