c5a7eefae50978ee1be05000300e9090274fc017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 03:09:06 +0000

    Add package Janex/0.0.63/Janex-0.0.63.tar.gz

---
+++ packages/Janex/Janex-0.0.63.tar.gz/Janex-0.0.63/Janex/chat.py
@@ -0,0 +1,23 @@
+from main import *
+
+intents_file_path = "./intents.json"
+thesaurus_file_path = "./thesaurus.json"
+
+matcher = IntentMatcher(intents_file_path, thesaurus_file_path)
+
+input_string = input("You: ")
+
+intent_class, percentage = matcher.pattern_compare(input_string)
+response = matcher.response_compare(input_string, intent_class)
+
+#try:
+generated_response = matcher.ResponseGenerator(response)
+#except:
+#    pass
+
+print(response)
+
+#try:
+print(generated_response)
+#except:
+#    pass
