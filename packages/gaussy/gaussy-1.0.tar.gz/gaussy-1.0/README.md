b2d8b82ef22da3f4765b4816242e20ae248dbcc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 04:15:57 +0000

    Add package gaussy/1.0/gaussy-1.0.tar.gz

---
+++ packages/gaussy/gaussy-1.0.tar.gz/gaussy-1.0/README.md
@@ -0,0 +1,9 @@
+Gaussian Binomialdistribution Package
+
+Summary of the package
+
+#Files
+
+Explanation of files in the package
+
+#Installation
---
+++ packages/gaussy/gaussy-1.0.tar.gz/gaussy-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gaussy',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['gaussy'],
+      author = 'Evalyne Ndanu Musili',
+      zip_safe=False)
