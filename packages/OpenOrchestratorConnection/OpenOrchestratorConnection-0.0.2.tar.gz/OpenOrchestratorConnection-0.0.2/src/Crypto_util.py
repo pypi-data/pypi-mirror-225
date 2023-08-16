e6cf3c8d0971386b235a84f04c57f8366cd05f0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 03:09:04 +0000

    Add package OpenOrchestratorConnection/0.0.2/OpenOrchestratorConnection-0.0.2.tar.gz

---
+++ packages/OpenOrchestratorConnection/OpenOrchestratorConnection-0.0.2.tar.gz/OpenOrchestratorConnection-0.0.2/src/Crypto_util.py
@@ -0,0 +1,31 @@
+from cryptography.fernet import Fernet
+from cryptography.exceptions import InvalidSignature
+
+_enryption_key = None
+
+def generate_key():
+    return Fernet.generate_key()
+
+def set_key(key:str):
+    global _enryption_key 
+    _enryption_key = key
+
+def encrypt_string(data: str) -> str:
+    data = data.encode()
+    data = Fernet(_enryption_key).encrypt(data)
+    return data.decode()
+
+def decrypt_string(data:str) -> str:
+    try:
+        data = data.encode()
+        data = Fernet(_enryption_key).decrypt(data)
+        return data.decode()
+    except InvalidSignature:
+        raise ValueError("Couldn't verify signature. The decryption key is not the same as the encryption key.")
+
+def validate_key(key:str):
+    try:
+        Fernet(key)
+        return True
+    except:
+        return False
