d65961e532c3334b37d64a4f6a45ea23bff51c4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 15:11:45 +0000

    Add package jshmodule2/1.1.2/jshmodule2-1.1.2.tar.gz

---
+++ packages/jshmodule2/jshmodule2-1.1.2.tar.gz/jshmodule2-1.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jshmodule2',
+    version='1.1.2',
+    packages=find_packages(exclude=[]),
+    package_data={
+        'jshmodule2': ['data/*'],
+    },
+    install_requires=[
+        'geopandas',
+        'shapely',
+        'requests',
+        'pyproj'
+    ],
+)
