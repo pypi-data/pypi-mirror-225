dc6e199bbd93e42be5b0b6006859bcdabffcfe45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 03:09:36 +0000

    Add package django-df-api-drf/0.0.4/django_df_api_drf-0.0.4-py3-none-any.whl

---
+++ packages/django-df-api-drf/django_df_api_drf-0.0.4-py3-none-any.whl/df_api_drf/urls.py
@@ -0,0 +1,27 @@
+from django.apps import apps
+from django.conf.urls import include
+from django.urls import path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
+app_name = "df_api_drf"
+
+
+urlpatterns = [
+    path(f"{app.api_path}", include(f"{app.name}.drf.urls"))
+    for app in apps.get_app_configs()
+    if hasattr(app, "api_path")
+]
+
+urlpatterns += [
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("", SpectacularRedocView.as_view(url_name="df_api_drf:schema"), name="redoc"),
+    path(
+        "swagger/",
+        SpectacularSwaggerView.as_view(url_name="df_api_drf:schema"),
+        name="swagger-ui",
+    ),
+]
