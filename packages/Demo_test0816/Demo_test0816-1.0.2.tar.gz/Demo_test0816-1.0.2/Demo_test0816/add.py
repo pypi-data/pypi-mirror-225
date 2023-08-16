76c302a60d4734207d014827861671a7a6ae87d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 16:11:37 +0000

    Add package Demo_test0816/1.0.2/Demo_test0816-1.0.2.tar.gz

---
+++ packages/Demo_test0816/Demo_test0816-1.0.2.tar.gz/Demo_test0816-1.0.2/Demo_test0816/add.py
@@ -0,0 +1,6 @@
+# @Project  :pythonwork
+# @File     :add.py
+# @Date     :2023/8/16 11:03
+# @Author   :ZZL
+def func():
+    print("I am add methods！！！")
---
+++ packages/Demo_test0816/Demo_test0816-1.0.2.tar.gz/Demo_test0816-1.0.2/Demo_test0816/sub.py
@@ -0,0 +1,6 @@
+# @Project  :pythonwork
+# @File     :sub
+# @Date     :2023/8/16 11:04
+# @Author   :ZZL
+def func():
+    print("I'm sub method!")
