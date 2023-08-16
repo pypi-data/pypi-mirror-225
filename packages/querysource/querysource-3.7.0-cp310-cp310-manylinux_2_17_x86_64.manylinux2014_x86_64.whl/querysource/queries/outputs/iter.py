7e5a390219fab174af47ebda5137ca76e56b4bcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 04:13:32 +0000

    Add package querysource/3.7.0/querysource-3.7.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/querysource/querysource-3.7.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/querysource/queries/outputs/iter.py
@@ -0,0 +1,22 @@
+"""
+Iterable.
+
+Output format returning a simple list of dictionaries.
+"""
+import pandas
+from .abstract import OutputFormat
+
+
+class iterFormat(OutputFormat):
+    """
+    Most Basic Definition of Format.
+    """
+    async def serialize(self, result, error, *args, **kwargs):
+        print('RESULT >> ', type(result))
+        if isinstance(result, pandas.DataFrame):
+            data = result
+        elif isinstance(result, list):
+            data = [dict(row) for row in result]
+        else:
+            data = dict(result)
+        return (data, error)
