7ed4d1e72907da3c974bcf9c91714f8b7c045918
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 03:08:23 +0000

    Add package cnn-cam/0.0.2/cnn-cam-0.0.2.tar.gz

---
+++ packages/cnn-cam/cnn-cam-0.0.2.tar.gz/cnn-cam-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+    name="cnn-cam",
+    long_description=long_description, 
+    long_description_content_type = 'text/markdown',
+    version='0.0.2',
+    description='Gradient Based Class Activation Maps for TensorFlow models.',
+    py_modules=["tensorflow", "numpy"],
+    package_dir={'': 'cnn_cam'},
+    extras_require = {
+        "dev" : ["twine>=4.0.2"]
+    }
+)
