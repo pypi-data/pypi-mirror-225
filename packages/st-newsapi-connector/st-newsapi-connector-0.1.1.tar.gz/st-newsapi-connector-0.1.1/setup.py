36e2bdd7216427794ed11559d3634d6b09c38200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 04:14:11 +0000

    Add package st-newsapi-connector/0.1.1/st-newsapi-connector-0.1.1.tar.gz

---
+++ packages/st-newsapi-connector/st-newsapi-connector-0.1.1.tar.gz/st-newsapi-connector-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['st_newsapi_connector']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pandas==1.5.1', 'pycountry==22.3.5', 'requests==2.31.0', 'streamlit==1.25.0']
+
+setup_kwargs = {
+    'name': 'st-newsapi-connector',
+    'version': '0.1.1',
+    'description': 'A Python package to query data from NewsAPI in Streamlit apps',
+    'long_description': None,
+    'author': 'D. Carpintero',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://st-newsapi-connector.streamlit.app/',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<3.9',
+}
+
+
+setup(**setup_kwargs)
