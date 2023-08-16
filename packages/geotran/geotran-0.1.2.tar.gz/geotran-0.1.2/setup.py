75f21a452411d1a956fa16eb86991c04a1af634e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 14:09:23 +0000

    Add package geotran/0.1.2/geotran-0.1.2.tar.gz

---
+++ packages/geotran/geotran-0.1.2.tar.gz/geotran-0.1.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+setup(
+    name='geotran',
+    packages=find_packages(include=['geotran']),
+    version='0.1.2',
+    description='Python Geospatial Library',
+    author='Thai Tran',
+    license='MIT',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests',
+)
---
+++ packages/geotran/geotran-0.1.2.tar.gz/geotran-0.1.2/tests/test_geofuncs.py
@@ -0,0 +1,4 @@
+from geotran import raster
+
+def test_haversine():
+    assert raster.haversine(52.370216, 4.895168, 52.520008, 13.404954) == 946.3876221719836
