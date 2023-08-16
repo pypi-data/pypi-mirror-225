9efbb0d7fd0f36a8b78bf1418a5b11aab352d969
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 04:13:41 +0000

    Add package cancerontogpt/0.2.15/cancerontogpt-0.2.15-py3-none-any.whl

---
+++ packages/cancerontogpt/cancerontogpt-0.2.15-py3-none-any.whl/cancerontogpt/clients/__init__.py
@@ -0,0 +1,4 @@
+"""Clients for accessing different APIs."""
+from cancerontogpt.clients.openai_client import OpenAIClient  # noqa:F401
+from cancerontogpt.clients.pubmed_client import PubmedClient  # noqa:F401
+from cancerontogpt.clients.soup_client import SoupClient  # noqa:F401
