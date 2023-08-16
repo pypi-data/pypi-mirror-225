d0bc6d9f995d214345b3c3e18ce90674837378f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 15:10:22 +0000

    Add package datajunction-server/0.0.1a19/datajunction_server-0.0.1a19-py3-none-any.whl

---
+++ packages/datajunction-server/datajunction_server-0.0.1a19-py3-none-any.whl/datajunction_server/api/cubes.py
@@ -0,0 +1,26 @@
+"""
+Cube related APIs.
+"""
+import logging
+
+from fastapi import APIRouter, Depends
+from sqlmodel import Session
+
+from datajunction_server.api.helpers import get_node_by_name
+from datajunction_server.models.cube import CubeRevisionMetadata
+from datajunction_server.models.node import NodeType
+from datajunction_server.utils import get_session
+
+_logger = logging.getLogger(__name__)
+router = APIRouter(tags=["cubes"])
+
+
+@router.get("/cubes/{name}/", response_model=CubeRevisionMetadata, name="Get a Cube")
+def get_cube(
+    name: str, *, session: Session = Depends(get_session)
+) -> CubeRevisionMetadata:
+    """
+    Get information on a cube
+    """
+    node = get_node_by_name(session=session, name=name, node_type=NodeType.CUBE)
+    return node.current
