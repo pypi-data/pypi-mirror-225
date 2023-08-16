d14b3bc36253b594956ee8e1691d65fe28bd1c75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 04:13:18 +0000

    Add package python-flint/0.4.2/python_flint-0.4.2-cp310-cp310-win_amd64.whl

---
+++ packages/python-flint/python_flint-0.4.2-cp310-cp310-win_amd64.whl/flint/__init__.py
@@ -0,0 +1,15 @@
+"""""" # start delvewheel patch
+def _delvewheel_patch_1_5_0():
+    import os
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'python_flint.libs'))
+    if os.path.isdir(libs_dir):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_5_0()
+del _delvewheel_patch_1_5_0
+# end delvewheel patch
+
+from ._flint import *
+
+__version__ = '0.4.2'
