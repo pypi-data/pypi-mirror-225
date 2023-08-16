60ad5d45a7581ed388f15eb26d39e0e2e634d6ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 03:09:20 +0000

    Add package megadata/0.0.69/megadata-0.0.69.tar.gz

---
+++ packages/megadata/megadata-0.0.69.tar.gz/megadata-0.0.69/tests/test_mypy.py
@@ -0,0 +1,45 @@
+"""
+pytest
+"""
+import sys,os
+
+#print("__package__",__package__)
+#print("__file__",__file__)
+
+sys.path.insert(0, '..')
+
+path = os.path.dirname(os.path.dirname(__file__))
+sys.path.insert(0, path)
+
+print('sys.path',sys.path)
+
+import megadata
+print('megadata.load_time=',megadata.load_time)
+print('megadata.module_version=',megadata.module_version)
+
+from megadata.mypy import try_asyncio
+
+##################################################
+
+import asyncio
+from asyncio import get_event_loop, run as asyncio_run,new_event_loop
+
+# debug get_event_loop
+#try_asyncio = lambda func:get_event_loop().run_in_executor(None,func)
+# debug new_event_loop
+#try_asyncio_new = lambda func:new_event_loop().run_in_executor(None,func)
+
+async def async_print(*args,**kwargs):
+    return await try_asyncio(lambda:print(*args,**kwargs))
+
+async def async_print_new(*args,**kwargs):
+    return await try_asyncio(lambda:print(*args,**kwargs),new=True)
+
+def test_print():
+    rt = asyncio_run(async_print('hello world'))
+    assert rt is None, 'async_print should return None'
+
+# TODO: sth wrong with future
+#def test_print_new():
+#    rt = asyncio_run(async_print_new('hello world2'))
+#    assert rt is None, 'async_print should return None'
