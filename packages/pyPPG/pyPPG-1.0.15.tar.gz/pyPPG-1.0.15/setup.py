0d12c57e1b80d7c5f498177ed42f4f9beffe35d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 03:11:15 +0000

    Add package pyPPG/1.0.15/pyPPG-1.0.15.tar.gz

---
+++ packages/pyPPG/pyPPG-1.0.15.tar.gz/pyPPG-1.0.15/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import io
+
+with io.open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='pyPPG',
+    version='1.0.15',
+    description='pyPPG: a python toolbox for PPG morphological analysis.',
+    author='Marton A. Goda, PhD',
+    author_email="marton.goda@campus.technion.ac.il",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/godamartonaron/GODA_pyPPG",
+    project_urls={"Bug Tracker": "https://github.com/pypa/sampleproject/issues",},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+    ],
+    packages={"pyPPG", "pyPPG/ppg_bm", "pyPPG/pack_ppg"},
+    package_data={
+        "pyPPG": ["*"],
+        "pyPPG/ppg_bm": ["*"],
+        "pyPPG/pack_ppg": ["*"],
+    },
+
+    install_requires=[required],
+    python_requires=">=3.10",
+    include_package_data=True,
+)
