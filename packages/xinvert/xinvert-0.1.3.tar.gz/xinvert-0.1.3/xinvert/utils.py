74c447a34e9570e5d47838b40b28bfe2f6b397f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 14:09:06 +0000

    Add package xinvert/0.1.3/xinvert-0.1.3.tar.gz

---
+++ packages/xinvert/xinvert-0.1.3.tar.gz/xinvert-0.1.3/xinvert/utils.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2021.01.03
+
+@author: MiniUFO
+Copyright 2018. All rights reserved. Use is subject to license terms.
+"""
+#%%
+import numpy as np
+
+
+_R_earth = 6371200.0 # consistent with GrADS
+_omega = 7.292e-5    # angular speed of the earth rotation
+_g = 9.80665         # gravitational acceleration
+_undeftmp = -9.99e8  # default undefined value
+_deg2m = np.pi * _R_earth / 180.0
+
+_latlon = ['lat', 'latitude' , 'lats', 'yc', 'y',
+           'lon', 'longitude', 'long', 'xc', 'x']
+
+
+def loop_noncore(data, dims=None):
+    """Loop over the non-core dimensions using generator.
+
+    The non-core dimensions are given outside the list in `dims`.
+    
+    Parameters
+    ----------
+    data: xarray.DataArray
+        A given multidimensional data.
+    dims: list of str
+        Core dimensions.  The remaining dimensions are non-core dimension
+    
+    Yields
+    ------
+    dict
+        dict indicates the portion of `data` can be extracted
+    """
+    dimAll = data.dims
+    
+    dimCore = [] # ordered core dims
+    dimNonC = [] # ordered non-core dims
+    
+    for dim in dimAll:
+        if dim in dims:
+            dimCore.append(dim)
+        else:
+            dimNonC.append(dim)
+    
+    dimLopVars = []
+    for dim in dimNonC:
+        dimLopVars.append(data[dim].values)
+    
+    if len(dimNonC) >= 1:
+        from itertools import product
+        for idices in product(*dimLopVars):
+            selDict = {}
+            for d, i in zip(dimNonC, idices):
+                selDict[d] = i
+                
+                yield selDict
+    else:
+        yield {}
+
