7ae9428835b31e0d484c6d0f8aeac65353188ce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 03:09:20 +0000

    Add package confutilPPP/1.3.1/confutilPPP-1.3.1.tar.gz

---
+++ packages/confutilPPP/confutilPPP-1.3.1.tar.gz/confutilPPP-1.3.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+from confutilPPP import __version__
+
+URL = 'https://github.com/Aurorax-own/confutilPPP'
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='confutilPPP',
+    version=__version__,
+    description='A simple configuration file tool',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url=URL,
+    author='Aurorax-own',
+    author_email='15047150695@163.com',
+    packages=find_packages('src'),
+    package_dir={'confutilPPP': 'confutilPPP'},
+    include_package_data=True,
+    install_requires=[
+        'PyYAML==6.0.1'
+    ],
+    project_urls={
+        'Source': URL,
+        'Tracker': f'{URL}/issues',
+    },
+)
