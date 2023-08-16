46ccc3e97d9a7d00781b6af0ea892d6be5807e99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 04:13:39 +0000

    Add package pytoolbox/14.6.0/pytoolbox-14.6.0-py3-none-any.whl

---
+++ packages/pytoolbox/pytoolbox-14.6.0-py3-none-any.whl/pytoolbox/django/signals/dispatch.py
@@ -0,0 +1,17 @@
+from django import dispatch as _dispatch
+
+from pytoolbox.django.models import utils as _utils
+
+__all__ = ['InstanceSignal', 'post_state_transition']
+
+
+class InstanceSignal(_dispatch.Signal):
+
+    def send(self, sender=None, **named):
+        return super().send(_utils.get_base_model(sender or named['instance']), **named)
+
+    def send_robust(self, sender=None, **named):
+        return super().send(_utils.get_base_model(sender or named['instance']), **named)
+
+
+post_state_transition = InstanceSignal()
