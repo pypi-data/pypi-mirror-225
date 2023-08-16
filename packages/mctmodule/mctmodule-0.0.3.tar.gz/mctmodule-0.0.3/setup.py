1311c0f06099c6071f901cb140ef998ba60ba773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 05:07:20 +0000

    Add package mctmodule/0.0.3/mctmodule-0.0.3.tar.gz

---
+++ packages/mctmodule/mctmodule-0.0.3.tar.gz/mctmodule-0.0.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3' 
+DESCRIPTION = 'Third time'
+LONG_DESCRIPTION = 'Third long time'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="mctmodule", 
+        version=VERSION,
+        author="Gawain Southerland",
+        author_email="<gavinsoutherland@email.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=["psycopg2"], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
