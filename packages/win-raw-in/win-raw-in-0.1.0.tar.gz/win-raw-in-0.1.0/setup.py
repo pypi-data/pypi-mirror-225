ba0557d65fb6cf8be8b04af9e3f9094bea008866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 04:13:18 +0000

    Add package win-raw-in/0.1.0/win-raw-in-0.1.0.tar.gz

---
+++ packages/win-raw-in/win-raw-in-0.1.0.tar.gz/win-raw-in-0.1.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+long_description = """
+Enumerate raw input devices and receive input events with device ID on Windows.
+
+Homepage: https://github.com/holl-/win-raw-in
+"""
+
+setup(
+    name='win-raw-in',
+    version='0.1.0',
+    author='Philipp Holl',
+    author_email='philipp@mholl.de',
+    packages=['winrawin'],
+    url='https://github.com/holl-/win-raw-in',
+    license='MIT',
+    description='Enumerate raw input devices and receive input events with device ID on Windows',
+    keywords='keyboard mouse hook raw input',
+    long_description=long_description,
+    install_requires=['dataclasses'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
+)
