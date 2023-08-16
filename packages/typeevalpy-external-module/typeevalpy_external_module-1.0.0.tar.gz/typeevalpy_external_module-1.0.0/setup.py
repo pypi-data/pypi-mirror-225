d339336d876b00f2c2120d4cfcef3ce0b917f616
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-16 03:09:15 +0000

    Add package typeevalpy-external-module/1.0.0/typeevalpy_external_module-1.0.0.tar.gz

---
+++ packages/typeevalpy-external-module/typeevalpy_external_module-1.0.0.tar.gz/typeevalpy_external_module-1.0.0/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="typeevalpy_external_module",
+    version="1.0.0",
+    description="External module for TypeEvalPy micro-benchmark for type inference",
+    author="",
+    author_email="",
+    license="GPL-3.0",
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+)
+
+# pip install --upgrade typeevalpy_external_module
