5979c40856487be5adb59fd6b3cf966d6e17551d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 04:13:04 +0000

    Add package audrey-math/0.0.3/audrey_math-0.0.3.tar.gz

---
+++ packages/audrey-math/audrey_math-0.0.3.tar.gz/audrey_math-0.0.3/audrey_math/extras/__init__.py
@@ -0,0 +1,12 @@
+from .divide import divide
+from .multiply import multiply
+
+# math 
+# └── audrey_math 
+#     ├── extras
+#     │   ├── __init__.py
+#     │   ├── divide.py
+#     │   ├── multiply.py
+#     ├── add.py
+#     ├── subtract.py
+#     ├── __init__.py
