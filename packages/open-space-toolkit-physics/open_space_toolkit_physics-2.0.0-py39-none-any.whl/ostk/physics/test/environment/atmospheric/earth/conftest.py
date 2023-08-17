45ed97717a124d3ae39883d21df24b281023d0ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 14:08:09 +0000

    Add package open-space-toolkit-physics/2.0.0/open_space_toolkit_physics-2.0.0-py39-none-any.whl

---
+++ packages/open-space-toolkit-physics/open_space_toolkit_physics-2.0.0-py39-none-any.whl/ostk/physics/test/environment/atmospheric/earth/conftest.py
@@ -0,0 +1,50 @@
+# Apache License 2.0
+
+import pytest
+
+import pathlib
+
+from ostk.core.filesystem import Path
+from ostk.core.filesystem import File
+
+from ostk.io import URL
+
+from ostk.physics.environment.atmospheric.earth import Manager
+from ostk.physics.environment.atmospheric.earth import CSSISpaceWeather
+from ostk.physics.environment.atmospheric.earth import Exponential
+from ostk.physics.environment.atmospheric.earth import NRLMSISE00
+
+
+@pytest.fixture
+def data_directory_path() -> str:
+    return f"{pathlib.Path(__file__).parent.absolute()}/data"
+
+
+@pytest.fixture
+def cssi_space_weather_file(data_directory_path: str) -> File:
+    return File.path(Path.parse(f"{data_directory_path}/SW-Last5Years.test.csv"))
+
+
+@pytest.fixture
+def cssi_space_weather(cssi_space_weather_file: File) -> CSSISpaceWeather:
+    return CSSISpaceWeather.load(cssi_space_weather_file)
+
+
+@pytest.fixture
+def manager() -> Manager:
+    manager = Manager.get()
+
+    yield manager
+
+    manager.reset()
+    manager.clear_local_repository()
+
+
+@pytest.fixture
+def exponential_model() -> Exponential:
+    return Exponential()
+
+
+@pytest.fixture
+def nrlmsise00_model() -> NRLMSISE00:
+    return NRLMSISE00()
