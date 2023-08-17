f949006492cebcc91ab3c9af81eabb4b7ffd3d80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 03:12:09 +0000

    Add package lupin-danquin/1.0.4.dev1/lupin-danquin-1.0.4.dev1.tar.gz

---
+++ packages/lupin-danquin/lupin-danquin-1.0.4.dev1.tar.gz/lupin-danquin-1.0.4.dev1/lupin_danquin/main_test.py
@@ -0,0 +1,9 @@
+from lupin_danquin.core.application import Application
+
+
+print(
+    Application(
+        "D:/FDLCdev/Projets/Lupindental/DevOps/danquin/val3/LupinCell/LupinController/usr/usrapp/Msgs/CartesianMsg",
+        "CartesianMsg",
+    ).get_all_information_from_app()
+)
