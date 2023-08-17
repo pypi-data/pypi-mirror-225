f596e15437ed0613b54d3c2a747e4f7425c743f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 05:08:20 +0000

    Add package pyrockoeost/2023.2/pyrockoeost-2023.2.tar.gz

---
+++ packages/pyrockoeost/pyrockoeost-2023.2.tar.gz/pyrockoeost-2023.2/src/info.py
@@ -0,0 +1,7 @@
+# This module is automatically created from setup.py
+git_sha1 = None
+local_modifications = None
+version = '2023.06.07'
+long_version = 'pyrocko-eost-2023.06.07-2023-08-14_14:18:21'  # noqa
+installed_date = '2023-08-14_14:18:21'
+src_path = 'C:\\Users\\kylia\\Desktop\\pyrocko_test'
