7afc14ed59eef7ac2fa344b9cc3874d9429eafbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 03:10:03 +0000

    Add package seiketsu-api/1.4.3/seiketsu-api-1.4.3.tar.gz

---
+++ packages/seiketsu-api/seiketsu-api-1.4.3.tar.gz/seiketsu-api-1.4.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='seiketsu-api',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version='1.4.3',
+    description='A Python library to interact with a chat API',
+    author='LoLip_p',
+    author_email='mr.timon51@gmail.com',
+    url='https://github.com/yourusername/my-json-package',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "api_seiketsu": ["data/*.json"],
+    },
+    install_requires=[
+        "google-cloud-firestore>=2.11.1",
+        "google-auth>=2.22.0",
+        "google-auth-httplib2>=0.1.0",
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
