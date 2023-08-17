35e957ae9690544f1cbb8efa009b373c106d66c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 04:14:38 +0000

    Add package wakis/0.3.3/wakis-0.3.3.tar.gz

---
+++ packages/wakis/wakis-0.3.3.tar.gz/wakis-0.3.3/README.md
@@ -0,0 +1,14 @@
+wakis
+===
+
+[![PyPI version](https://badge.fury.io/py/wakis.svg)](https://badge.fury.io/py/wakis)
+[![stability-alpha](https://img.shields.io/badge/stability-alpha-f4d03f.svg)](https://github.com/mkenney/software-guides/blob/master/STABILITY-BADGES.md#alpha)
+
+WAKIS stands for (WA)ke and (I)mpedance (S)olver. It is a postprocessing tool that can obtain the longitudinal and transverse Wake Potential and Impedance from pre-computed electromagnetic fields. These magnitudes are important to quantify the losses of accelerator structures with a passing beam. 
+
+- Procedural version available in :file_folder: source/
+- OOP version available now in pyPI [wakis 0.2.0](https://pypi.org/project/wakis/)
+- Check :file_folder: examples/ on how to use!
+
+More information about theory, installation and usage can be found on: https://wakis.readthedocs.io/en/latest/
+Github link: https://github.com/ImpedanCEI/wakis
---
+++ packages/wakis/wakis-0.3.3.tar.gz/wakis-0.3.3/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+#from .wakis import __version__
+
+####################################
+# Add long description from README #
+####################################
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+#########
+# Setup #
+#########
+setup(
+    name="wakis",
+    version="0.3.3",
+    description="Wakis obtains Wake potential and Impedance from pre-computed fields",
+    author="Elena de la Fuente Garcia",
+    author_email="elena.de.la.fuente.garcia@cern.ch",
+    packages=['wakis'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ImpedanCEI/WAKIS",
+    project_urls={"Bug Tracker": "https://github.com/ImpedanCEI/WAKIS/issues"},
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'h5py',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+
+)
