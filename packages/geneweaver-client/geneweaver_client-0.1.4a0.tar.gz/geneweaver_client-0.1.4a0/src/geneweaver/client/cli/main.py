b7d085c1b56cffaf856870e7c4a981f3ced85c7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 03:14:22 +0000

    Add package geneweaver-client/0.1.4a0/geneweaver_client-0.1.4a0.tar.gz

---
+++ packages/geneweaver-client/geneweaver_client-0.1.4a0.tar.gz/geneweaver_client-0.1.4a0/src/geneweaver/client/cli/main.py
@@ -0,0 +1,28 @@
+"""The main entrypoint to the GeneWeaver CLI client."""
+# ruff: noqa: B008
+import pkg_resources
+import typer
+
+from geneweaver.client.cli import parser
+from geneweaver.client.cli import auth
+
+cli = typer.Typer(no_args_is_help=True)
+cli.add_typer(parser.cli, name="parse")
+cli.add_typer(auth.cli, name="auth")
+
+
+def version_callback(version: bool) -> None:
+    """Print the version of the GeneWeaver CLI client."""
+    if version:
+        version = pkg_resources.get_distribution("geneweaver-client").version
+        typer.echo(f"GeneWeaver CLI client (gweave) version: {version}")
+        raise typer.Exit(code=0)
+
+
+@cli.callback()
+def common(
+    ctx: typer.Context,
+    version: bool = typer.Option(None, "--version", callback=version_callback),
+) -> None:
+    """GeneWeaver CLI client."""
+    pass
