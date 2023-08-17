6d3aad18fcc7c6906921d7f312c398a7c055ab08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 03:11:18 +0000

    Add package taskp5/2.0.3/taskp5-2.0.3.tar.gz

---
+++ packages/taskp5/taskp5-2.0.3.tar.gz/taskp5-2.0.3/log_parser2/__init__.py
@@ -0,0 +1,13 @@
+from log_parser2.cli import parse_cli
+from log_parser2.__version__ import __version__
+
+from log_parser2.log_parser2 import logger, LogHandler, Functions
+
+__all__ = [
+    '__version__',
+    "logger",
+    "parse_cli",
+    "LogHandler",
+    "Functions",
+]
+
