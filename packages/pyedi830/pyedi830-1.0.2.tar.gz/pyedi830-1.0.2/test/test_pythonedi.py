be3d4ae99398c1d217bef7b1e6c19319598d28be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 04:14:38 +0000

    Add package pyedi830/1.0.2/pyedi830-1.0.2.tar.gz

---
+++ packages/pyedi830/pyedi830-1.0.2.tar.gz/pyedi830-1.0.2/test/test_pythonedi.py
@@ -0,0 +1,20 @@
+"""
+Test cases for pyedi830 module
+"""
+
+import string
+import random
+import unittest
+from datetime import datetime
+
+import pyedi830
+
+class TestConvertEdiToCsv(unittest.TestCase):
+
+    def setUp(self):
+        self.parser_df = pyedi830.EDIParserDF()
+
+    def test_csv(self):
+        edi_file_path = "test/test_edi_830_forecast.edi"
+        csv_file_path = "test/test_edi_830_forecast.csv"
+        self.parser_df.to_csv(edi_file_path, csv_file_path)
