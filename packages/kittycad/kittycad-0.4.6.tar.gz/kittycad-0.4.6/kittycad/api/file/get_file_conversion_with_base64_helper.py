a63f85e8626bdb5e654d18b9d434ab344f91e71d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 14:09:37 +0000

    Add package kittycad/0.4.6/kittycad-0.4.6.tar.gz

---
+++ packages/kittycad/kittycad-0.4.6.tar.gz/kittycad-0.4.6/kittycad/api/file/get_file_conversion_with_base64_helper.py
@@ -0,0 +1,49 @@
+import base64
+from typing import Any, Optional, Union
+
+from ...api.api_calls.get_async_operation import asyncio as fc_asyncio, sync as fc_sync
+from ...client import Client
+from ...models import Error
+from ...models.file_conversion import FileConversion
+
+
+def sync(
+    id: str,
+    *,
+    client: Client,
+) -> Optional[Union[Any, FileConversion, Error]]:
+    """Get the status of a file conversion. This function automatically base64 decodes the output response if there is one."""
+
+    fc = fc_sync(
+        id=id,
+        client=client,
+    )
+
+    if isinstance(fc, FileConversion) and fc.output != "":
+        if isinstance(fc.output, str):
+            b = base64.b64decode(fc.output + "=" * (-len(fc.output) % 4))
+            # decode the bytes to a string
+            fc.output = b.decode("utf-8")
+
+    return fc
+
+
+async def asyncio(
+    id: str,
+    *,
+    client: Client,
+) -> Optional[Union[Any, FileConversion, Error]]:
+    """Get the status of a file conversion. This function automatically base64 decodes the output response if there is one."""
+
+    fc = await fc_asyncio(
+        id=id,
+        client=client,
+    )
+
+    if isinstance(fc, FileConversion) and fc.output != "":
+        if isinstance(fc.output, str):
+            b = base64.b64decode(fc.output + "=" * (-len(fc.output) % 4))
+            # decode the bytes to a string
+            fc.output = b.decode("utf-8")
+
+    return fc
