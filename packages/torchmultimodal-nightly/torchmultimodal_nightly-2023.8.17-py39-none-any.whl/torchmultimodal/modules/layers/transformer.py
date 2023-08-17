877a096421635f8970d92de29b6b782c4a504411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 14:08:03 +0000

    Add package torchmultimodal-nightly/2023.8.17/torchmultimodal_nightly-2023.8.17-py39-none-any.whl

---
+++ packages/torchmultimodal-nightly/torchmultimodal_nightly-2023.8.17-py39-none-any.whl/torchmultimodal/modules/layers/transformer.py
@@ -0,0 +1,21 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+# Code for some of the transformers components in this file are initialized
+# from their counterparts in Hugging Face Transformers library.
+
+from typing import List, NamedTuple, Optional, Tuple
+
+from torch import Tensor
+
+
+class TransformerOutput(NamedTuple):
+    last_hidden_state: Optional[Tensor] = None
+    pooler_output: Optional[Tensor] = None
+    hidden_states: Optional[List[Tensor]] = None
+    attentions: Optional[List[Tensor]] = None
+    image_labels: Optional[Tensor] = None
+    current_key_values: Optional[List[Tuple[Tensor, Tensor]]] = None
