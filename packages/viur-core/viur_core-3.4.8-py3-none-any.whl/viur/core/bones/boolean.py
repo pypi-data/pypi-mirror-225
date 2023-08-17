b56e539d0e21d20e9188ac3aa6a0f77de7279479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 04:12:30 +0000

    Add package viur-core/3.4.8/viur_core-3.4.8-py3-none-any.whl

---
+++ packages/viur-core/viur_core-3.4.8-py3-none-any.whl/viur/core/bones/boolean.py
@@ -0,0 +1,81 @@
+from viur.core.bones.base import BaseBone, ReadFromClientError, ReadFromClientErrorSeverity
+from viur.core import db, conf
+from typing import Dict, List, Optional, Any, Union
+
+
+class BooleanBone(BaseBone):
+    """
+    Represents a boolean data type, which can have two possible values: `True` or `False`.
+
+    BooleanBones cannot be defined as `multiple=True`.
+
+    :param defaultValue: The default value of the `BooleanBone` instance. Defaults to `False`.
+    :type defaultValue: bool
+    :raises ValueError: If the `defaultValue` is not a boolean value (`True` or `False`).
+    """
+    type = "bool"
+
+    def __init__(
+        self,
+        *,
+        defaultValue: Union[
+            bool,
+            List[bool],
+            Dict[str, Union[List[bool], bool]],
+        ] = None,
+        **kwargs
+    ):
+        if defaultValue is None:
+            if kwargs.get("multiple") or kwargs.get("languages"):
+                # BaseBone's __init__ will choose an empty container for this
+                defaultValue = None
+            else:
+                # We have a single bone which is False
+                defaultValue = False
+        else:
+            # We have given an explicit defaultValue and maybe a complex structure
+            if not (kwargs.get("multiple") or kwargs.get("languages")) and not isinstance(defaultValue, bool):
+                raise TypeError("Only 'True' or 'False' can be provided as BooleanBone defaultValue")
+            # TODO: missing validation for complex types, but in other bones too
+
+        super().__init__(defaultValue=defaultValue, **kwargs)
+
+        # Disallow creation of BooleanBone(multiple=True)
+        if self.multiple:
+            raise ValueError("BooleanBone cannot be multiple")
+
+    def singleValueFromClient(self, value, skel: 'viur.core.skeleton.SkeletonInstance', name: str, origData):
+        return str(value).strip().lower() in conf["viur.bone.boolean.str2true"], None
+
+    def getEmptyValue(self):
+        return False
+
+    def isEmpty(self, rawValue: Any):
+        if rawValue is self.getEmptyValue():
+            return True
+        return not bool(rawValue)
+
+    def refresh(self, skel: 'viur.core.skeleton.SkeletonInstance', boneName: str) -> None:
+        """
+            Inverse of serialize. Evaluates whats
+            read from the datastore and populates
+            this bone accordingly.
+
+            :param name: The property-name this bone has in its Skeleton (not the description!)
+        """
+        if not isinstance(skel[boneName], bool):
+            skel[boneName] = str(skel[boneName]).strip().lower() in conf["viur.bone.boolean.str2true"]
+
+    def buildDBFilter(
+        self,
+        name: str,
+        skel: 'viur.core.skeleton.SkeletonInstance',
+        dbFilter: db.Query,
+        rawFilter: Dict,
+        prefix: Optional[str] = None
+    ) -> db.Query:
+        if name in rawFilter:
+            val = str(rawFilter[name]).strip().lower() in conf["viur.bone.boolean.str2true"]
+            return super().buildDBFilter(name, skel, dbFilter, {name: val}, prefix=prefix)
+
+        return dbFilter
