0cdd45fc3eb3c9b557fce0b621ed29f3a7893aa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 14:08:11 +0000

    Add package ckanext-drupal-api/0.6.0/ckanext_drupal_api-0.6.0-py3-none-any.whl

---
+++ packages/ckanext-drupal-api/ckanext_drupal_api-0.6.0-py3-none-any.whl/ckanext/drupal_api/config.py
@@ -0,0 +1,18 @@
+CONFIG_DRUPAL_URL = "ckanext.drupal_api.drupal_url"
+
+CONFIG_CACHE_DURATION = "ckanext.drupal_api.cache.duration"
+CONFIG_REQUEST_TIMEOUT = "ckanext.drupal_api.timeout"
+CONFIG_REQUEST_HTTP_USER = "ckanext.drupal_api.request.user"
+CONFIG_REQUEST_HTTP_PASS = "ckanext.drupal_api.request.pass"
+
+CONFIG_DRUPAL_API_VERSION = "ckanext.drupal_api.api_version"
+JSON_API = "json"
+CORE_API = "core"
+DEFAULT_API_VERSION = CORE_API
+
+
+CONFIG_MENU_EXPORT = "ckanext.drupal_api.core.menu_export_endpoint"
+DEFAULT_MENU_EXPORT_EP = "/resource/layout/export"
+
+DEFAULT_REQUEST_TIMEOUT = 5
+DEFAULT_CACHE_DURATION = 3600
