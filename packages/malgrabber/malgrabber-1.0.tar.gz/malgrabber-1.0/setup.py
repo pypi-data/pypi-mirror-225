25b89414547821cf7e13e660ae0cd70bbda595d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 03:13:53 +0000

    Add package malgrabber/1.0/malgrabber-1.0.tar.gz

---
+++ packages/malgrabber/malgrabber-1.0.tar.gz/malgrabber-1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="malgrabber",
+    version="1.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "beautifulsoup4",
+        "asyncio",
+        "aiohttp"
+    ],
+    author="Swargaraj Bhowmik",
+    author_email="contact@swargarajbhowmik.me",
+    license="MIT",
+    url="https://github.com/swargarajbhowmik/malgrabber",
+)
