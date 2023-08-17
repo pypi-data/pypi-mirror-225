43fb5b1df33a2c1a0f2beb86518f6442a58ccb29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 15:09:50 +0000

    Add package MedCodeMap/1.4/MedCodeMap-1.4.tar.gz

---
+++ packages/MedCodeMap/MedCodeMap-1.4.tar.gz/MedCodeMap-1.4/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="MedCodeMap",
+    version="1.4",
+    author="Chaoqi Yang",
+    author_email="chaoqiy2@illinois.edu",
+    description="A package for medical code mapping",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ycq091044/MedCodeMap",
+    packages=setuptools.find_packages(),
+    install_requires=["networkx", "pandas"],
+    entry_points={
+        "console_scripts": ["MedCodeMap=MedCodeMap:MedCodeMap"],
+    },
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
