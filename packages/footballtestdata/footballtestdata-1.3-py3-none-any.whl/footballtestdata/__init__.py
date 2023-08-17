14a1d25a003097618c8a21c8c27d606780e03092
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 03:11:37 +0000

    Add package footballtestdata/1.3/footballtestdata-1.3-py3-none-any.whl

---
+++ packages/footballtestdata/footballtestdata-1.3-py3-none-any.whl/footballtestdata/__init__.py
@@ -0,0 +1,5 @@
+# __init__.py
+
+# Import the modules or subpackages you want to make accessible from the package level
+from .data import ENG, ITA, SPA, GER, position
+from .generator import FootballerGenerator
