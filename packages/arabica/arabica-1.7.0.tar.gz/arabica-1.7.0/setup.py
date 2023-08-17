f20cbfbaa68e52b2371c5bedf7b4a66fd82074bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 05:07:41 +0000

    Add package arabica/1.7.0/arabica-1.7.0.tar.gz

---
+++ packages/arabica/arabica-1.7.0.tar.gz/arabica-1.7.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*
+
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    description = fh.read()
+
+    setuptools.setup(
+        name="arabica",
+        version="1.7.0",
+        author="Petr Koráb",
+        author_email="xpetrkorab@gmail.com",
+        packages=["arabica"],
+        description="Python package for exploratory text data analysis",
+        long_description=description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/PetrKorab/Arabica",
+        python_requires='>3.9, <3.11',
+        install_requires = ['pandas == 1.4.0',
+                            'nltk == 3.6.2',
+                            'regex == 2022.10.31',
+                            'finvader',
+                            'matplotlib == 3.6.0',
+                            'matplotlib-inline == 0.1.6',
+                            'plotnine == 0.10.1',
+                            'wordcloud == 1.8.2.2',
+                            'jenkspy == 0.3.2',
+                            'vaderSentiment == 3.3.2',
+                            'cleantext == 1.1.4',
+                            'mizani == 0.9.2'],
+        license='OSI Approved :: Apache Software License'
+    )
