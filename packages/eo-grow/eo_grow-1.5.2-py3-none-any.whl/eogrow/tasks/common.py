7dec8bf28381824cfc669dcd71518775ddb1a3b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 03:12:27 +0000

    Add package eo-grow/1.5.2/eo_grow-1.5.2-py3-none-any.whl

---
+++ packages/eo-grow/eo_grow-1.5.2-py3-none-any.whl/eogrow/tasks/common.py
@@ -0,0 +1,63 @@
+"""Common tasks shared between pipelines."""
+from __future__ import annotations
+
+from typing import Callable
+
+import numpy as np
+
+from eolearn.core import EOPatch, EOTask, SaveTask
+from eolearn.core.utils.parsing import parse_renamed_feature
+from eolearn.geometry import MorphologicalOperations
+
+from ..types import Feature
+
+
+class ClassFilterTask(EOTask):
+    """Run class specific morphological operation."""
+
+    def __init__(
+        self,
+        feature: Feature,
+        labels: list[int],
+        morph_operation: MorphologicalOperations | Callable,
+        struct_elem: np.ndarray | None = None,
+    ):
+        """Perform a morphological operation on a given feature mask
+
+        :param feature: Feature to be modified
+        :param labels: List of labels to be considered for morphological operation
+        :param morph_operation: Type of morphological operation ot perform
+        :param struct_elem: Structured element to be used. Taken from `ml_tools.MorphologicalStructFactory`
+        """
+        self.feature_name: str | None
+        self.new_feature_name: str | None
+        self.renamed_feature = parse_renamed_feature(feature)
+        self.labels = labels
+
+        if isinstance(morph_operation, MorphologicalOperations):
+            self.morph_operation = MorphologicalOperations.get_operation(morph_operation)
+        else:
+            self.morph_operation = morph_operation
+        self.struct_elem = struct_elem
+
+    def execute(self, eopatch: EOPatch) -> EOPatch:
+        feature_type, feature_name, new_feature_name = self.renamed_feature
+        mask = eopatch[(feature_type, feature_name)].copy()
+
+        for label in self.labels:
+            label_mask = np.squeeze((mask == label), axis=-1)
+            mask_mod = self.morph_operation(label_mask, self.struct_elem) * label
+            mask_mod = mask_mod[..., np.newaxis]
+            mask[mask == label] = mask_mod[mask == label]
+
+        eopatch[(feature_type, new_feature_name)] = mask
+        return eopatch
+
+
+class SkippableSaveTask(SaveTask):
+    """Same as `SaveTask` but can be skipped if the `eopatch_folder` is set to `None`."""
+
+    def execute(self, eopatch: EOPatch, *, eopatch_folder: str | None = "") -> EOPatch:
+        if eopatch_folder is None:
+            return eopatch
+        return super().execute(eopatch, eopatch_folder=eopatch_folder)
