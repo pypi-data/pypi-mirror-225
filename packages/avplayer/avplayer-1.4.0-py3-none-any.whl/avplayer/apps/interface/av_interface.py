4541ebf3dd09430112a6a1b7f7578818a0466acb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 15:10:03 +0000

    Add package avplayer/1.4.0/avplayer-1.4.0-py3-none-any.whl

---
+++ packages/avplayer/avplayer-1.4.0-py3-none-any.whl/avplayer/apps/interface/av_interface.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from abc import ABCMeta, abstractmethod
+from typing import Optional
+
+from numpy import uint8
+from numpy.typing import NDArray
+
+
+class AvInterface(metaclass=ABCMeta):
+    @abstractmethod
+    def on_image(self, image: NDArray[uint8]) -> Optional[NDArray[uint8]]:
+        raise NotImplementedError
+
+
+class AsyncAvInterface(metaclass=ABCMeta):
+    @abstractmethod
+    async def on_image(self, image: NDArray[uint8]) -> Optional[NDArray[uint8]]:
+        raise NotImplementedError
