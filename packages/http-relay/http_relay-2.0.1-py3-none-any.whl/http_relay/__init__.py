3254c2ec8127fd33f3c30efb6836db2372992c3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 04:13:29 +0000

    Add package http-relay/2.0.1/http_relay-2.0.1-py3-none-any.whl

---
+++ packages/http-relay/http_relay-2.0.1-py3-none-any.whl/http_relay/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# SPDX-FileCopyrightText: Czech Technical University in Prague
+
+from .relay import run, sigkill_after
+
+__all__ = [run.__name__, sigkill_after.__name__]
