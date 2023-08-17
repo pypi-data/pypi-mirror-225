f1fc9d837db2aeb6cb523e1c6b4e153e4a6ecd16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 03:13:46 +0000

    Add package csle-ryu/0.3.0/csle_ryu-0.3.0.tar.gz

---
+++ packages/csle-ryu/csle_ryu-0.3.0.tar.gz/csle_ryu-0.3.0/tests/test_learning_switch_controller.py
@@ -0,0 +1,14 @@
+
+
+class TestLearningSwitchControllerSuite:
+    """
+    Test suite for learning_switch_controller.py
+    """
+
+    def test_add_flow(self) -> None:
+        """
+        Tests the add_flow function
+
+        :return: None
+        """
+        assert True
