91cd44bdc883381900c4edb21732de9a28238dc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 15:09:37 +0000

    Add package nacos-python-sdk/0.1/nacos-python-sdk-0.1.tar.gz

---
+++ packages/nacos-python-sdk/nacos-python-sdk-0.1.tar.gz/nacos-python-sdk-0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+
+setuptools.setup(
+    name="nacos-python-sdk",
+    version="0.1",
+    description="nacos-python-sdk",
+    long_description="debug client.py for nacos-python-sdk to fit python3.8 and Nacos 2.2+",
+    long_description_content_type="text/markdown",
+    url="https://codeup.aliyun.com/612cb27957e7cd986dfaf21f/nacos-python-sdk",
+    keywords=['nacos', 'nacos-sdk-python'],
+    install_requires=[],
+    packages=setuptools.find_packages(),
+)
