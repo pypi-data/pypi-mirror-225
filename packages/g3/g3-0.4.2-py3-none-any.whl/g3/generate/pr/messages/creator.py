f139189af94819c1a8a7e52e8bd6311a509c4c47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 04:11:52 +0000

    Add package g3/0.4.2/g3-0.4.2-py3-none-any.whl

---
+++ packages/g3/g3-0.4.2-py3-none-any.whl/g3/generate/pr/messages/creator.py
@@ -0,0 +1,56 @@
+from typing import Optional
+
+from g3.domain.message_tone import MessageTone
+from g3.generate.client import OpenAIChat
+from g3.generate.pr.prompts.creator import Creator as PromptCreator
+from g3.generate.preview.cli import Presenter
+from g3.git import client as git
+from g3.git.client import get_commit_messages
+from g3.git.gitinfo import GitInfo
+from g3.github.client import Client as GHClient
+from g3.github.github_info import GithubInfo
+
+
+class Creator:
+    def __init__(self):
+        self.prompt_creator = PromptCreator()
+        self.openai = OpenAIChat()
+        self.gh = GHClient()
+        self.git_info = GitInfo()
+
+    def create(self, tone: MessageTone, jira=None, include=None, edit: Optional[int] = None) -> None:
+        if edit:
+            pr = GHClient().get_pull_request(edit)
+            commit_messages = []
+            for commit in pr.get_commits():
+                commit_messages.append(commit.commit.message)
+            prompt = self.prompt_creator.create(tone, commit_messages, jira, include)
+            stream = self.openai.stream(prompt)
+
+            original_message = f"{pr.title}\n\n{pr.body}"
+
+            reviewed_message, retry = Presenter.present_comparison(original_message, stream, "pr")
+            while retry:
+                stream = self.openai.stream(prompt)
+                reviewed_message, retry = Presenter.present_comparison(original_message, stream, "pr")
+
+            title = reviewed_message.split("\n")[1]
+            description = reviewed_message.split(title)[1]
+            git.push("origin", self.git_info.branch, force=True)
+            self.gh.update_pull_request(pr.number, title, description)
+            print(f"Successfully updated PR: {pr.html_url}")
+        else:
+            commit_messages = get_commit_messages(GithubInfo().default_branch)
+            prompt = self.prompt_creator.create(tone, commit_messages, jira, include)
+            stream = self.openai.stream(prompt)
+
+            reviewed_message, retry = Presenter.present(stream, "pr")
+            while retry:
+                stream = self.openai.stream(prompt)
+                reviewed_message, retry = Presenter.present(stream, "pr")
+
+            title = reviewed_message.split("\n")[1]
+            description = reviewed_message.split(title)[1]
+            git.push("origin", self.git_info.branch, force=True)
+            pr = self.gh.create_pull_request(title, description)
+            print(f"Opened PR: {pr.html_url}")
---
+++ packages/g3/g3-0.4.2-py3-none-any.whl/g3/generate/pr/prompts/creator.py
@@ -0,0 +1,58 @@
+from typing import Optional
+
+from g3.domain.message_tone import MessageTone
+from g3.generate.pr.prompts.template import pr_template
+from g3.git.gitinfo import GitInfo
+from g3.main import config
+
+
+class Creator:
+    def __init__(self):
+        self.git_info = GitInfo()
+        self.commit_messages: list[str] = []
+
+    def create(
+        self, tone: MessageTone, commit_messages, jira: Optional[str] = None, include: Optional[str] = None
+    ) -> list:
+        self.commit_messages = commit_messages
+        system_messages = self.create_system_messages(tone, jira, include)
+        return system_messages + self.user_messages
+
+    @property
+    def user_messages(self) -> list:
+        content = f"""Please provide a pull request description for the provided commit messages.
+ Commits: ```{self.commit_messages}```. The commit messages are form a git branch named {self.git_info.branch}.
+ The commit messages are from a git repository named {self.git_info.repo}."""
+
+        return [
+            {
+                "role": "user",
+                "content": content.replace("\n", ""),
+            }
+        ]
+
+    def create_system_messages(
+        self, tone: MessageTone, jira: Optional[str] = None, include: Optional[str] = None
+    ) -> list:
+        content = f"""You are a helpful assistant creating Pull Request titles and descriptions for a software
+ engineering team. The pull request title and description should be created from a commit message or a
+ series of commit messages that will be provided. A commit message, is a message which explains briefly
+ what was changed in this commit. A pull request is a github event that takes place in software development
+ when a contributor is ready to begin the process of merging new code changes with the main project
+ repository. The pull request contains a description of what changed and why it changed.
+ The pull request's title should be a sentence containing a title summing up the provided commits.
+ Leave an new empty line always after the title. The pull reuqest's description should be under
+ {config.pr_description_max_words} words in total and it should be helpful to the reader in order to
+ understand the changes made. The reader of the pull request description needs to understand the changes
+ in order to approve the pull request or ask for the needed changes. If Jira tickets are provided then include them in
+ the pull request description. Use the following tone when creating the pull request message: {tone}."""
+        if include:
+            content += f" Include in your response the following: {include}"
+        if jira:
+            content += f" Include in the end of your response the following Jira ticket: ```{jira}```."
+        content += (
+            f" Your response should be in Github markdown syntax format and comply on the following template:"
+            f"```{pr_template}```."
+        )
+
+        return [{"role": "system", "content": content.replace("\n", "")}]
