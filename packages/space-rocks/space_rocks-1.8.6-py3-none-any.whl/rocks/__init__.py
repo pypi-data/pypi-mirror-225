85434a090b7eb103b2c74c2323ca1d1dd096d41c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 04:11:20 +0000

    Add package space-rocks/1.8.6/space_rocks-1.8.6-py3-none-any.whl

---
+++ packages/space-rocks/space_rocks-1.8.6-py3-none-any.whl/rocks/__init__.py
@@ -0,0 +1,18 @@
+"""For space rocks."""
+
+import inspect
+
+# Welcome to rocks
+__version__ = "1.8.6"
+
+# Only define user API if rocks is not called via command line
+context = inspect.stack()[-1].code_context
+if context is None or "rocks.cli" not in context[0]:
+    # Expose API to user
+    from .core import Rock
+    from .core import rocks_ as rocks
+    from .resolve import identify
+    from .logging import set_log_level
+
+    # Alias id to identify
+    id = identify
