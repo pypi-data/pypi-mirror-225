986fee8bf116f9f958187d8a9465120afd7f963a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 03:10:03 +0000

    Add package chift/0.1.3/chift-0.1.3-py3-none-any.whl

---
+++ packages/chift/chift-0.1.3-py3-none-any.whl/chift/models/consumers/data/__init__.py
@@ -0,0 +1,31 @@
+from typing import ClassVar
+
+from chift.api.mixins import CreateMixin, ListMixin, UpdateMixin
+from chift.openapi.models import Data as DataModel
+
+
+class DataRouter:
+    def __init__(self, consumer_id, connection_id):
+        self.Data = Data(consumer_id, connection_id)
+
+
+class Data(
+    ListMixin[DataModel],
+    CreateMixin[DataModel],
+    UpdateMixin[DataModel],
+):
+    chift_vertical: ClassVar = "datastore"
+    chift_model: ClassVar = ""
+    model = DataModel
+
+    def get(self, datastore_id, params=None, client=None):
+        self.extra_path = f"{datastore_id}/data"
+        return super().all(params=params, client=client)
+
+    def create(self, datastore_id, data, client=None):
+        self.extra_path = f"{datastore_id}/data"
+        return super().create(data, map_model=False, client=client)
+
+    def update(self, datastore_id, datastoredata_id, data, client=None):
+        self.extra_path = f"{datastore_id}/data/{datastoredata_id}"
+        return super().update(None, data, map_model=False, client=client)
