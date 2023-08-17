9e957cc0db7c54b8429c30f10fe248bccfd2c8bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 15:10:02 +0000

    Add package datetime-process/0.1/datetime_process-0.1.tar.gz

---
+++ packages/datetime-process/datetime_process-0.1.tar.gz/datetime_process-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='datetime_process',
+    version='0.1',  # Phiên bản của gói
+    description='Rules date_time',
+    author='ndtands',
+    author_email='ndtan.fpt@email.com',
+    packages=['datetime_process'],  # Danh sách các thư mục chứa mã của bạn
+)
