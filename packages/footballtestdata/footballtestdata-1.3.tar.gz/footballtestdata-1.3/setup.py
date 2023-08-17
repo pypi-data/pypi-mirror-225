e3dcce4d334753668fb623873d8c050495bbe8ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 03:11:37 +0000

    Add package footballtestdata/1.3/footballtestdata-1.3.tar.gz

---
+++ packages/footballtestdata/footballtestdata-1.3.tar.gz/footballtestdata-1.3/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# Read the contents of README file
+this_directory = Path(__file__).parent
+LONG_DESCRIPTION = (this_directory / "README.md").read_text()
+
+# Define package metadata
+VERSION = '1.3'
+DESCRIPTION = 'A package to generate fake footballer data'
+AUTHOR = 'Marek Zarzycki'
+AUTHOR_EMAIL = 'contact@mazarzycki.com'
+AUTHOR_WEBSITE = 'https://mazarzycki.com/'
+
+# Define classifiers for your package
+CLASSIFIERS = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 3",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
+
+setup(
+    name='footballtestdata',
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=AUTHOR_WEBSITE,
+    packages=find_packages(),
+    classifiers=CLASSIFIERS,  # Add your classifiers here
+    keywords=['python', 'football', 'soccer', 'data'],
+)
