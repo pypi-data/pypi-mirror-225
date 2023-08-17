16feee5a63111208b5c50aeea142735242904162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 15:10:26 +0000

    Add package modal-client/0.50.3052/modal_client-0.50.3052-py3-none-any.whl

---
+++ packages/modal-client/modal_client-0.50.3052-py3-none-any.whl/modal/secret.pyi
@@ -0,0 +1,35 @@
+import modal.object
+import typing
+
+class _SecretHandle(modal.object._Handle):
+    ...
+
+class SecretHandle(modal.object.Handle):
+    def __init__(self):
+        ...
+
+
+class _Secret(modal.object._Provider):
+    @staticmethod
+    def from_dict(env_dict: typing.Dict[str, str] = {}, template_type=''):
+        ...
+
+    def __init__(self, env_dict: typing.Dict[str, str]):
+        ...
+
+    @staticmethod
+    def from_dotenv(path=None):
+        ...
+
+
+class Secret(modal.object.Provider):
+    def __init__(self, env_dict: typing.Dict[str, str]):
+        ...
+
+    @staticmethod
+    def from_dict(env_dict: typing.Dict[str, str] = {}, template_type=''):
+        ...
+
+    @staticmethod
+    def from_dotenv(path=None):
+        ...
