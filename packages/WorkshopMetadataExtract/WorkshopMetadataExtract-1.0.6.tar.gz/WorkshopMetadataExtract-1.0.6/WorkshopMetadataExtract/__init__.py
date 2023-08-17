cc293f87bb73d63ec1b4c26293e57ea17083c9c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 03:09:43 +0000

    Add package WorkshopMetadataExtract/1.0.6/WorkshopMetadataExtract-1.0.6.tar.gz

---
+++ packages/WorkshopMetadataExtract/WorkshopMetadataExtract-1.0.6.tar.gz/WorkshopMetadataExtract-1.0.6/WorkshopMetadataExtract/__init__.py
@@ -0,0 +1,13 @@
+"""
+:authors: laVashik
+:license: MIT license 
+
+:copyright: (c) 2023 laVashik
+"""
+
+from .WorkshopExplorer import *
+
+
+__author__ = 'laVashik'
+__version__ = '1.0.6'
+__email__ = 'lavash.assistant@gmail.com	'
