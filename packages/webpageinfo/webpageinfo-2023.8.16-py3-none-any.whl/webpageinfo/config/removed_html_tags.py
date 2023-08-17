572a1e4e3ac9a100287a25ca3fc86f1b52f8476e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 03:14:32 +0000

    Add package webpageinfo/2023.8.16/webpageinfo-2023.8.16-py3-none-any.whl

---
+++ packages/webpageinfo/webpageinfo-2023.8.16-py3-none-any.whl/webpageinfo/config/removed_html_tags.py
@@ -0,0 +1,14 @@
+removed_html_tags = {
+    "button",
+    "canvas",
+    "form",
+    "hr",
+    "input",
+    "link",
+    "meta",
+    "nav",
+    "script",
+    "select",
+    "style",
+    "video-js",
+}
