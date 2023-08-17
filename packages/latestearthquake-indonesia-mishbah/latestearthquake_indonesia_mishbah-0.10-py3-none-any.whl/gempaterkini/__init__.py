0a81d5778bf4233ce515e15f901727202d7b4330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 15:10:16 +0000

    Add package latestearthquake-indonesia-mishbah/0.10/latestearthquake_indonesia_mishbah-0.10-py3-none-any.whl

---
+++ packages/latestearthquake-indonesia-mishbah/latestearthquake_indonesia_mishbah-0.10-py3-none-any.whl/gempaterkini/__init__.py
@@ -0,0 +1,86 @@
+import requests
+import bs4
+
+description = 'To get the latest earthquake in Indonesia from BMKG.go.id'
+
+
+def ekstraksi_data():
+    """
+    Tanggal: 17 Mei 2023
+    Waktu: 22:29:25 WIB
+    Magnitudo: 4.6
+    Kedalaman: 29 km
+    Lokasi: LS=3.26 BT=135.30
+    Pusat Gempa: Pusat gempa berada di darat 26 km BaratLaut Nabire
+    Dirasakan: Dirasakan (Skala MMI): II - III Nabire
+    :return:
+    """
+    try:
+        content = requests.get('https://bmkg.go.id')
+    except Exception:
+        return None
+    if content.status_code == 200:
+        soup = bs4.BeautifulSoup(content.text,'html.parser')
+
+        result = soup.find('span', {'class': 'waktu'})
+        result = result.text.split(', ')
+        tanggal = result[0]
+        waktu = result[1]
+
+        result = soup.find('div', {'class': 'col-md-6 col-xs-6 gempabumi-detail no-padding'})
+        result = result.findChildren('li')
+        i = 0
+        magnitudo = None
+        kedalaman = None
+        ls = None
+        bt = None
+        lokasi = None
+        dirasakan = None
+
+        for res in result:
+            # print(i, res)
+            if i == 1:
+                magnitudo = res.text
+            elif i == 2:
+                kedalaman = res.text
+            elif i == 3:
+                koordinat = res.text.split(' - ')
+                ls = koordinat[0]
+                bt = koordinat[1]
+            elif i == 4:
+                lokasi = res.text
+            elif i == 5:
+                dirasakan = res.text
+            i = i + 1
+
+        hasil = dict()
+        hasil['tanggal'] = tanggal
+        hasil['waktu'] = waktu
+        hasil['magnitudo'] =  magnitudo
+        hasil['kedalaman'] =  kedalaman
+        hasil['koordinat'] = {'ls': ls, 'bt': bt}
+        hasil['lokasi'] = lokasi
+        hasil['dirasakan'] = dirasakan
+        return hasil
+    else:
+        return None
+
+
+def tampilkan_data(result):
+    if result is None:
+        print("Tidak bisa menemukan data gempa terkini")
+        return
+
+    print('Gempa terakhir berdasarkan BMKG')
+    print(f"Tanggal {result['tanggal']}")
+    print(f"Waktu {result['waktu']}")
+    print(f"Magnitudo {result['magnitudo']}")
+    print(f"Kedalaman {result['kedalaman']}")
+    print(f"Lokasi {result['lokasi']}")
+    print(f"Koordinat: LS={result['koordinat']['ls']}, BT={result['koordinat']['bt']}")
+    print(f"Dirasakan {result['dirasakan']}")
+
+if __name__ == '__main__':
+    print('Deskripsi package', description)
+    result = ekstraksi_data()
+    tampilkan_data(result)
