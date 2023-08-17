c076308b8c8cb86eee862e97960e664389aca278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 03:10:07 +0000

    Add package cyanobot/0.0.1.4/cyanobot-0.0.1.4-py3-none-any.whl

---
+++ packages/cyanobot/cyanobot-0.0.1.4-py3-none-any.whl/cyanobot/__init__.py
@@ -0,0 +1,21 @@
+from .Adapter import *
+from .Bot import *
+from .Logger import *
+from .Plugins import *
+import pyfiglet
+
+name='cyanobot'
+
+__version__ = '0.0.1.4'
+VERSION = __version__
+
+__all__ = [
+    'Adapter',
+    'Bot',
+    'Logger',
+    'Plugins'
+]
+
+result = pyfiglet.figlet_format(text="CyanoBot", font='stop')
+print(result)
+
