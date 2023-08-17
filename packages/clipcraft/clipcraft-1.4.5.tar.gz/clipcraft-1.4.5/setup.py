a92cb53b095e9d5ae9a5d96392aab8cf7cd26218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 05:07:53 +0000

    Add package clipcraft/1.4.5/clipcraft-1.4.5.tar.gz

---
+++ packages/clipcraft/clipcraft-1.4.5.tar.gz/clipcraft-1.4.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+with open('README.md', 'r', encoding='utf-8') as readme_file:
+    long_description = readme_file.read()
+
+setup(
+    name="clipcraft",
+    version="1.4.5",
+    description="A package for CLIP-based image and text processing.",
+    author='Morgan McKenzie',
+    author_email='morgancmckenziecs@gmail.com',
+    packages=["clipcraft"],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/mmckenzieORCEN/CLIPCraft',
+    install_requires=[
+        "transformers",
+        "Pillow",
+        "numpy",
+        "requests",
+    ],
+)
