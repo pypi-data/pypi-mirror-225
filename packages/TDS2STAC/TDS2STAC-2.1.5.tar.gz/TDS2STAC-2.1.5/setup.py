0669f54ee6c708e770ce3a0379ddf1ced30b775b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 14:09:13 +0000

    Add package TDS2STAC/2.1.5/TDS2STAC-2.1.5.tar.gz

---
+++ packages/TDS2STAC/TDS2STAC-2.1.5.tar.gz/TDS2STAC-2.1.5/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+"""Setup script for the TDS2STAC package."""
+
+from setuptools import setup, find_packages
+import versioneer
+from setuptools import setup
+
+setup(
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    long_description_content_type="text/x-rst",
+    packages=['tds2stac'],
+    install_requires=[requirement.strip() for requirement in open('requirements_dev.txt').readlines()],
+)
