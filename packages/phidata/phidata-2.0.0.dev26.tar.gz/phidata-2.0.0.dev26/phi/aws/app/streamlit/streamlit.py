11332620e5095c6bfcfdab2a365563392e1cbc97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 04:11:20 +0000

    Add package phidata/2.0.0.dev26/phidata-2.0.0.dev26.tar.gz

---
+++ packages/phidata/phidata-2.0.0.dev26.tar.gz/phidata-2.0.0.dev26/phi/aws/app/streamlit/streamlit.py
@@ -0,0 +1,27 @@
+from typing import Optional, Union, List
+
+from phi.aws.app.base import AwsApp, WorkspaceVolumeType, ContainerContext  # noqa: F401
+
+
+class Streamlit(AwsApp):
+    # -*- App Name
+    name: str = "streamlit"
+
+    # -*- Image Configuration
+    image_name: str = "phidata/streamlit"
+    image_tag: str = "1.23"
+    command: Optional[Union[str, List[str]]] = "streamlit hello"
+
+    # -*- App Ports
+    # Open a container port if open_container_port=True
+    open_container_port: bool = True
+    # Port number on the container
+    container_port: int = 8501
+    # Host port to map to the container port
+    host_port: int = 8501
+
+    # -*- ECS Configuration
+    ecs_task_cpu: str = "1024"
+    ecs_task_memory: str = "2048"
+    ecs_service_count: int = 1
+    assign_public_ip: Optional[bool] = True
