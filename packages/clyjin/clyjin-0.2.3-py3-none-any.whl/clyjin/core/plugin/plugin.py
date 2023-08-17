696fc21b6e39180db609a15ec389aaed9ae35500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 15:09:40 +0000

    Add package clyjin/0.2.3/clyjin-0.2.3-py3-none-any.whl

---
+++ packages/clyjin/clyjin-0.2.3-py3-none-any.whl/clyjin/core/plugin/plugin.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import clyjin
+from clyjin._project import get_version
+from clyjin.base.module import Module
+from clyjin.base.plugin import Plugin
+from clyjin.core.plugin.configurator import ConfiguratorModule
+
+
+class CorePlugin(Plugin):
+    NAME = "core"
+    MODULE_CLASSES = [
+        ConfiguratorModule,
+    ]
+    VERSION = get_version()
+
+    @classmethod
+    def get_version(cls) -> str | None:
+        cls.VERSION = clyjin.__version__
+        return super().get_version()
