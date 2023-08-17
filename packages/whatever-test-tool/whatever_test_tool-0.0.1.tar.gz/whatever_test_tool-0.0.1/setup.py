b4d7f49e675a0ec584f41a890c7f8752e5312ff2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 15:10:03 +0000

    Add package whatever-test-tool/0.0.1/whatever_test_tool-0.0.1.tar.gz

---
+++ packages/whatever-test-tool/whatever_test_tool-0.0.1.tar.gz/whatever_test_tool-0.0.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from setuptools import setup
+
+
+setup(
+    name="whatever_test_tool",
+    version="0.0.1",
+    author="chris_wu",
+    author_email="wuhan6582@gmail.com",
+    description="test tool",
+    py_modules=["test_app"]
+)
