bbe56efedd87134e7b230174686f89fdd3e4c83b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 04:12:04 +0000

    Add package DashAI/0.0.12/DashAI-0.0.12.tar.gz

---
+++ packages/DashAI/DashAI-0.0.12.tar.gz/DashAI-0.0.12/DashAI/back/dataloaders/classes/json_dataloader.py
@@ -0,0 +1,91 @@
+import os
+import shutil
+from typing import Dict
+
+from datasets import DatasetDict, load_dataset
+from starlette.datastructures import UploadFile
+
+from DashAI.back.dataloaders.classes.dataloader import BaseDataLoader
+
+
+class JSONDataLoader(BaseDataLoader):
+    """Data loader for tabular data in JSON files."""
+
+    COMPATIBLE_COMPONENTS = [
+        "TabularClassificationTask",
+        "TextClassificationTask",
+        "TranslationTask",
+    ]
+
+    def load_data(
+        self,
+        dataset_path: str,
+        params: Dict[str, any],
+        file: UploadFile = None,
+        url: str = None,
+    ) -> DatasetDict:
+        """
+        Load the dataset uploaded in JSON files in a DatasetDict.
+
+        Args:
+            dataset_path (str): Path of the folder with the dataset files.
+            params (dict[str, any]): Dict with the parameters for loading JSON files.
+                These parameters are:
+                    - data_key (str): The key of the json where the data is contained.
+
+            file (UploadFile, optional): File uploaded.
+                It's optional because is not necessary if dataset is uploaded in a URL.
+
+            url (str, optional): For load the dataset from an URL.
+                It's optional because is not necessary if dataset is uploaded in files.
+
+        Returns
+        -------
+            DatasetDict: Dataset loaded in Hugging Face format.
+        """
+        if file is None and url is None:
+            raise ValueError("Dataset should be a file or a url, both are None")
+        if file is not None and url is not None:
+            raise ValueError("Dataset should be a file or a url, got both")
+        if not isinstance(dataset_path, str):
+            raise TypeError(
+                f"dataset_path should be a string, got {type(dataset_path)}"
+            )
+        if not isinstance(params, dict):
+            raise TypeError(f"params should be a dict, got {type(params)}")
+
+        if "data_key" not in params.keys():
+            raise ValueError(
+                "Error loading JSON file: data_key parameter was not provided."
+            )
+        else:
+            if not isinstance(params["data_key"], str):
+                raise TypeError(
+                    "params['data_key'] should be a string, "
+                    f"got {type(params['data_key'])}"
+                )
+        if not isinstance(file, (UploadFile, type(None))):
+            raise TypeError(
+                f"file should be an uploaded file from user, got {type(file)}"
+            )
+        if not isinstance(url, (str, type(None))):
+            raise TypeError(
+                f"url should be a string with a web site adress, got {type(url)}"
+            )
+
+        field = params["data_key"]
+        if url:
+            dataset = load_dataset("json", data_files=url, field=field)
+        elif file:
+            files_path = self.extract_files(dataset_path, file)
+            if files_path.split("/")[-1] == "files":
+                try:
+                    dataset = load_dataset("json", data_dir=files_path, field=field)
+                finally:
+                    shutil.rmtree(dataset_path, ignore_errors=True)
+            else:
+                try:
+                    dataset = load_dataset("json", data_files=files_path, field=field)
+                finally:
+                    os.remove(files_path)
+        return dataset
---
+++ packages/DashAI/DashAI-0.0.12.tar.gz/DashAI-0.0.12/DashAI/back/models/hugging_face/distilbert_transformer.py
@@ -0,0 +1,165 @@
+import shutil
+
+import numpy as np
+from sklearn.exceptions import NotFittedError
+from transformers import (
+    DistilBertForSequenceClassification,
+    DistilBertTokenizer,
+    Trainer,
+    TrainingArguments,
+)
+
+from DashAI.back.dataloaders.classes.dashai_dataset import DashAIDataset
+from DashAI.back.models.text_classification_model import TextClassificationModel
+
+
+class DistilBertTransformer(TextClassificationModel):
+    """
+    Pre-trained transformer DistilBERT allowing English text classification.
+    """
+
+    def __init__(self, model=None, **kwargs):
+        """
+        Initialize the transformer class by calling the pretrained model and its
+        tokenizer. Include an attribute analogous to sklearn's check_is_fitted to
+        see if it was fine-tuned.
+        """
+        self.model_name = "distilbert-base-uncased"
+        self.tokenizer = DistilBertTokenizer.from_pretrained(self.model_name)
+        self.model = (
+            model
+            if model is not None
+            else DistilBertForSequenceClassification.from_pretrained(self.model_name)
+        )
+        self.fitted = model is not None
+        if model is None:
+            self.training_args = kwargs
+            self.batch_size = kwargs.pop("batch_size")
+            self.device = kwargs.pop("device")
+
+    def get_tokenizer(self, input_column: str, output_column: str):
+        """Tokenize input and output.
+
+        Parameters
+        ----------
+        input_column : str
+            name the input column to be tokenized.
+        output_column : str
+            name the output column to be tokenized.
+
+        Returns
+        -------
+        Function
+            Function for batch tokenization of the dataset.
+        """
+
+        def tokenize(batch):
+            return {
+                "input_ids": self.tokenizer(
+                    batch[input_column],
+                    padding="max_length",
+                    truncation=True,
+                    max_length=512,
+                )["input_ids"],
+                "attention_mask": self.tokenizer(
+                    batch[input_column],
+                    padding="max_length",
+                    truncation=True,
+                    max_length=512,
+                )["attention_mask"],
+                "labels": batch[output_column],
+            }
+
+        return tokenize
+
+    def fit(self, dataset: DashAIDataset):
+        """Fine-tune the pre-trained model.
+
+        Parameters
+        ----------
+        dataset : DashAIDataset
+            DashAIDataset with training data.
+
+        """
+
+        input_column = dataset.inputs_columns[0]
+        output_column = dataset.outputs_columns[0]
+
+        tokenizer_func = self.get_tokenizer(input_column, output_column)
+        dataset = dataset.map(tokenizer_func, batched=True, batch_size=8)
+        dataset.set_format("torch", columns=["input_ids", "attention_mask", "labels"])
+
+        # Arguments for fine-tuning
+        training_args = TrainingArguments(
+            output_dir="DashAI/back/user_models/temp_checkpoints_distilbert",
+            save_steps=1,
+            save_total_limit=1,
+            per_device_train_batch_size=self.batch_size,
+            per_device_eval_batch_size=self.batch_size,
+            no_cuda=self.device != "gpu",
+            **self.training_args,
+        )
+
+        # The Trainer class is used for fine-tuning the model.
+        trainer = Trainer(
+            model=self.model,
+            args=training_args,
+            train_dataset=dataset,
+        )
+
+        trainer.train()
+        self.fitted = True
+        shutil.rmtree(
+            "DashAI/back/user_models/temp_checkpoints_distilbert", ignore_errors=True
+        )
+
+        return self
+
+    def predict(self, dataset: DashAIDataset) -> np.array:
+        """Make a prediction with the fine-tuned model.
+
+        Parameters
+        ----------
+        dataset : DashAIDataset
+            DashAIDataset with text data.
+
+        Returns
+        -------
+        np.array
+            Numpy array with the probabilities for each class.
+        """
+        if not self.fitted:
+            raise NotFittedError(
+                f"This {self.__class__.__name__} instance is not fitted yet. Call 'fit'"
+                " with appropriate arguments before using this "
+                "estimator."
+            )
+
+        input_column = dataset.inputs_columns[0]
+        output_column = dataset.outputs_columns[0]
+        tokenizer_func = self.get_tokenizer(input_column, output_column)
+        dataset = dataset.map(tokenizer_func, batched=True, batch_size=len(dataset))
+        dataset.set_format("torch", columns=["input_ids", "attention_mask", "labels"])
+
+        probabilities = []
+
+        # Iterate over each batch in the dataset
+        for batch in dataset:
+            # Make sure that the tensors are in the correct device.
+            batch = {k: v.to(self.model.device) for k, v in batch.items()}
+
+            outputs = self.model(**batch)
+
+            # Takes the model probability using softmax
+            probs = outputs.logits.softmax(dim=-1)
+
+            probabilities.extend(probs.detach().cpu().numpy())
+        return np.array(probabilities)
+
+    def save(self, filename=None):
+        self.model.save_pretrained(filename)
+
+    @classmethod
+    def load(cls, filename):
+        model = DistilBertForSequenceClassification.from_pretrained(filename)
+        return cls(model=model)
