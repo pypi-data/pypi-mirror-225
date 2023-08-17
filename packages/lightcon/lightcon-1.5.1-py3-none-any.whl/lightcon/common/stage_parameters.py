aaaa757ec5ff0b4ed8218fc6dd841dbabcd9d4bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 03:13:54 +0000

    Add package lightcon/1.5.1/lightcon-1.5.1-py3-none-any.whl

---
+++ packages/lightcon/lightcon-1.5.1-py3-none-any.whl/lightcon/common/stage_parameters.py
@@ -0,0 +1,19 @@
+stage_parameters = {
+  "Standa 8MT184-13": {
+    "MotorName": "Standa 8CMA20",
+    "ref_travel_len": 334000,
+    # 1 mm/s, 2.5 mm/s, 5 mm/s
+    "test_speed_arr": [53, 133, 266],
+    "cycle_test_boundary": 0.1,
+    "ls_enable": 3,
+    "ls_invert": 3,
+    "ls_swap": 0},
+  "984-70A": {
+    "MotorName": "Oriental Motor PKP213D05B",
+    "ref_travel_len": 5800,
+    "test_speed_arr": [5, 10, 20],
+    "cycle_test_boundary": 0.025,
+    "ls_enable": 2,
+    "ls_invert": 2,
+    "ls_swap": 1}
+}
