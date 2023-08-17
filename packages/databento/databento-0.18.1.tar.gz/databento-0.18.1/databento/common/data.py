8857626d9bafa4576bfd74db1b674ffeebef7c3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 14:09:29 +0000

    Add package databento/0.18.1/databento-0.18.1.tar.gz

---
+++ packages/databento/databento-0.18.1.tar.gz/databento-0.18.1/databento/common/data.py
@@ -0,0 +1,103 @@
+from __future__ import annotations
+
+import numpy as np
+from databento_dbn import ImbalanceMsg
+from databento_dbn import InstrumentDefMsg
+from databento_dbn import MBOMsg
+from databento_dbn import MBP1Msg
+from databento_dbn import MBP10Msg
+from databento_dbn import OHLCVMsg
+from databento_dbn import Schema
+from databento_dbn import StatMsg
+from databento_dbn import TradeMsg
+
+from databento.live import DBNRecord
+
+
+SCHEMA_STRUCT_MAP: dict[Schema, type[DBNRecord]] = {
+    Schema.DEFINITION: InstrumentDefMsg,
+    Schema.IMBALANCE: ImbalanceMsg,
+    Schema.MBO: MBOMsg,
+    Schema.MBP_1: MBP1Msg,
+    Schema.MBP_10: MBP10Msg,
+    Schema.OHLCV_1S: OHLCVMsg,
+    Schema.OHLCV_1M: OHLCVMsg,
+    Schema.OHLCV_1H: OHLCVMsg,
+    Schema.OHLCV_1D: OHLCVMsg,
+    Schema.STATISTICS: StatMsg,
+    Schema.TBBO: MBP1Msg,
+    Schema.TRADES: TradeMsg,
+}
+
+DERIV_SCHEMAS = (
+    Schema.MBP_1,
+    Schema.MBP_10,
+    Schema.TBBO,
+    Schema.TRADES,
+)
+
+SCHEMA_DTYPES_MAP: dict[Schema, list[tuple[str, str]]] = {
+    Schema.MBO: MBOMsg._dtypes,
+    Schema.MBP_1: MBP1Msg._dtypes,
+    Schema.MBP_10: MBP10Msg._dtypes,
+    Schema.TBBO: MBP1Msg._dtypes,
+    Schema.TRADES: TradeMsg._dtypes,
+    Schema.OHLCV_1S: OHLCVMsg._dtypes,
+    Schema.OHLCV_1M: OHLCVMsg._dtypes,
+    Schema.OHLCV_1H: OHLCVMsg._dtypes,
+    Schema.OHLCV_1D: OHLCVMsg._dtypes,
+    Schema.DEFINITION: InstrumentDefMsg._dtypes,
+    Schema.IMBALANCE: ImbalanceMsg._dtypes,
+    Schema.STATISTICS: StatMsg._dtypes,
+}
+
+DEFINITION_CHARARRAY_COLUMNS = [
+    "currency",
+    "settl_currency",
+    "secsubtype",
+    "raw_symbol",
+    "group",
+    "exchange",
+    "asset",
+    "cfi",
+    "security_type",
+    "unit_of_measure",
+    "underlying",
+    "strike_price_currency",
+    "instrument_class",
+    "match_algorithm",
+    "security_update_action",
+    "user_defined_instrument",
+]
+
+DEFINITION_PRICE_COLUMNS = [
+    "min_price_increment",
+    "high_limit_price",
+    "low_limit_price",
+    "max_price_variation",
+    "trading_reference_price",
+    "min_price_increment_amount",
+    "price_ratio",
+    "strike_price",
+]
+
+DEFINITION_TYPE_MAX_MAP = {
+    x[0]: np.iinfo(x[1]).max
+    for x in InstrumentDefMsg._dtypes
+    if not isinstance(x[1], str)
+}
+
+SCHEMA_COLUMNS = {
+    Schema.MBO: MBOMsg._ordered_fields,
+    Schema.MBP_1: MBP1Msg._ordered_fields,
+    Schema.MBP_10: MBP10Msg._ordered_fields,
+    Schema.TBBO: MBP1Msg._ordered_fields,
+    Schema.TRADES: TradeMsg._ordered_fields,
+    Schema.OHLCV_1S: OHLCVMsg._ordered_fields,
+    Schema.OHLCV_1M: OHLCVMsg._ordered_fields,
+    Schema.OHLCV_1H: OHLCVMsg._ordered_fields,
+    Schema.OHLCV_1D: OHLCVMsg._ordered_fields,
+    Schema.DEFINITION: InstrumentDefMsg._ordered_fields,
+    Schema.IMBALANCE: ImbalanceMsg._ordered_fields,
+    Schema.STATISTICS: StatMsg._ordered_fields,
+}
