a23d3d440eecea45f6a4f7f8be538254891ee1cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 15:10:22 +0000

    Add package forest-timer/1.4.6/forest-timer-1.4.6.tar.gz

---
+++ packages/forest-timer/forest-timer-1.4.6.tar.gz/forest-timer-1.4.6/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='forest-timer',
+    version='1.4.6',
+    url='https://github.com/RayYangFromOrderly/forest-timer',
+    license='BSD',
+    description='Little pretty printer for nested for-loops',
+    author='RayYang',
+    author_email='ray.yang@ezorderly.com',
+
+
+    install_requires=['setuptools', 'termcolor'],
+
+)
