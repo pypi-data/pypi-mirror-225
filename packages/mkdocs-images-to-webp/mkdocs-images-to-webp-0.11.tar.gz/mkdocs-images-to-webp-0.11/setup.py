a3c6e58a567345d194e75a84cff47c3dc73d852a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 03:14:30 +0000

    Add package mkdocs-images-to-webp/0.11/mkdocs-images-to-webp-0.11.tar.gz

---
+++ packages/mkdocs-images-to-webp/mkdocs-images-to-webp-0.11.tar.gz/mkdocs-images-to-webp-0.11/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='mkdocs-images-to-webp',
+    version='0.11',
+    install_requires=[
+        'mkdocs == 1.4.2',
+        'pillow',
+        'importlib-metadata; python_version == "3.8"',
+    ],
+    author='mur4d1n',
+    author_email='mur4d1n@yandex.ru',
+    url='https://github.com/mur4d1n',
+    download_url='https://github.com/mur4d1n-lib/mkdocs-images-to-webp/archive/refs/tags/v_011.tar.gz',
+    keywords=['mkdocs', 'images', 'webp'],
+
+    entry_points={
+        'mkdocs.plugins': [
+            'images-to-webp = mkdocs_images_to_webp.plugin:ConvertImagesToWebpPlugin',
+        ]
+    }
+)
