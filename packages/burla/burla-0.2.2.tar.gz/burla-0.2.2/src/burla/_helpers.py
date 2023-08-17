b1792b51a7e29575bbe9708cc82fcc0869f0890f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 05:07:58 +0000

    Add package burla/0.2.2/burla-0.2.2.tar.gz

---
+++ packages/burla/burla-0.2.2.tar.gz/burla-0.2.2/src/burla/_helpers.py
@@ -0,0 +1,2 @@
+def nopath_warning(message, category, filename, lineno, line=None):
+    return f"{category.__name__}: {message}\n"
---
+++ packages/burla/burla-0.2.2.tar.gz/burla-0.2.2/src/burla/_logstream.py
@@ -0,0 +1,36 @@
+import threading
+import queue
+from time import sleep
+from datetime import datetime, timezone
+
+
+def print_logs_from_queue(log_queue: queue.PriorityQueue, stop_event: threading.Event):
+    last_log_timestamp = None
+    last_log_printed_at = None
+    is_first_log = True
+    while True:
+        try:
+            timestamp, log_message = log_queue.get_nowait()
+
+            if is_first_log:
+                sleep(6)  # remote_parallel_map.JOB_STATUS_POLL_RATE_SEC
+                is_first_log = False
+
+            current_epoch = datetime.now(timezone.utc).timestamp()
+
+            last_log_printed_delta = current_epoch - (last_log_printed_at or current_epoch)
+            last_log_occurred_delta = timestamp - (last_log_timestamp or timestamp)
+            sleep_time = max(last_log_occurred_delta - last_log_printed_delta, 0)
+
+            # if last_log_occurred_delta - last_log_printed_delta < 0:
+            #     lateness = (last_log_occurred_delta - last_log_printed_delta) * -1
+            #     print(f"LATE!! Log should have printed {lateness} seconds ago!")
+
+            sleep(sleep_time)
+            print(log_message)
+            last_log_printed_at = datetime.now(timezone.utc).timestamp()
+            last_log_timestamp = timestamp
+        except queue.Empty:
+            if stop_event.is_set():
+                return
+            sleep(0.5)
