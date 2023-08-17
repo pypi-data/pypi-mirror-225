5fb0bbebfd324c7a65f067ecf1420405a9387a27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 03:13:54 +0000

    Add package buteo/0.9.40/buteo-0.9.40.tar.gz

---
+++ packages/buteo/buteo-0.9.40.tar.gz/buteo-0.9.40/buteo/ai/__init__.py
@@ -0,0 +1,9 @@
+"""
+Tools for working with sentinel data
+"""
+from .augmentation import *
+from .augmentation_funcs import *
+from .augmentation_utils import *
+from .scalers import *
+from .selection import *
+from .encoding import *
