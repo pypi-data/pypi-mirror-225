a5117c6ad5fc8e4075a82e370cc193855951bbee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 04:11:20 +0000

    Add package neurum/1.0.7/neurum-1.0.7.tar.gz

---
+++ packages/neurum/neurum-1.0.7.tar.gz/neurum-1.0.7/neurum/ask_realtime.py
@@ -0,0 +1,6 @@
+from bardapi import Bard
+def ask_realtime(prompt : str):
+    token="ZwgDyGQ6NLqpLei6zs3ZSeuNv_TOf-hFlFKM1uQf3GckA3mRp9cQNBFzvzmQoc_xD8uWRg."
+    chatbot = Bard(token=token)
+    ans=chatbot.get_answer(f"{prompt}")['content']
+    return ans
