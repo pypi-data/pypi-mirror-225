4415138ed205bfc302d8ec57ee3c381b23f04148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 03:12:48 +0000

    Add package qalita/1.1.3/qalita-1.1.3-py3-none-any.whl

---
+++ packages/qalita/qalita-1.1.3-py3-none-any.whl/qalita/internal/utils.py
@@ -0,0 +1,10 @@
+"""
+# QALITA (c) COPYRIGHT 2023 - ALL RIGHTS RESERVED -
+"""
+from qalita.internal.logger import init_logging
+
+logger = init_logging()
+
+
+def get_version():
+    return "1.1.3"
