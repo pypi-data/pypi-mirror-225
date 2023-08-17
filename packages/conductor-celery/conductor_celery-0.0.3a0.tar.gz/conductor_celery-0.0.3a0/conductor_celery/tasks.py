c5cb15ac41b07811689efc4d5ad03f108722b813
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 14:09:18 +0000

    Add package conductor-celery/0.0.3a0/conductor_celery-0.0.3a0.tar.gz

---
+++ packages/conductor-celery/conductor_celery-0.0.3a0.tar.gz/conductor_celery-0.0.3a0/conductor_celery/tasks.py
@@ -0,0 +1,60 @@
+from typing import Any
+
+from celery import Task, shared_task
+
+from conductor_celery.utils import configure_runner
+from conductor_celery.utils import update_task as real_update_task
+
+
+class ConductorPollTask(Task):
+    pass
+
+
+class ConductorTask(Task):
+    """
+    This handle a canductor task
+    """
+
+    def apply(
+        self,
+        args=None,
+        kwargs=None,
+        link=None,
+        link_error=None,
+        task_id=None,
+        retries=None,
+        throw=None,
+        logfile=None,
+        loglevel=None,
+        headers=None,
+        **options,
+    ) -> Any:
+        runner = configure_runner(server_api_url=self.app.conf["conductor_server_api_url"], name=self.name, debug=True)
+        conductor_task = runner.poll_task()
+
+        ret = super().apply(
+            None,
+            conductor_task.input_data,
+            link,
+            link_error,
+            task_id,
+            retries,
+            throw,
+            logfile,
+            loglevel,
+            headers,
+            **options,
+        )
+
+        runner.update_task(
+            real_update_task(
+                conductor_task.task_id, conductor_task.workflow_instance_id, conductor_task.worker_id, ret.result
+            )
+        )
+        return ret
+
+
+@shared_task(bind=True)
+def update_task(self, name, task_id, workflow_instance_id, worker_id, values):
+    runner = configure_runner(server_api_url=self.app.conf["conductor_server_api_url"], name=name, debug=True)
+    runner.update_task(real_update_task(task_id, workflow_instance_id, worker_id, values))
