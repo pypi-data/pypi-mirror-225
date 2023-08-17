16ccf46b0dd05e80c8f046e37d6c692582dea3e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 03:13:46 +0000

    Add package csle-attacker/0.3.0/csle_attacker-0.3.0.tar.gz

---
+++ packages/csle-attacker/csle_attacker-0.3.0.tar.gz/csle_attacker-0.3.0/tests/test_shell_util.py
@@ -0,0 +1,13 @@
+
+class TestShellUtilSuite:
+    """
+    Test suite for shell_util.py
+    """
+
+    def test_execute_service_login_helper(self) -> None:
+        """
+        Tests the execute_service_login_helper function
+
+        :return: None
+        """
+        assert True
