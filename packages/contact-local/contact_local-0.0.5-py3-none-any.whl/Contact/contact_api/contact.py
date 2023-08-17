17a582aae2caaf28c53bf705a7a384ed6a401c62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 03:14:32 +0000

    Add package contact-local/0.0.5/contact_local-0.0.5-py3-none-any.whl

---
+++ packages/contact-local/contact_local-0.0.5-py3-none-any.whl/Contact/contact_api/contact.py
@@ -0,0 +1,152 @@
+import mysql.connector
+from dotenv import load_dotenv
+from logger_local.LoggerLocal import logger_local
+from circles_local_database_python.connection import Connection
+from datetime import datetime
+from circles_local_database_python import generic_crud
+from logger_local.LoggerComponentEnum import LoggerComponentEnum
+
+
+
+load_dotenv()
+
+
+obj = {
+    'component_id': 123,
+    'componenet_name':'contact-local',
+    'comopmnenet_categroy':LoggerComponentEnum.ComponentCategory.Code.value,
+    'developer_email':'shavit.m@circ.zone'
+
+    
+}
+logger_local.init(object=obj)
+
+
+
+db = Connection("contact")
+connection = db.connect()
+
+
+class contact(generic_crud.GenericCRUD): 
+
+    def __init__(self):
+        super().__init__(connection)
+
+    @staticmethod
+    def insert(first_name,last_name,phone,
+                    birthday,email,location,job_title,organization):
+        try:
+            object1 = {
+                'first_name': first_name
+            }
+            logger_local.start(object=object1)
+            connection.connect()
+            cursor = connection.cursor()
+            insert_query = """
+            INSERT INTO contact.contact_table (
+                first_name,
+                last_name,
+                phone1,
+                birthday,
+                email1,
+                address1_street,
+                job_title,
+                organization
+            )
+            VALUES (
+                %s, %s, %s, %s, %s, %s, %s,%s
+            )
+            """
+
+            data_values = (
+                first_name,
+                last_name,
+                phone,
+                birthday,
+                email,
+                location,
+                job_title,
+                organization
+            )
+
+            cursor.execute(insert_query, data_values)
+            cursor.close()
+            connection.commit()
+            connection.close()
+        except mysql.connector.Error as err:
+            logger_local.error(f"Error: {err}")
+        finally:
+            logger_local.info("contact added "+first_name)
+
+    @staticmethod
+    def update(person_id, name_prefix, first_name, additional_name, job_title, id):
+        try:
+            object1 = {
+            'idToChange' :id
+            }
+            logger_local.start(object=object1)
+            connection.connect()
+            cursor = connection.cursor()
+            update_query = """
+            UPDATE contact.contact_table
+            SET
+                person_id = %s,
+                name_prefix = %s,
+                first_name = %s,
+                additional_name = %s,
+                job_title=%s
+            WHERE
+                id = %s
+            """
+
+            data_values = (
+                person_id,
+                name_prefix,
+                first_name,
+                additional_name,
+                job_title,
+                id
+            )
+
+            cursor.execute(update_query, data_values)
+            cursor.close()
+            connection.commit()
+            connection.close()
+        except mysql.connector.Error as err:
+            logger_local.exception(f"Error: {err}", err)
+        finally:
+            logger_local.info("contact updated"+str(id))
+            cursor.close()
+
+    @staticmethod
+    def delete(contact_id):
+        try:
+            object1 = {
+                'contact_id': contact_id,
+            }
+            logger_local.start(object=object1)
+            connection.connect()
+            cursor = connection.cursor()
+            update_query = """
+            UPDATE contact.contact_table
+            SET
+                last_sync_timestamp = %s
+            WHERE
+                id = %s
+            """
+
+            current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+            data_values = (
+                current_time,
+                contact_id
+            )
+
+            cursor.execute(update_query, data_values)
+            cursor.close()
+            connection.commit()
+            connection.close()
+        except mysql.connector.Error as err:
+            logger_local.exception(f"Error: {err}", err)
+        finally:
+            logger_local.info("contact deleted"+str(id))
---
+++ packages/contact-local/contact_local-0.0.5-py3-none-any.whl/Contact/tests/test_contact.py
@@ -0,0 +1,72 @@
+import os
+import sys
+script_directory = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(script_directory, '..'))
+
+import pytest
+from dotenv import load_dotenv
+from datetime import datetime
+from db import contactView
+from contact_api import contact
+from logger_local.LoggerLocal import logger_local
+from logger_local.LoggerComponentEnum import LoggerComponentEnum
+
+
+load_dotenv()
+
+obj = {
+    'component_id': 123,
+    'component_name':'contact-local',
+    'component_category':LoggerComponentEnum.ComponentCategory.Code.value,
+    'testing_framework' : LoggerComponentEnum.testingFramework.pytest.value
+    
+}
+logger_local.init(object=obj)
+
+
+def test_insert_select():
+    object1 = {
+            'first_name': "ai"
+        }
+    logger_local.start(object=object1)
+    contact.contact.insert("ai","bye","0539229102","","sami@gmail.com","haifa","sniper","wha")
+    contactRes = contactView.get_contact_by_first_name('ai')
+    contactRes is not None
+    logger_local.info("contact added "+"ai")
+
+
+
+
+def test_update():
+    object1 = {
+           'idToChange' :99
+        }
+    logger_local.start(object=object1)
+    contact.contact.update(11, "sami", "ve", "sumo", "ars", 99)
+    contactRes = contactView.get_contact_by_id(99)
+    print(contactRes[3])
+    assert contactRes[3] == 11
+    assert contactRes[6] == "sami"
+    assert contactRes[7] == "ve"
+    assert contactRes[8] == "sumo"
+    assert contactRes[17] == "ars"
+    logger_local.info("contact updated"+str(id))
+
+
+
+def test_select_valid():
+    logger_local.start()
+    contactRes = contactView.get_contact_by_id(99)
+    assert contactRes is not None 
+    logger_local.end("contact selected")
+
+
+
+def test_select_invalid():
+    logger_local.start()
+    contactRes = contactView.get_contact_by_id(12121231765)
+    assert contactRes is None
+    logger_local.end("contact not selected")
+
+
+test_select_valid()
