b84120eba9435d7ad009d32b5bca6ce073083cd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 04:12:22 +0000

    Add package indexrl/0.1.4/indexrl-0.1.4.tar.gz

---
+++ packages/indexrl/indexrl-0.1.4.tar.gz/indexrl-0.1.4/README.md
@@ -0,0 +1,2 @@
+# IndexRL
+Using reinfrocement learning to generate mathematical operations for remote sensing indices.
---
+++ packages/indexrl/indexrl-0.1.4.tar.gz/indexrl-0.1.4/indexrl/configs/config_transfomer.py
@@ -0,0 +1,43 @@
+# -----------------------------------------------------------------------------
+# default config values designed to train a gpt2 (124M) on OpenWebText
+# I/O
+out_dir = "out"
+eval_interval = 2000
+log_interval = 1
+eval_iters = 200
+eval_only = False  # if True, script exits right after the first eval
+always_save_checkpoint = True  # if True, always save a checkpoint after each eval
+init_from = "scratch"  # 'scratch' or 'resume' or 'gpt2*'
+# wandb logging
+wandb_log = False  # disabled by default
+wandb_project = "owt"
+wandb_run_name = "gpt2"  # 'run' + str(time.time())
+# data
+dataset = "openwebtext"
+gradient_accumulation_steps = 1  # used to simulate larger batch sizes
+batch_size = 12  # if gradient_accumulation_steps > 1, this is the micro-batch size
+block_size = 16
+# model
+n_layer = 4
+n_head = 4
+n_embd = 128
+dropout = 0.0  # for pretraining 0 is good, for finetuning try 0.1+
+bias = False  # do we use bias inside LayerNorm and Linear layers?
+# adamw optimizer
+learning_rate = 1e-4  # max learning rate
+max_iters = 2000  # total number of training iterations
+weight_decay = 1e-1
+beta1 = 0.9
+beta2 = 0.95
+grad_clip = 1.0  # clip gradients at this value, or disable if == 0.0
+# learning rate decay settings
+decay_lr = True  # whether to decay the learning rate
+warmup_iters = 2000  # how many steps to warm up for
+lr_decay_iters = 2000  # should be ~= max_iters per Chinchilla
+min_lr = 6e-5  # minimum learning rate, should be ~= learning_rate/10 per Chinchilla
+# DDP settings
+backend = "nccl"  # 'nccl', 'gloo', etc.
+# system
+device = "cuda"  # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1' etc., or try 'mps' on macbooks
+dtype = "bfloat16"  # 'float32', 'bfloat16', or 'float16', the latter will auto implement a GradScaler
+compile = True  # use PyTorch 2.0 to compile the model to be faster
