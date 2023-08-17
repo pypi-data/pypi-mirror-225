95d2984de5dd255dada33049118ee0161716453e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 03:11:16 +0000

    Add package encord-active/0.1.70/encord_active-0.1.70.tar.gz

---
+++ packages/encord-active/encord_active-0.1.70.tar.gz/encord_active-0.1.70/src/encord_active/lib/constants.py
@@ -0,0 +1,6 @@
+DOCS_URL = "https://docs.encord.com/docs/active-overview"
+GITHUB_URL = "https://github.com/encord-team/encord-active"
+JOIN_DISCORD_URL = "https://discord.gg/TU6yT7Uvx3"
+DISCORD_URL = "https://discord.gg/TU6yT7Uvx3"
+
+ENCORD_EMAIL = "active@encord.com"
