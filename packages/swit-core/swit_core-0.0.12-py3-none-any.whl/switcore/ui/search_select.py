368c3222086f4c714d6ac3e39eb3a2e7ef77a9e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 15:10:10 +0000

    Add package swit-core/0.0.12/swit_core-0.0.12-py3-none-any.whl

---
+++ packages/swit-core/swit_core-0.0.12-py3-none-any.whl/switcore/ui/search_select.py
@@ -0,0 +1,12 @@
+# from pydantic import BaseModel
+#
+#
+#
+#
+# class SearchSelect(BaseModel):
+#     type: str = 'search_select'
+#     placeholder: str | None
+#     multiselect: bool = False
+#     # trigger_on_input: bool
+#     value: list[str] | None
+#     options: list[Option] = []
---
+++ packages/swit-core/swit_core-0.0.12-py3-none-any.whl/switcore/ui/select.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+
+from switcore.ui.element_components import StaticAction
+
+
+class Option(BaseModel):
+    label: str
+    action_id: str
+    static_action: StaticAction | None = None
+
+
+class Style(BaseModel):
+    variant: str | None
+
+
+class Select(BaseModel):
+    type: str = 'select'
+    placeholder: str | None
+    multiselect: bool = False
+    trigger_on_input: bool = False
+    value: list[str] | None
+    options: list[Option] = []
+    style: Style | None = None
