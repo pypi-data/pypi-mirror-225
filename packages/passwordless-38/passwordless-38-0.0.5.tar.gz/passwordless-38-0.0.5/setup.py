7d49435dbf8556b6a8881f22a22933e07bcf594f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 03:09:59 +0000

    Add package passwordless-38/0.0.5/passwordless-38-0.0.5.tar.gz

---
+++ packages/passwordless-38/passwordless-38-0.0.5.tar.gz/passwordless-38-0.0.5/setup.py
@@ -0,0 +1,24 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='passwordless-38',
+    version='0.0.5',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    author='Matthew Fiallos',
+    author_email='matthew.fiallos@randstadusa.com',
+    description='A client library for Bitwarden''s Passwordless.dev API',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    #url='https://github.com/yourusername/passwordless_38',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
