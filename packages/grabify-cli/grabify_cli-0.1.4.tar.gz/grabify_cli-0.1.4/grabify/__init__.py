c99cd9537c7f19bc764cf50430cd9fc39bd60332
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 03:10:39 +0000

    Add package grabify-cli/0.1.4/grabify_cli-0.1.4.tar.gz

---
+++ packages/grabify-cli/grabify_cli-0.1.4.tar.gz/grabify_cli-0.1.4/grabify/__init__.py
@@ -0,0 +1,13 @@
+"""
+Grabify
+~~~~~~~~~~~~~~~~~~~
+
+A command-line tool that allows you to download
+artwork and metadata from Spotify tracks and albums.
+
+:copyright: (c) 2023 woidzero
+:license: MIT, see LICENSE for more details.
+"""
+
+__version__ = "0.1.3"
+__prog_name__ = "grabify"
