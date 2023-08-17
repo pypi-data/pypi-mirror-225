d4c398b7ba3a4361ebaccd589e62df0ff0ff34f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 05:07:33 +0000

    Add package cumulus-library/1.2.0/cumulus_library-1.2.0.tar.gz

---
+++ packages/cumulus-library/cumulus_library-1.2.0.tar.gz/cumulus_library-1.2.0/cumulus_library/errors.py
@@ -0,0 +1,19 @@
+"""Error types"""
+
+
+class LibraryError(Exception):
+    """
+    Package level error
+    """
+
+    pass
+
+
+class LibrarySchemaError(Exception):
+    """
+    Package level error
+    """
+
+
+class StudyManifestParsingError(Exception):
+    """Basic error for StudyManifestParser"""
