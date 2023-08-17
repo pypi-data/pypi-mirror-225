95092e5a15f63320da074888cd9e7e44a53ad4db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 04:13:44 +0000

    Add package memray/1.9.1/memray-1.9.1-cp312-cp312-manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/memray/memray-1.9.1-cp312-cp312-manylinux_2_17_i686.manylinux2014_i686.whl/memray/reporters/templates/__init__.py
@@ -0,0 +1,59 @@
+"""Templates to render reports in HTML."""
+from functools import lru_cache
+from typing import Any
+from typing import Dict
+from typing import Iterable
+from typing import Union
+
+import jinja2
+from markupsafe import Markup
+
+from memray import MemorySnapshot
+from memray import Metadata
+
+
+@lru_cache(maxsize=1)
+def get_render_environment() -> jinja2.Environment:
+    loader = jinja2.PackageLoader("memray.reporters")
+    env = jinja2.Environment(loader=loader)
+
+    def include_file(name: str) -> Markup:
+        """Include a file from the templates directory without
+        interpolating its contents"""
+        source, *_ = loader.get_source(env, name)
+        return Markup(source)
+
+    env.globals["include_file"] = include_file
+    env.policies["json.dumps_kwargs"] = {"sort_keys": True, "separators": (",", ":")}
+    return env
+
+
+def get_report_title(*, kind: str, show_memory_leaks: bool) -> str:
+    if show_memory_leaks:
+        return f"{kind} report (memory leaks)"
+    return f"{kind} report"
+
+
+def render_report(
+    *,
+    kind: str,
+    data: Union[Dict[str, Any], Iterable[Dict[str, Any]]],
+    metadata: Metadata,
+    memory_records: Iterable[MemorySnapshot],
+    show_memory_leaks: bool,
+    merge_threads: bool,
+) -> str:
+    env = get_render_environment()
+    template = env.get_template(kind + ".html")
+
+    pretty_kind = kind.replace("_", " ")
+    title = get_report_title(kind=pretty_kind, show_memory_leaks=show_memory_leaks)
+    return template.render(
+        kind=pretty_kind,
+        title=title,
+        data=data,
+        metadata=metadata,
+        memory_records=memory_records,
+        show_memory_leaks=show_memory_leaks,
+        merge_threads=merge_threads,
+    )
