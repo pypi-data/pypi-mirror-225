3bcb99605c9e4c13e9c59d0d3cea4bf63cb2c4ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 03:09:52 +0000

    Add package codelink/0.0.2/codelink-0.0.2-py3-none-any.whl

---
+++ packages/codelink/codelink-0.0.2-py3-none-any.whl/codelink/sockets/shape.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+# ***************************************************************************
+# *   Copyright (c) 2023 Ronny Scharf-W. <ronny.scharf08@gmail.com>         *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU Lesser General Public License (LGPL)    *
+# *   as published by the Free Software Foundation; either version 2 of     *
+# *   the License, or (at your option) any later version.                   *
+# *   for detail see the LICENCE text file.                                 *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU Library General Public License for more details.                  *
+# *                                                                         *
+# *   You should have received a copy of the GNU Library General Public     *
+# *   License along with this program; if not, write to the Free Software   *
+# *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+# *   USA                                                                   *
+# *                                                                         *
+# ***************************************************************************
+
+from __future__ import annotations
+from typing import TYPE_CHECKING, Optional
+
+import FreeCAD
+import Part
+
+import PySide2.QtGui as QtGui
+import PySide2.QtWidgets as QtWidgets
+
+from socket_widget import SocketWidget
+
+if TYPE_CHECKING:
+	from node_item import NodeItem
+
+
+class Shape(SocketWidget):
+	def __init__(
+			self, undo_stack: QtWidgets.QUndoStack, label: str = "Shape", is_input: bool = True,
+			data: str = str(Part.Shape()), parent_node: Optional[NodeItem] = None,
+			parent_widget: Optional[QtWidgets.QWidget] = None
+	) -> None:
+		super().__init__(undo_stack, label, is_input, data, parent_node, parent_widget)
+
+		# Pin setup
+		self._pin_item.color = QtGui.QColor("blue")
+		self._pin_item.pin_type = Part.Shape
+
+	# --------------- Socket data ---------------
+
+	def input_data(self) -> list:
+		result: list = []
+		if self._pin_item.has_edges():
+			for edge in self._pin_item.edges:
+				pre_node: NodeItem = edge.start_pin.parent_node
+				if len(pre_node.sub_scene.nodes) > 0:
+					result.append(pre_node.linked_lowest_socket(edge.start_pin.socket_widget).pin)
+				else:
+					result.append(edge.start_pin)
+		else:
+			linked_highest: SocketWidget = self.parent_node.linked_highest_socket(self)
+			if linked_highest != self:
+				result.extend(linked_highest.input_data())
+
+		if len(result) == 0:
+			result.append(Part.Shape())
+
+		return result
