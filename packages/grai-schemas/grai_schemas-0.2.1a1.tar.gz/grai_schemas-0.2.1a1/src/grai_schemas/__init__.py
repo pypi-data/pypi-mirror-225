f0b503d6df6701db8fe5c0723c2d8ce7a49acb84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 04:13:34 +0000

    Add package grai-schemas/0.2.1a1/grai_schemas-0.2.1a1.tar.gz

---
+++ packages/grai-schemas/grai_schemas-0.2.1a1.tar.gz/grai_schemas-0.2.1a1/src/grai_schemas/__init__.py
@@ -0,0 +1,13 @@
+from grai_schemas import (
+    base,
+    generics,
+    human_ids,
+    package_definitions,
+    schema,
+    serializers,
+    utilities,
+    v1,
+)
+from grai_schemas.package_definitions import config
+
+__version__ = "0.2.1-alpha1"
