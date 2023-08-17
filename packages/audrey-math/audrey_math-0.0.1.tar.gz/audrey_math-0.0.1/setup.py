d8c4b22c9dda014b6da1e01b9ce74a5b70b5f4d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 04:12:30 +0000

    Add package audrey-math/0.0.1/audrey_math-0.0.1.tar.gz

---
+++ packages/audrey-math/audrey_math-0.0.1.tar.gz/audrey_math-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.1"
+DESCRIPTION = "first python package!"
+
+setup (
+    # name should match the name of module (second folder)
+    name = "audrey_math",
+    version = VERSION,
+    author = "Audrey Lin",
+    description = DESCRIPTION,
+    packages = find_packages(),
+    # add any additional packages that need to be installed w/ my package
+    # install_requires = []
+    keywords = ["python", "first package"]
+    #provide more information about my package
+    # classifiers = []
+)
