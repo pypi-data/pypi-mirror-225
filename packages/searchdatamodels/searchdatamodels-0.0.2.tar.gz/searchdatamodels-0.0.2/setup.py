9aa15a8ee010d797a8745d4da9682224a1021f3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-17 05:08:20 +0000

    Add package searchdatamodels/0.0.2/searchdatamodels-0.0.2.tar.gz

---
+++ packages/searchdatamodels/searchdatamodels-0.0.2.tar.gz/searchdatamodels-0.0.2/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+ 
+setuptools.setup(
+    name="searchdatamodels",
+    version="0.0.2",
+    author="James Baker",
+    author_email="james@talentium.io", 
+    license="MIT",
+ 
+    # classifiers like program is suitable for python3, just leave as it is.
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
