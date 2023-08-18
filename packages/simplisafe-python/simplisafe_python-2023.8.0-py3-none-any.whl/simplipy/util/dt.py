7134d476b3afdeba4399cbb7304c711ae10e7c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 05:08:47 +0000

    Add package simplisafe-python/2023.8.0/simplisafe_python-2023.8.0-py3-none-any.whl

---
+++ packages/simplisafe-python/simplisafe_python-2023.8.0-py3-none-any.whl/simplipy/util/dt.py
@@ -0,0 +1,18 @@
+"""Define datetime utilities."""
+from datetime import datetime
+
+import pytz
+
+UTC = pytz.utc
+
+
+def utc_from_timestamp(timestamp: float) -> datetime:
+    """Return a UTC time from a timestamp.
+
+    Args:
+        timestamp: The epoch to convert.
+
+    Returns:
+        A parsed ``datetime.datetime`` object.
+    """
+    return UTC.localize(datetime.utcfromtimestamp(timestamp))
