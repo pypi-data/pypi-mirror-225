a8c670755bb340bd93db6c93a37f0715945ca870
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 15:09:35 +0000

    Add package safucall/0.1.2/safucall-0.1.2-py3-none-any.whl

---
+++ packages/safucall/safucall-0.1.2-py3-none-any.whl/safucall/call.py
@@ -0,0 +1,35 @@
+from eth_utils import to_checksum_address
+from . import Signature
+from web3 import Web3
+
+class Call:
+    def __init__(self, target, function, returns=None, chain=None, block="latest", _w3=None):
+        self.target = to_checksum_address(target)
+        self.block = block
+
+        if isinstance(function, list):
+            self.function, *self.args = function
+        else:
+            self.function = function
+            self.args = None
+
+        self.w3 = _w3
+        self.signature = Signature(self.function)
+        self.returns = returns
+
+    @property
+    def data(self):
+        return self.signature.encode_data(self.args)
+
+    def decode_output(self, output):
+        decoded = self.signature.decode_data(output)
+        if self.returns:
+            return {name: handler(value) if handler else value for (name, handler), value in zip(self.returns, decoded)}
+        else:
+            return decoded if len(decoded) > 1 else decoded[0]
+
+    def __call__(self, args=None):
+        args = args or self.args
+        calldata = self.signature.encode_data(args)
+        output = self.w3.eth.call({"to": self.target, "data": calldata}, block_identifier=self.block)
+        return self.decode_output(output)
---
+++ packages/safucall/safucall-0.1.2-py3-none-any.whl/safucall/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="safucall",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=[
+        "web3==5.31.3"
+    ],
+)
