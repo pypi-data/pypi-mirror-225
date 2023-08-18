ea5eef89a69c2db38248dfe0d642a8a74dee0675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 05:08:46 +0000

    Add package ckanext-nswdesignsystem/0.1.6/ckanext_nswdesignsystem-0.1.6-py3-none-any.whl

---
+++ packages/ckanext-nswdesignsystem/ckanext_nswdesignsystem-0.1.6-py3-none-any.whl/ckanext/nswdesignsystem/types.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from typing import Any
+
+from typing_extensions import NotRequired, TypedDict
+
+
+class NavDict(TypedDict):
+    href: str
+    label: str
+    current: NotRequired[bool]
+    active: NotRequired[bool]
+    subnav: NotRequired[SubNavDict]
+    attrs: NotRequired[dict[str, Any]]
+
+
+class SubNavDict(TypedDict):
+    label: NotRequired[str]
+    description: NotRequired[str]
+    children: list[NavDict]
