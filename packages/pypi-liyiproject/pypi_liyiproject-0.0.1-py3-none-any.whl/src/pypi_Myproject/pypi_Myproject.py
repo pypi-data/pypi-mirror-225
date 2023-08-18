0458120457fbf8837ed2c28ab3e8a17a9f305b4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 14:10:06 +0000

    Add package pypi-liyiproject/0.0.1/pypi_liyiproject-0.0.1-py3-none-any.whl

---
+++ packages/pypi-liyiproject/pypi_liyiproject-0.0.1-py3-none-any.whl/src/pypi_Myproject/pypi_Myproject.py
@@ -0,0 +1,22 @@
+import os
+
+
+class Test_operation():
+    def Local_ip(self, ip_address):
+        return ip_address
+
+    def Local_hostname(self, hostname):
+        return hostname
+
+    def operation(self, statement):
+        info = os.popen(statement)
+        return info.read()
+
+    def test_mat(self, add_number):
+        return add_number + 1
+
+
+if __name__ == "__main__":
+    print('')
+else:
+    Test_operation
