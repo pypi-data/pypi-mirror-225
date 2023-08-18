7017fcf721384570c33efd1f257c8df6d9db57f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 05:09:18 +0000

    Add package UnBIAS/0.2/UnBIAS-0.2.tar.gz

---
+++ packages/UnBIAS/UnBIAS-0.2.tar.gz/UnBIAS-0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="UnBIAS",
+    version="0.2",
+    packages=find_packages(),
+    install_requires=[
+        "transformers",
+    ],
+    author="Shaina Raza",
+    author_email="shaina.raza@utoronto.ca",
+    description="A package for detecting bias, performing named entity, and debiasing text.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/VectorInstitute/NewsMediaBias/UnBIAS",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
