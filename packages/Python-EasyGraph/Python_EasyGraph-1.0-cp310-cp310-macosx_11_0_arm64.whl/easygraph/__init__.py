4b45ab1af722cfe063da91cb396b9d68c6ce3131
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 04:13:32 +0000

    Add package Python-EasyGraph/1.0/Python_EasyGraph-1.0-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/Python-EasyGraph/Python_EasyGraph-1.0-cp310-cp310-macosx_11_0_arm64.whl/easygraph/__init__.py
@@ -0,0 +1,29 @@
+import easygraph.classes
+import easygraph.convert
+import easygraph.datapipe
+import easygraph.datasets
+import easygraph.experiments
+import easygraph.functions
+import easygraph.ml_metrics
+import easygraph.model
+import easygraph.nn
+import easygraph.randomhypergraph
+import easygraph.readwrite
+import easygraph.utils
+
+from easygraph.classes import *
+from easygraph.convert import *
+from easygraph.datapipe import *
+from easygraph.datasets import *
+from easygraph.experiments import *
+from easygraph.functions import *
+from easygraph.ml_metrics import *
+from easygraph.model import *
+from easygraph.nn import *
+from easygraph.randomhypergraph import *
+from easygraph.readwrite import *
+from easygraph.utils import *
+
+
+def __getattr__(name):
+    print(f"attr {name} doesn't exist!")
