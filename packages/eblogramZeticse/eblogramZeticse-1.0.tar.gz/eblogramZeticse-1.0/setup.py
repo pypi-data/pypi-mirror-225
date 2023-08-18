3ec7beb716da544a281a896f09c84aa873a303ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 04:15:31 +0000

    Add package eblogramZeticse/1.0/eblogramZeticse-1.0.tar.gz

---
+++ packages/eblogramZeticse/eblogramZeticse-1.0.tar.gz/eblogramZeticse-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='eblogramZeticse',
+    version='1.0',
+    author='Aicorelz',
+    packages=['eblogramZeticse'],
+    install_requires=[
+        
+    ],
+)
