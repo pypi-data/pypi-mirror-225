48fb937e692ed87d0c77fadf941792c6efd9bc7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 03:10:06 +0000

    Add package windmill-api/1.149.0/windmill_api-1.149.0.tar.gz

---
+++ packages/windmill-api/windmill_api-1.149.0.tar.gz/windmill_api-1.149.0/windmill_api/models/list_completed_jobs_response_200_item_job_kind.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class ListCompletedJobsResponse200ItemJobKind(str, Enum):
+    SCRIPT = "script"
+    PREVIEW = "preview"
+    DEPENDENCIES = "dependencies"
+    FLOW = "flow"
+    FLOWPREVIEW = "flowpreview"
+    SCRIPT_HUB = "script_hub"
+    IDENTITY = "identity"
+
+    def __str__(self) -> str:
+        return str(self.value)
