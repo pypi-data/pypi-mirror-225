20f5ab9168d1be48f46eabc1819eb2dcfa26dc15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 05:09:05 +0000

    Add package wesync/0.0.2.post6/wesync-0.0.2.post6-py3-none-any.whl

---
+++ packages/wesync/wesync-0.0.2.post6-py3-none-any.whl/wesync/services/processors/processor.py
@@ -0,0 +1,17 @@
+import logging
+from wesync.services.interaction.userInteraction import UserInteraction
+
+
+class Processor():
+
+    name = "processor"
+    projectTypes = ['*']
+
+    def __init__(self):
+        self.userInteraction = UserInteraction()
+
+    def askForArgument(self, argument: str, **kwargs):
+        return self.userInteraction.askForAnswer(self.name + " value for '" + argument + "'", **kwargs)
+
+    def execute(self, *args, **kwargs):
+        logging.log(1, "Running " + self.name)
