38b82c6b488bf1e2f95647ac8731c9e59b82b4fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 14:10:19 +0000

    Add package Shortners/0.0.15/Shortners-0.0.15.tar.gz

---
+++ packages/Shortners/Shortners-0.0.15.tar.gz/Shortners-0.0.15/Shortners/__init__.py
@@ -0,0 +1,1 @@
+from .shortners import Slink
---
+++ packages/Shortners/Shortners-0.0.15.tar.gz/Shortners-0.0.15/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+from setuptools import setup
+
+with open("README.md", "r") as o:
+    long_description = o.read()
+
+DATA01 = "clintonabrahamc@gmail.com"
+
+DATA02 = ["Programming Language :: Python :: 3",
+          "Operating System :: OS Independent",
+          "License :: OSI Approved :: MIT License"]
+
+setup(
+    name='Shortners',
+    version='0.0.15',
+    author='Clinton Abraham',
+    author_email=DATA01,
+    classifiers=DATA02,
+    zip_safe=False,
+    packages=['Shortners'],
+    python_requires='~=3.8',
+    install_requires = ['aiohttp'],
+    description='Python url shortner',
+    long_description=long_description,
+    package_data={'Shortners': ['py.typed']},
+    keywords=['python', 'shortner', 'telegram'],
+    long_description_content_type="text/markdown",
+    url='https://github.com/Clinton-Abraham/SHORTNER',)
