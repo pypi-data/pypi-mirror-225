63749b6b2fcb119329deb6a40e982bb8248aaecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 03:10:25 +0000

    Add package wappstorest/0.0.6/wappstorest-0.0.6.tar.gz

---
+++ packages/wappstorest/wappstorest-0.0.6.tar.gz/wappstorest-0.0.6/test/test_value.py
@@ -0,0 +1,45 @@
+import uuid
+import logging
+
+from pytest_httpx import HTTPXMock
+
+from wappstorest import WappstoEnv
+from wappstorest import WappstoPath
+from wappstorest import WappstoRest
+from wappstorest import WappstoService
+
+
+logging.root.level = logging.DEBUG
+
+
+class TestValueRest:
+    def test_get(self, httpx_mock: HTTPXMock):
+        pass
+        # session_uuid = uuid.UUID('2d94624a-0a3d-45c6-88ee-ecbf3ff3a980')
+
+        # httpx_mock.add_response(
+        #     method="GET",
+        #     url='https://wappsto.com/services/2.1/user/me',
+        #     match_headers={
+        #         'x-session': str(session_uuid),
+        #         "Accept": "application/json",
+        #         "Content-type": "application/json",
+        #     },
+        #     json={
+        #         'first_name': 'Tom',
+        #         'meta': {
+        #             'id': str(uuid.uuid4()),
+        #             'type': 'user',
+        #         }
+        #     },  # The Reply
+        # )
+
+        # rest = WappstoRest(
+        #     env=WappstoEnv.PROD,
+        #     session=session_uuid,
+        # )
+
+        # reply = rest.service(WappstoService.USER).service_id('me').get()
+
+        # assert reply.first_name == 'Tom'
+        # # assert reply.first_name == 'Tom'
