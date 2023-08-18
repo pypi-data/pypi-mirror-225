fcddd29d6dbecc230a41689480e43369529d35d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 03:09:25 +0000

    Add package lightbus/1.3.0a1/lightbus-1.3.0a1-py3-none-any.whl

---
+++ packages/lightbus/lightbus-1.3.0a1-py3-none-any.whl/lightbus/commands/version.py
@@ -0,0 +1,38 @@
+import argparse
+import logging
+import sys
+from pathlib import Path
+
+if sys.version_info < (3, 10):
+    from importlib_metadata import version as importlib_version
+else:
+    from importlib.metadata import version as importlib_version
+
+
+logger = logging.getLogger(__name__)
+
+
+class Command:
+    def setup(self, parser, subparsers):
+        parser_version = subparsers.add_parser(
+            "version", help="Show the currently installed Lightbus version"
+        )
+        # Read version directly out of pyproject.toml. Useful for the release process
+        parser_version.add_argument("--pyproject", action="store_true", help=argparse.SUPPRESS)
+        # Show the version to be used for creating the docs
+        parser_version.add_argument("--docs", action="store_true", help=argparse.SUPPRESS)
+        parser_version.set_defaults(func=self.handle)
+
+    def handle(self, args):
+        if args.pyproject:
+            import lightbus
+            import toml
+
+            file_path = Path(lightbus.__file__).parent.parent / "pyproject.toml"
+            with file_path.open() as f:
+                version = toml.load(f)["tool"]["poetry"]["version"]
+            if args.docs:
+                version = ".".join(version.split(".")[:2])
+            print(version)
+        else:
+            print(importlib_version("lightbus"))
