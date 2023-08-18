73434856311efe184ce2df549e95c14592b9c689
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 03:09:59 +0000

    Add package calcgp/0.1.1/calcgp-0.1.1-py3-none-any.whl

---
+++ packages/calcgp/calcgp-0.1.1-py3-none-any.whl/calcgp/models.py
@@ -0,0 +1,4 @@
+from ._src.regression.full_regression import (FullFunction, FullGradient,
+                                              FullIntegral)
+from ._src.regression.sparse_regression import (SparseFunction, SparseGradient,
+                                                SparseIntegral)
