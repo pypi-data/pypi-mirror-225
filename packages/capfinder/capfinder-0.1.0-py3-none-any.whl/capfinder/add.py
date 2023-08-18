1bc21f5e0f9f6682a93035d3428a67e3b6e2a98a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 03:09:51 +0000

    Add package capfinder/0.1.0/capfinder-0.1.0-py3-none-any.whl

---
+++ packages/capfinder/capfinder-0.1.0-py3-none-any.whl/capfinder/add.py
@@ -0,0 +1,13 @@
+def add(a: int, b: int, c: int) -> int:
+    """
+    Add three numbers together
+    param a: first number
+    param b: second number
+    param c: third number
+    return: sum of a, b, and c
+    """
+    return a + b + c
+
+
+if __name__ == "__main__":
+    print(add(2, 2, 3))
