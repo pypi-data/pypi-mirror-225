c7ed485611402aaa83187c68009589a24c87b77a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 05:07:15 +0000

    Add package pyfindmaxima/0.1.1.1/pyfindmaxima-0.1.1.1.tar.gz

---
+++ packages/pyfindmaxima/pyfindmaxima-0.1.1.1.tar.gz/pyfindmaxima-0.1.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import Extension, setup
+import setuptools
+from Cython.Build import cythonize
+
+extensions = Extension("cy_findmaxima", ["findmaxima/cy_findmaxima.pyx"])
+setup(
+    name="pyfindmaxima",
+    version="0.1.1.1",
+    setup_requires=["cython", "numpy"],
+    install_requires=['numpy'],
+    ext_modules=cythonize(["findmaxima/cy_findmaxima.pyx"]),
+    #ext_modules=cythonize(extensions),
+    package_data = {'findmaxima':['*.pyx']},
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    license="APACHE",
+    #license_files=["LICENSE"],
+    author="Chris Wood",
+    url="https://github.com/cwood1967/pyfindmaxima",
+    long_description="Find local maxima in a 2D image, similar to ImageJ Find Maxima."
+)
