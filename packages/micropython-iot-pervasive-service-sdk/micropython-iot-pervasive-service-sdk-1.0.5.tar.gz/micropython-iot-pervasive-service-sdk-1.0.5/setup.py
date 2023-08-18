a8b4152b59b9b611bf65dfc5c4dec00a4b52cd6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 03:10:14 +0000

    Add package micropython-iot-pervasive-service-sdk/1.0.5/micropython-iot-pervasive-service-sdk-1.0.5.tar.gz

---
+++ packages/micropython-iot-pervasive-service-sdk/micropython-iot-pervasive-service-sdk-1.0.5.tar.gz/micropython-iot-pervasive-service-sdk-1.0.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+    name='micropython-iot-pervasive-service-sdk',
+    version="1.0.5",
+    description="iot-pervasive-service-sdk module customized for micropython.",
+    packages=find_packages(),
+    author="whu贾向阳团队-葛家和",    #作者，可以写自己的姓名
+    author_email="2898534520@qq.com",    #作者联系方式，可写自己的邮箱地址
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+    ],
+    keywords='micropython',
+    include_package_data=True
+    )
