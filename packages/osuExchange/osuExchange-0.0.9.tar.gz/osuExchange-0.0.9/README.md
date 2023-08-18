7c52d4abcc1726f9e96c9749cc7e61b9b4d6ba6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 04:15:26 +0000

    Add package osuExchange/0.0.9/osuExchange-0.0.9.tar.gz

---
+++ packages/osuExchange/osuExchange-0.0.9.tar.gz/osuExchange-0.0.9/README.md
@@ -0,0 +1,25 @@
+# osuExchange
+
+![osuExchange Logo](logo.png)
+
+An osu! API wrapper written in Python. Built to give developers a simple and easy-to-use interface to the osu! API.
+
+## Usage
+
+```
+pip install osuExchange
+```
+
+## Building
+
+Install necessary packages:
+
+```
+pip install build
+```
+
+Run this command inside of the osuExchange environment:
+
+```
+python -m build
+```
---
+++ packages/osuExchange/osuExchange-0.0.9.tar.gz/osuExchange-0.0.9/osuExchange/client.py
@@ -0,0 +1,39 @@
+from osuExchange.endpoints import UsersEndpoints, BeatmapsEndpoints, Score
+from osuExchange.auth import ClientCredentialsToken, OAuth2Scope, get_access_token, get_client_credentials_token
+from osuExchange.typing import GameMode
+from osuExchange import api
+from json import JSONDecoder
+
+JSON_DECODER = JSONDecoder()
+
+class BaseOsuApiClient:
+	def __init__(self, token: ClientCredentialsToken):
+		self.token = token
+		self.users = UsersEndpoints(token.access_token)
+		self.beatmaps = BeatmapsEndpoints(token.access_token)
+
+	# https://osu.ppy.sh/docs/index.html#get-apiv2scoresmodescore
+	def get_score(self, mode: GameMode, score_id: int) -> Score:
+		return Score(api.get(f'/scores/{mode}/{score_id}', token=self.token.access_token).json())
+	
+	# https://osu.ppy.sh/docs/#revoke-current-token
+	def revoke_current_token(self) -> None:
+		api.delete('/oauth/tokens/current')
+
+class OsuUserClient(BaseOsuApiClient):
+	@staticmethod
+	def from_json_file(filename: str) -> 'OsuUserClient':
+		with open(filename, 'r') as file:
+			return OsuUserClient(**JSON_DECODER.decode(file.read()))
+
+	def __init__(self, *, client_id: int, client_secret: str, redirect_uri: str, scopes: list[OAuth2Scope]):
+		super().__init__(get_access_token(client_id, client_secret, redirect_uri, scopes))
+
+class OsuApiClient(BaseOsuApiClient):
+	@staticmethod
+	def from_json_file(filename: str) -> 'OsuApiClient':
+		with open(filename, 'r') as file:
+			return OsuApiClient(**JSON_DECODER.decode(file.read()))
+
+	def __init__(self, *, client_id: int, client_secret: str):
+		super().__init__(get_client_credentials_token(client_id, client_secret))
