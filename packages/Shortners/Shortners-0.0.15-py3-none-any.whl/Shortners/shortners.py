b05c4949510933cb69ae2fc796224b3347ff1a78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 14:10:19 +0000

    Add package Shortners/0.0.15/Shortners-0.0.15-py3-none-any.whl

---
+++ packages/Shortners/Shortners-0.0.15-py3-none-any.whl/Shortners/shortners.py
@@ -0,0 +1,16 @@
+import aiohttp
+from Shortners.script import Scripted
+#====================================================================
+
+async def Slink(SHORTNER, API, URL):
+    midos = {'api': API, 'url': URL}
+    async with aiohttp.ClientSession() as session:
+        async with session.get(SHORTNER, params=midos) as res:
+            if res.status == "success" or res.status == 200:
+                dataes = await res.json()
+                ourais = dataes["shortenedUrl"]
+                return ourais
+            else:
+                return URL
+
+#====================================================================
