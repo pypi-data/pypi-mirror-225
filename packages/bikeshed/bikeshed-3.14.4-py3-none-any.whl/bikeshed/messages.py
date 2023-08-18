9d994575734b8f041a10a2eff2de8134cee1b4df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 05:11:25 +0000

    Add package bikeshed/3.14.4/bikeshed-3.14.4-py3-none-any.whl

---
+++ packages/bikeshed/bikeshed-3.14.4-py3-none-any.whl/bikeshed/messages.py
@@ -0,0 +1,224 @@
+from __future__ import annotations
+
+import sys
+from collections import Counter
+
+import lxml.html
+
+from . import constants, t
+
+messages: set[str | tuple[str, str]]
+messages = set()
+
+messageCounts: dict[str, int]
+messageCounts = Counter()
+
+
+def p(msg: str | tuple[str, str], sep: str | None = None, end: str | None = None) -> None:
+    if constants.quiet == float("infinity"):
+        return
+    if isinstance(msg, tuple):
+        msg, ascii = msg
+    else:
+        ascii = msg.encode("ascii", "replace").decode()
+    if constants.asciiOnly:
+        msg = ascii
+    try:
+        print(msg, sep=sep, end=end)
+    except UnicodeEncodeError:
+        if ascii is not None:
+            print(ascii, sep=sep, end=end)
+        else:
+            warning = formatMessage(
+                "warning",
+                "Your console does not understand Unicode.\n  Messages may be slightly corrupted.",
+            )
+            if warning not in messages:
+                print(warning)
+                messages.add(warning)
+            print(msg.encode("ascii", "xmlcharrefreplace"), sep=sep, end=end)
+
+
+def die(msg: str, el: t.ElementT | None = None, lineNum: str | int | None = None) -> None:
+    if lineNum is None and el is not None and el.get("line-number"):
+        lineNum = el.get("line-number")
+    formattedMsg = formatMessage("fatal", msg, lineNum=lineNum)
+    if formattedMsg not in messages:
+        messageCounts["fatal"] += 1
+        messages.add(formattedMsg)
+        if constants.quiet < 3:
+            p(formattedMsg)
+    if constants.errorLevelAt("fatal"):
+        errorAndExit()
+
+
+def linkerror(msg: str, el: t.ElementT | None = None, lineNum: str | int | None = None) -> None:
+    if lineNum is None and el is not None and el.get("line-number"):
+        lineNum = el.get("line-number")
+    suffix = ""
+    if el is not None:
+        if el.get("bs-autolink-syntax"):
+            suffix = "\n" + t.cast(str, el.get("bs-autolink-syntax"))
+        else:
+            suffix = "\n" + lxml.html.tostring(el, with_tail=False, encoding="unicode")
+    formattedMsg = formatMessage("link", msg + suffix, lineNum=lineNum)
+    if formattedMsg not in messages:
+        messageCounts["link-error"] += 1
+        messages.add(formattedMsg)
+        if constants.quiet < 2:
+            p(formattedMsg)
+    if constants.errorLevelAt("link-error"):
+        errorAndExit()
+
+
+def lint(msg: str, el: t.ElementT | None = None, lineNum: str | int | None = None) -> None:
+    if lineNum is None and el is not None and el.get("line-number"):
+        lineNum = el.get("line-number")
+    suffix = ""
+    if el is not None:
+        if el.get("bs-autolink-syntax"):
+            suffix = "\n" + t.cast(str, el.get("bs-autolink-syntax"))
+        else:
+            suffix = "\n" + lxml.html.tostring(el, with_tail=False, encoding="unicode")
+    formattedMsg = formatMessage("lint", msg + suffix, lineNum=lineNum)
+    if formattedMsg not in messages:
+        messageCounts["lint"] += 1
+        messages.add(formattedMsg)
+        if constants.quiet < 1:
+            p(formattedMsg)
+    if constants.errorLevelAt("lint"):
+        errorAndExit()
+
+
+def warn(msg: str, el: t.ElementT | None = None, lineNum: str | int | None = None) -> None:
+    if lineNum is None and el is not None and el.get("line-number"):
+        lineNum = el.get("line-number")
+    formattedMsg = formatMessage("warning", msg, lineNum=lineNum)
+    if formattedMsg not in messages:
+        messageCounts["warning"] += 1
+        messages.add(formattedMsg)
+        if constants.quiet < 1:
+            p(formattedMsg)
+    if constants.errorLevelAt("warning"):
+        errorAndExit()
+
+
+def say(msg: str) -> None:
+    if constants.quiet < 1:
+        p(formatMessage("message", msg))
+
+
+def success(msg: str) -> None:
+    if constants.quiet < 4:
+        p(formatMessage("success", msg))
+
+
+def failure(msg: str) -> None:
+    if constants.quiet < 4:
+        p(formatMessage("failure", msg))
+
+
+def resetSeenMessages() -> None:
+    global messages  # noqa: PLW0603
+    messages = set()
+    global messageCounts  # noqa: PLW0603
+    messageCounts = Counter()
+
+
+def retroactivelyCheckErrorLevel(level: str | None = None) -> bool:
+    if level is None:
+        level = constants.getErrorLevel()
+    for levelName, msgCount in messageCounts.items():
+        if msgCount > 0 and constants.errorLevelAt(levelName):
+            errorAndExit()
+    return True
+
+
+def printColor(text: str, color: str = "white", *styles: str) -> str:
+    if constants.printMode == "console":
+        colorsConverter = {
+            "black": 30,
+            "red": 31,
+            "green": 32,
+            "yellow": 33,
+            "blue": 34,
+            "magenta": 35,
+            "cyan": 36,
+            "light gray": 37,
+            "dark gray": 90,
+            "light red": 91,
+            "light green": 92,
+            "light yellow": 93,
+            "light blue": 94,
+            "light magenta": 95,
+            "light cyan": 96,
+            "white": 97,
+        }
+        stylesConverter = {
+            "normal": 0,
+            "bold": 1,
+            "bright": 1,
+            "dim": 2,
+            "underline": 4,
+            "underlined": 4,
+            "blink": 5,
+            "reverse": 7,
+            "invert": 7,
+            "hidden": 8,
+        }
+
+        colorNum = colorsConverter[color.lower()]
+        styleNum = ";".join(str(stylesConverter[style.lower()]) for style in styles)
+        return f"\033[{styleNum};{colorNum}m{text}\033[0m"
+    return text
+
+
+def formatMessage(type: str, text: str, lineNum: str | int | None = None) -> str | tuple[str, str]:
+    if constants.printMode == "markup":
+        text = text.replace("<", "&lt;")
+        if type == "fatal":
+            return f"<fatal>{text}</fatal>"
+        if type == "link":
+            return f"<linkerror>{text}</linkerror>"
+        if type == "lint":
+            return f"<lint>{text}</lint>"
+        if type == "warning":
+            return f"<warning>{text}</warning>"
+        if type == "message":
+            return f"<message>{text}</message>"
+        if type == "success":
+            return f"<final-success>{text}</final-success>"
+        if type == "failure":
+            return f"<final-failure>{text}</final-failure>"
+    if type == "message":
+        return text
+    if type == "success":
+        return (
+            printColor(" ✔ ", "green", "invert") + " " + text,
+            printColor("YAY", "green", "invert") + " " + text,
+        )
+    if type == "failure":
+        return (
+            printColor(" ✘ ", "red", "invert") + " " + text,
+            printColor("ERR", "red", "invert") + " " + text,
+        )
+    if type == "fatal":
+        headingText = "FATAL ERROR"
+        color = "red"
+    elif type == "link":
+        headingText = "LINK ERROR"
+        color = "yellow"
+    elif type == "lint":
+        headingText = "LINT"
+        color = "yellow"
+    elif type == "warning":
+        headingText = "WARNING"
+        color = "light cyan"
+    if lineNum is not None:
+        headingText = f"LINE {lineNum}"
+    return printColor(headingText + ":", color, "bold") + " " + text
+
+
+def errorAndExit() -> None:
+    failure("Did not generate, due to errors exceeding the allowed error level.")
+    sys.exit(2)
---
+++ packages/bikeshed/bikeshed-3.14.4-py3-none-any.whl/bikeshed/test.py
@@ -0,0 +1,190 @@
+from __future__ import annotations
+
+import dataclasses
+import difflib
+import glob
+import os
+import re
+
+from alive_progress import alive_it
+
+from . import config, metadata, retrieve, t
+from . import messages as m
+from .Spec import Spec
+
+if t.TYPE_CHECKING:
+    import argparse
+
+TEST_DIR = os.path.abspath(os.path.join(config.scriptPath(), "..", "tests"))
+TEST_FILE_EXTENSIONS = (".bs", ".tar")
+
+
+@dataclasses.dataclass
+class TestFilter:
+    globs: list[str] | None = None
+    folders: list[str] | None = None
+    manualOnly: bool = False
+
+    @staticmethod
+    def fromOptions(options: argparse.Namespace) -> TestFilter:
+        return TestFilter(globs=options.testFiles, folders=options.folder, manualOnly=options.manualOnly)
+
+
+def testPaths(filters: TestFilter) -> list[str]:
+    # if None, get all the test paths
+    # otherwise, glob the provided paths, rooted at the test dir
+    if not filters.globs:
+        return list(sortTests(findTestFiles(filters)))
+    return [
+        path
+        for pattern in filters.globs
+        for path in glob.glob(os.path.join(TEST_DIR, pattern))
+        if path.endswith(TEST_FILE_EXTENSIONS)
+    ]
+
+
+def findTestFiles(filters: TestFilter) -> t.Generator[str, None, None]:
+    for root, _, filenames in os.walk(TEST_DIR):
+        for filename in filenames:
+            filePath = testNameForPath(os.path.join(root, filename))
+            if not allowedPath(filePath, filters):
+                continue
+            if not filePath.endswith(TEST_FILE_EXTENSIONS):
+                continue
+            yield os.path.join(root, filename)
+
+
+def allowedPath(filePath: str, filters: TestFilter) -> bool:
+    pathSegs = splitPath(filePath)
+    if filters.manualOnly and pathSegs[0] == "github":
+        return False
+    if re.search(r"\d{3}-files$", pathSegs[0]):
+        # support files for a manual test
+        return False
+    if filters.folders:
+        if not any(folder in pathSegs for folder in filters.folders):
+            return False
+    return True
+
+
+def splitPath(path: str, reverseSegs: list[str] | None = None) -> list[str]:
+    if reverseSegs is None:
+        reverseSegs = []
+    [head, tail] = os.path.split(path)
+    reverseSegs.append(tail)
+    if head in ["", "/"]:
+        return list(reversed(reverseSegs))
+    return splitPath(head, reverseSegs)
+
+
+# The test name will be the path relative to the tests directory, or the path as
+# given if the test is outside of that directory.
+def testNameForPath(path: str) -> str:
+    if path.startswith(TEST_DIR):
+        return path[len(TEST_DIR) + 1 :]
+    return path
+
+
+def sortTests(tests: t.Iterable[str]) -> t.Iterable[str]:
+    return sorted(tests, key=lambda x: ("/" in testNameForPath(x), x))
+
+
+def runAllTests(
+    filters: TestFilter,
+    md: t.MetadataManager | None = None,
+) -> bool:
+    paths = testPaths(filters)
+    if len(paths) == 0:
+        m.p("No tests were found")
+        return True
+    numPassed = 0
+    total = 0
+    fails = []
+    pathProgress = alive_it(paths, dual_line=True, length=20)
+    try:
+        for path in pathProgress:
+            testName = testNameForPath(path)
+            pathProgress.text(testName)
+            total += 1
+            doc = processTest(path, md)
+            outputText = doc.serialize()
+            if outputText is None:
+                m.p(m.printColor("Serialization failed.", color="red"))
+                fails.append(testName)
+                continue
+            with open(os.path.splitext(path)[0] + ".html", encoding="utf-8") as golden:
+                goldenText = golden.read()
+            if compare(outputText, goldenText):
+                numPassed += 1
+            else:
+                fails.append(testName)
+    except:  # pylint: disable=bare-except
+        m.say(testName)
+    if numPassed == total:
+        m.p(m.printColor("✔ All tests passed.", color="green"))
+        return True
+    m.p(m.printColor(f"✘ {numPassed}/{total} tests passed.", color="red"))
+    m.p(m.printColor("Failed Tests:", color="red"))
+    for fail in fails:
+        m.p("* " + fail)
+    return False
+
+
+def processTest(
+    path: str,
+    md: metadata.MetadataManager | None = None,
+    fileRequester: t.DataFileRequester = retrieve.DataFileRequester(fileType="readonly"),
+) -> t.SpecT:
+    try:
+        doc = Spec(inputFilename=path, fileRequester=fileRequester, testing=True)
+        if md is not None:
+            doc.mdCommandLine = md
+        addTestMetadata(doc)
+        doc.preprocess()
+    except Exception as e:
+        m.p(f"Error running test {path}:\n  {e}")
+    return doc
+
+
+def compare(suspect: str, golden: str) -> bool:
+    if suspect == golden:
+        return True
+    for line in difflib.unified_diff(golden.split("\n"), suspect.split("\n"), fromfile="golden", tofile="suspect"):
+        if line[0] == "-":
+            m.p(m.printColor(line, color="red"))
+        elif line[0] == "+":
+            m.p(m.printColor(line, color="green"))
+        else:
+            m.p(line)
+    m.p("")
+    return False
+
+
+def rebase(
+    filters: TestFilter,
+    md: t.MetadataManager | None = None,
+) -> bool:
+    paths = testPaths(filters)
+    if len(paths) == 0:
+        m.p("No tests were found.")
+        return True
+    pathProgress = alive_it(paths, dual_line=True, length=20)
+    for path in pathProgress:
+        name = testNameForPath(path)
+        pathProgress.text(name)
+        m.resetSeenMessages()
+        doc = processTest(path, md)
+        doc.finish(newline="\n")
+    return True
+
+
+def addTestMetadata(doc: t.SpecT) -> None:
+    assert doc.mdBaseline is not None
+    assert doc.mdCommandLine is not None
+    doc.mdBaseline.addData("Boilerplate", "omit feedback-header, omit generator, omit document-revision")
+    doc.mdBaseline.addData("Repository", "test/test")
+    _, md = metadata.parse(lines=doc.lines)
+    if "Date" not in md.manuallySetKeys:
+        doc.mdCommandLine.addData("Date", "1970-01-01")
+    if "Inline Github Issue" not in md.manuallySetKeys:
+        doc.mdCommandLine.addData("Inline Github Issues", "no")
