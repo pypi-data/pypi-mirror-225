21ac06ffc6a197949a63e68aa08a4266382d50ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 05:11:28 +0000

    Add package bia-bob/0.2.1/bia_bob-0.2.1-py3-none-any.whl

---
+++ packages/bia-bob/bia_bob-0.2.1-py3-none-any.whl/bia_bob/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "0.2.1"
+
+__all__ = (
+    )
+
+from ._machinery import bob, init_assistant, add_function_tool
+from ._speech_recognition import _listen
+bob.initialize = init_assistant
+bob.add_function_tool = add_function_tool
+
+bob.__version__ = __version__
+bob.listen = _listen
