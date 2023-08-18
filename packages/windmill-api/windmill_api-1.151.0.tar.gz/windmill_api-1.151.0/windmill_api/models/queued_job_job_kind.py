af7f318b3916c9af6f58a3e97f3a7d07630942b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 06:15:20 +0000

    Add package windmill-api/1.151.0/windmill_api-1.151.0.tar.gz

---
+++ packages/windmill-api/windmill_api-1.151.0.tar.gz/windmill_api-1.151.0/windmill_api/models/queued_job_job_kind.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class QueuedJobJobKind(str, Enum):
+    SCRIPT = "script"
+    PREVIEW = "preview"
+    DEPENDENCIES = "dependencies"
+    FLOW = "flow"
+    FLOWPREVIEW = "flowpreview"
+    SCRIPT_HUB = "script_hub"
+    IDENTITY = "identity"
+
+    def __str__(self) -> str:
+        return str(self.value)
