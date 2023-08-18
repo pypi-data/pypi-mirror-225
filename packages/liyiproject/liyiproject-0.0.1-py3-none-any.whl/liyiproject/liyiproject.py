b630fda1875706ac1f1c3d8ce80a93cb3d8a2dfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 14:10:17 +0000

    Add package liyiproject/0.0.1/liyiproject-0.0.1-py3-none-any.whl

---
+++ packages/liyiproject/liyiproject-0.0.1-py3-none-any.whl/liyiproject/liyiproject.py
@@ -0,0 +1,16 @@
+import os
+
+
+class Test_operation():
+    def Local_ip(self, ip_address):
+        return ip_address
+
+    def Local_hostname(self, hostname):
+        return hostname
+
+    def operation(self, statement):
+        info = os.popen(statement)
+        return info.read()
+
+    def test_mat(self, add_number):
+        return add_number + 1
