558aa29e097b36f1e9ff6cab92e7b85234843001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 03:10:04 +0000

    Add package nxtomomill/0.13.3/nxtomomill-0.13.3-py3-none-any.whl

---
+++ packages/nxtomomill/nxtomomill-0.13.3-py3-none-any.whl/nxtomomill/nexus/nxtransformations.py
@@ -0,0 +1,10 @@
+from silx.utils.deprecation import deprecated_warning
+from nxtomo.nxobject.nxtransformations import *  # noqa F401
+
+deprecated_warning(
+    type_="Module",
+    name="nxtomomill.nexus.nxtransformations",
+    reason="dedicated project created",
+    replacement="nxtomo.nxobject.nxtransformations",
+    since_version=1.0,
+)
