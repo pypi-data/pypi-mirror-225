60fe8ae99dcd750daba83c2621fcd996cd7274fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 14:09:30 +0000

    Add package openai-utilties/1.6/openai-utilties-1.6.tar.gz

---
+++ packages/openai-utilties/openai-utilties-1.6.tar.gz/openai-utilties-1.6/openai_utils/__init__.py
@@ -0,0 +1,27 @@
+import openai
+import numpy as np
+
+
+def AskGPT(model: str, context: list, temperature: float = 1) -> str:
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=context,
+        temperature=temperature
+    )
+    return response["choices"][0]["message"]["content"]
+
+
+def GetEmbedding(String: str) -> list:
+    data = openai.Embedding.create(
+        model="text-embedding-ada-002",
+        input=String
+    )
+    return data["data"][0]["embedding"]
+
+
+def GetEmbeddingDistance(Embedding1: list, Embedding2: list) -> float:
+    data1 = np.array(Embedding1)
+    data2 = np.array(Embedding2)
+    distance = np.linalg.norm(data1 - data2)
+    return distance
+
