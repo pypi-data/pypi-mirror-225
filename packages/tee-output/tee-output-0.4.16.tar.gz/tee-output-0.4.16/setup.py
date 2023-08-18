df7c0372ca82a5ef52f99b31fe2bfbd0798d5259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 05:08:36 +0000

    Add package tee-output/0.4.16/tee-output-0.4.16.tar.gz

---
+++ packages/tee-output/tee-output-0.4.16.tar.gz/tee-output-0.4.16/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='tee-output',
+      version='0.4.16',
+      description='A utility to tee standard output / standard error from the current process into a logfile. Preserves terminal semantics, so breakpoint() etc continue to work.',
+      install_requires=['psutil'],
+      author='Greg Brockman',
+      packages=['tee_output'],
+      scripts=["bin/parent-lifetime"]
+)
