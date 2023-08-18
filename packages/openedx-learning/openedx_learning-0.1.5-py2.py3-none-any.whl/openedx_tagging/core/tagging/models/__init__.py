78d5869e4866a57959c8d4f0928fd94fc9221ace
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 14:10:33 +0000

    Add package openedx-learning/0.1.5/openedx_learning-0.1.5-py2.py3-none-any.whl

---
+++ packages/openedx-learning/openedx_learning-0.1.5-py2.py3-none-any.whl/openedx_tagging/core/tagging/models/__init__.py
@@ -0,0 +1,15 @@
+from .base import (
+    Tag,
+    Taxonomy,
+    ObjectTag,
+)
+from .system_defined import (
+    ModelObjectTag,
+    ModelSystemDefinedTaxonomy,
+    UserSystemDefinedTaxonomy,
+    LanguageTaxonomy,
+)
+from .import_export import (
+    TagImportTask,
+    TagImportTaskState,
+)
