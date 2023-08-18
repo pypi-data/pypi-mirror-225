3a38228672fd08c47049e0727476d57560b261d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 05:10:48 +0000

    Add package emlabpkg/0.2/emlabpkg-0.2.tar.gz

---
+++ packages/emlabpkg/emlabpkg-0.2.tar.gz/emlabpkg-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="emlabpkg",
+    version="0.2",
+    author="Sushant Padhye",
+    author_email="padhyesm@mail.uc.edu",
+    packages=["emlabpkg"],
+    install_requires=["qcodes"],
+)
