2fa77c58a8e25e8684cd3758288f71f326810faf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 05:06:11 +0000

    Add package pygcransac/0.0.dev0/pygcransac-0.0.dev0-cp310-cp310-win_amd64.whl

---
+++ packages/pygcransac/pygcransac-0.0.dev0-cp310-cp310-win_amd64.whl/pygcransac/__init__.py
@@ -0,0 +1,14 @@
+"""""" # start delvewheel patch
+def _delvewheel_patch_1_5_0():
+    import os
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'pygcransac.libs'))
+    if os.path.isdir(libs_dir):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_5_0()
+del _delvewheel_patch_1_5_0
+# end delvewheel patch
+
+from .pygcransac import *
+__version__ = '0.1.0'
