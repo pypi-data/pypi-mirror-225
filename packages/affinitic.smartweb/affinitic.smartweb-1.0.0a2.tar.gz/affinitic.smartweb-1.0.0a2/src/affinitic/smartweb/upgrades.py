99109bcd3fb019c13e0e38a071b27606a9aabac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 15:09:46 +0000

    Add package affinitic.smartweb/1.0.0a2/affinitic.smartweb-1.0.0a2.tar.gz

---
+++ packages/affinitic.smartweb/affinitic.smartweb-1.0.0a2.tar.gz/affinitic.smartweb-1.0.0a2/src/affinitic/smartweb/upgrades.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from plone import api
+
+
+def update_types(context):
+    """
+    Update types
+    """
+    portal_setup = api.portal.get_tool("portal_setup")
+    portal_setup.runImportStepFromProfile(
+        "profile-affinitic.smartweb:default", "typeinfo"
+    )
