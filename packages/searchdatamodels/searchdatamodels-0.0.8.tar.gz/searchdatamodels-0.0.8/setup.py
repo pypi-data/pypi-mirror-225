fa1949da54b0beb6da8c1f778f6d5f1b3a184799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 05:06:34 +0000

    Add package searchdatamodels/0.0.8/searchdatamodels-0.0.8.tar.gz

---
+++ packages/searchdatamodels/searchdatamodels-0.0.8.tar.gz/searchdatamodels-0.0.8/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+ 
+setuptools.setup(
+    name="searchdatamodels",
+    version="0.0.8",
+    author="James Baker",
+    author_email="james@talentium.io", 
+    license="MIT",
+    install_requires=[
+          'wandb', 'numpy', 'strsimpy', 'scipy', 'sentence-transformers','pydantic'
+      ],
+    # classifiers like program is suitable for python3, just leave as it is.
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
