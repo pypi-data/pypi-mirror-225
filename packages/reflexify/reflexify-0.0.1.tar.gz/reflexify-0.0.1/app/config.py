be71155973994027db9c22385870fea8d8988238
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 05:09:08 +0000

    Add package reflexify/0.0.1/reflexify-0.0.1.tar.gz

---
+++ packages/reflexify/reflexify-0.0.1.tar.gz/reflexify-0.0.1/app/config.py
@@ -0,0 +1,33 @@
+app_configuration: dict = {
+    "site_name": "Reflexify",
+    "repo_name": "LineIndent/flexible",
+    "repo_url": "https://github.com/LineIndent/fletxible",
+    "copy_right": "Copyright © 2022 - 2023 S. Ahmad P. Hakimi ",
+    "attribute": "Made with Refelex & Reflexify",
+    "drawer": True,
+    "theme": {
+        "primary": "black",
+        "secondary": "teal",
+        "font": "Helvetica",
+    },
+    "socials": {
+        "github": "https://github.com/reflex-dev/reflex",
+        "twitter": "https://twitter.com/getreflex",
+        "youtube": "",
+        "mastodon": "",
+        "discord": "https://discord.gg/T5WSbC2YtQ",
+    },
+    "navigation": {
+        "home": {
+            "Getting started": "start.py",
+        },
+        "setup": {
+            "set up": "setup.py",
+            "changing the colors": "color.py",
+            # "changing the fonts": "fonts.py",
+            # "site navigation": "navigation.py",
+            # "Adding git repository": "git.py",
+            # "Adding social media": "media.py",
+        },
+    },
+}
