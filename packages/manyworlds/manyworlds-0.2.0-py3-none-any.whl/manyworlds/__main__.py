07c1071d74d5a0bdbda825a3235b1aa9dbe1b2bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 14:09:55 +0000

    Add package manyworlds/0.2.0/manyworlds-0.2.0-py3-none-any.whl

---
+++ packages/manyworlds/manyworlds-0.2.0-py3-none-any.whl/manyworlds/__main__.py
@@ -0,0 +1,37 @@
+# __main__.py
+import argparse
+import manyworlds as mw
+from termcolor import colored
+
+def main():
+    parser = argparse.ArgumentParser(prog="manyworlds")
+    parser.add_argument('--input', '-i',
+                        help="input scenario file")
+    parser.add_argument('--output', '-o',
+                        help="output scenario file")
+    parser.add_argument('--mode', '-m',
+                        choices=['strict', 'relaxed'],
+                        default='strict',
+                        help='flattening mode')
+    parser.add_argument('--comments', '-c',
+                        default=False,
+                        action='store_true',
+                        help='output comments')
+    args = parser.parse_args()
+
+    tree = mw.ScenarioForest.from_file(args.input)
+
+    # print tree:
+    for sc in tree.scenarios():
+        level = sc.level()
+        if level > 1:
+            indentation_string = '   '*(level-2) + colored('└─ ', 'blue')
+        else:
+            indentation_string = ''
+        print(indentation_string + sc.name)
+
+    if args.output:
+        tree.flatten(args.output, mode=args.mode, comments=args.comments)
+
+if __name__ == "__main__":
+    main()
---
+++ packages/manyworlds/manyworlds-0.2.0-py3-none-any.whl/manyworlds/exceptions.py
@@ -0,0 +1,3 @@
+class InvalidFeatureFileError(Exception):
+    """Exception for invalid line in a feature file"""
+    pass
