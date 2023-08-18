a9c1d0d64a89cb63d69f26df398c6a58db6ec02c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 14:08:07 +0000

    Add package Python-EasyGraph/1.0/Python_EasyGraph-1.0-cp39-cp39-macosx_11_0_arm64.whl

---
+++ packages/Python-EasyGraph/Python_EasyGraph-1.0-cp39-cp39-macosx_11_0_arm64.whl/easygraph/functions/hypergraph/generator/__init__.py
@@ -0,0 +1,3 @@
+from .classic import *
+from .hypergraph import *
+from .RandomNetwork import *
---
+++ packages/Python-EasyGraph/Python_EasyGraph-1.0-cp39-cp39-macosx_11_0_arm64.whl/easygraph/functions/hypergraph/generator/hypergraph/__init__.py
@@ -0,0 +1,6 @@
+from .hypergraph_classic import *
+
+from .lattice import *
+from .random import *
+from .simple import *
+from .uniform import *
