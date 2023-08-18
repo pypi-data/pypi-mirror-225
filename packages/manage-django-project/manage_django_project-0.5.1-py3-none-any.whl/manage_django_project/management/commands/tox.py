f20cd511103c8ec5b67e29d1688d7c8a79e00bef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 05:06:08 +0000

    Add package manage-django-project/0.5.1/manage_django_project-0.5.1-py3-none-any.whl

---
+++ packages/manage-django-project/manage_django_project-0.5.1-py3-none-any.whl/manage_django_project/management/commands/tox.py
@@ -0,0 +1,20 @@
+import sys
+
+from cli_base.cli_tools.subprocess_utils import verbose_check_call
+
+from manage_django_project.management.base import BasePassManageCommand
+from manage_django_project.management.commands.coverage import coverage_combine_report
+
+
+class Command(BasePassManageCommand):
+    help = 'Run tests via tox'
+
+    def run_from_argv(self, argv):
+        super().run_from_argv(argv)
+
+        # Just pass every argument to the origin tox CLI:
+        verbose_check_call(sys.executable, '-m', 'tox', *argv[2:], exit_on_error=True)
+
+        coverage_combine_report(verbose=True)
+
+        sys.exit(0)
