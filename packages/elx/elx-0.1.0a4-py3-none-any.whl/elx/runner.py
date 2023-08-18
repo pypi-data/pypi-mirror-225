318860fd06beb868a5a81fa29d12a79b18d5e1e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 03:10:55 +0000

    Add package elx/0.1.0a4/elx-0.1.0a4-py3-none-any.whl

---
+++ packages/elx/elx-0.1.0a4-py3-none-any.whl/elx/runner.py
@@ -0,0 +1,121 @@
+import datetime
+import json
+import logging
+import subprocess
+from typing import List, Optional
+
+from functools import cached_property
+from elx.tap import Tap
+from elx.target import Target
+from elx import StateManager
+from dotenv import load_dotenv
+
+logging.basicConfig(level=logging.INFO)
+
+
+class Runner:
+    def __init__(
+        self,
+        tap: Tap,
+        target: Target,
+        state_manager: StateManager = StateManager(),
+    ):
+        load_dotenv()
+        self.tap = tap
+        self.target = target
+        self.state_manager = state_manager
+
+        # Give the tap and target access to the runner.
+        self.tap.runner = self
+        self.target.runner = self
+
+    @property
+    def state_file_name(self) -> str:
+        return f"{self.tap.executable}-{self.target.executable}.json"
+
+    def load_state(self) -> dict:
+        return self.state_manager.load(self.state_file_name)
+
+    def save_state(self, state: dict) -> None:
+        self.state_manager.save(self.state_file_name, state)
+
+    @cached_property
+    def interpolation_values(self) -> dict:
+        """
+        Values that can be used in the config of the tap or target.
+        """
+        NOW = datetime.datetime.now()
+
+        return {
+            "NOW": NOW.isoformat(),
+            "YESTERDAY": (NOW - datetime.timedelta(days=1)).isoformat(),
+            "LAST_WEEK": (NOW - datetime.timedelta(days=7)).isoformat(),
+            "TAP_EXECUTABLE": self.tap.executable,
+            "TAP_NAME": self.tap.executable.replace("-", "_"),
+            "TARGET_EXECUTABLE": self.target.executable,
+            "TARGET_NAME": self.target.executable.replace("-", "_"),
+        }
+
+    def run(
+        self,
+        streams: Optional[List[str]] = None,
+        logger: logging.Logger = None,
+    ) -> None:
+        state = self.load_state()
+
+        with self.tap.process(state=state, streams=streams) as tap_process:
+            with self.target.process(tap_process=tap_process) as target_process:
+
+                def log_lines():
+                    yield from iter(tap_process.stderr.readline, b"")
+                    yield from iter(target_process.stderr.readline, b"")
+
+                for line in log_lines():
+                    print(line.decode("utf-8"))
+                    if logger:
+                        logger.info(line.decode("utf-8"))
+
+                tap_process.stdout.close()
+                stdout, stderr = target_process.communicate()
+
+                # If any of the processes exited with a non-zero exit code,
+                # raise an exception.
+                if tap_process.returncode and tap_process.returncode != 0:
+                    raise subprocess.CalledProcessError(
+                        tap_process.returncode, tap_process.args
+                    )
+                if target_process.returncode and target_process.returncode != 0:
+                    raise subprocess.CalledProcessError(
+                        target_process.returncode, target_process.args
+                    )
+
+                state = json.loads(stdout.decode("utf-8"))
+                self.save_state(state)
+
+
+if __name__ == "__main__":
+    tap = Tap(
+        executable="tap-smoke-test",
+        spec="git+https://github.com/meltano/tap-smoke-test.git",
+        config={
+            "streams": [
+                {
+                    "stream_name": "stream-one",
+                    "input_filename": "https://gitlab.com/meltano/tap-smoke-test/-/raw/main/demo-data/animals-data.jsonl",
+                },
+                {
+                    "stream_name": "stream-two",
+                    "input_filename": "https://gitlab.com/meltano/tap-smoke-test/-/raw/main/demo-data/animals-data.jsonl",
+                },
+            ],
+        },
+    )
+    target = Target(
+        spec="git+https://github.com/estrategiahq/target-parquet.git",
+        executable="target-parquet",
+        config={
+            "destination_path": "/tmp",
+        },
+    )
+    runner = Runner(tap, target, StateManager("/tmp"))
+    runner.run("stream-one")
---
+++ packages/elx/elx-0.1.0a4-py3-none-any.whl/elx/tap.py
@@ -0,0 +1,113 @@
+import logging
+import contextlib
+from functools import cached_property
+from pathlib import Path
+from typing import Generator, List, Optional
+from elx.singer import Singer, require_install
+from elx.catalog import Stream
+from elx.json_temp_file import json_temp_file
+from subprocess import Popen, PIPE
+
+
+class Tap(Singer):
+    def discover(self, config_path: Path) -> dict:
+        """
+        Run the tap in discovery mode.
+
+        Args:
+            config_path (Path): Path to the config file.
+
+        Returns:
+            dict: The catalog.
+        """
+        logging.debug(f"Discovering {self.executable} with {config_path}")
+        return self.run(["--config", str(config_path), "--discover"])
+
+    @cached_property
+    def catalog(self) -> dict:
+        """
+        Discover the catalog.
+
+        Returns:
+            Catalog: The catalog as a Pydantic model.
+        """
+        with json_temp_file(self.config) as config_path:
+            catalog = self.discover(config_path)
+            return catalog
+
+    def filtered_catalog(
+        self, catalog: dict, streams: Optional[List[str]] = None
+    ) -> dict:
+        """
+        Filter the catalog.
+
+        Args:
+            catalog (dict): The catalog.
+            streams (Optional[List[str]], optional): The streams to filter on. Defaults to None.
+
+        Returns:
+            dict: The filtered catalog.
+        """
+        if not streams:
+            return catalog
+
+        return {
+            "streams": [
+                {
+                    **stream,
+                    "selected": stream["tap_stream_id"] in streams,
+                    "metadata": stream["metadata"]
+                    + [
+                        {
+                            "metadata": {
+                                "selected": stream["tap_stream_id"] in streams,
+                            },
+                            "breadcrumb": [],
+                        }
+                    ],
+                }
+                for stream in catalog["streams"]
+            ]
+        }
+
+    @property
+    def streams(self) -> list[Stream]:
+        """
+        Get the streams from the catalog.
+
+        Returns:
+            list[Stream]: The streams.
+        """
+        return [Stream(**stream) for stream in self.catalog["streams"]]
+
+    @contextlib.contextmanager
+    @require_install
+    def process(
+        self,
+        state: dict = {},
+        streams: Optional[List[str]] = None,
+    ) -> Generator[Popen, None, None]:
+        """
+        Run the tap process.
+
+        Returns:
+            Popen: The tap process.
+        """
+        catalog = self.filtered_catalog(catalog=self.catalog, streams=streams)
+
+        with json_temp_file(self.config) as config_path:
+            with json_temp_file(catalog) as catalog_path:
+                with json_temp_file(state) as state_path:
+                    yield Popen(
+                        [
+                            self.executable,
+                            "--config",
+                            str(config_path),
+                            "--catalog",
+                            str(catalog_path),
+                            "--state",
+                            str(state_path),
+                        ],
+                        stdout=PIPE,
+                        stderr=PIPE,
+                    )
