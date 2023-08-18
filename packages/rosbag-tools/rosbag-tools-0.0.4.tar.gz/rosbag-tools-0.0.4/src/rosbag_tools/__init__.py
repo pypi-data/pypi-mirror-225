7939b078e95ccfad363c24661b8320573d482aab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 04:13:32 +0000

    Add package rosbag-tools/0.0.4/rosbag-tools-0.0.4.tar.gz

---
+++ packages/rosbag-tools/rosbag-tools-0.0.4.tar.gz/rosbag-tools-0.0.4/src/rosbag_tools/__init__.py
@@ -0,0 +1,7 @@
+"""A ROS-agnostic toolbox for common rosbag operations"""
+
+from .topic_compare import topic_compare
+from .topic_remove import topic_remove
+from .clip import clip
+
+__version__ = "0.0.4"
