935aae41eeb5c10f48fe03c59d3f955a903d5d55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 03:10:29 +0000

    Add package kraky/2023.8.17/kraky-2023.8.17-py2.py3-none-any.whl

---
+++ packages/kraky/kraky-2023.8.17-py2.py3-none-any.whl/kraky/cli.py
@@ -0,0 +1,51 @@
+from dotenv import load_dotenv
+import typer
+
+from kraky import KrakyApiClient
+
+load_dotenv()
+
+app = typer.Typer()
+
+
+@app.command(
+    context_settings={"allow_extra_args": True, "ignore_unknown_options": True}
+)
+def main(
+    ctx: typer.Context,
+    endpoint: str = typer.Argument(
+        "get_system_status",
+        envvar="KRAKY_ENDPOINT",
+        help="Kraky function to call. See full list here : https://kraky.readthedocs.io/en/latest/api/",
+    ),
+    kraken_api_key: str = typer.Option(
+        "",
+        envvar="KRAKEN_API_KEY",
+        help="See here to generate a pair : https://support.kraken.com/hc/en-us/articles/360000919966-How-to-generate-an-API-key-pair-",
+    ),
+    kraken_secret: str = typer.Option(
+        "",
+        envvar="KRAKEN_SECRET",
+        help="See here to generate a pair : https://support.kraken.com/hc/en-us/articles/360000919966-How-to-generate-an-API-key-pair-",
+    ),
+    tfa: bool = typer.Option(
+        False,
+        envvar="KRAKY_TFA",
+        help="Enable two-factor authentication. More info here : https://support.kraken.com/hc/en-us/articles/360000714526",
+    ),
+):
+    kraky_api_client = KrakyApiClient(
+        api_key=kraken_api_key, secret=kraken_secret, tfa=tfa
+    )
+    args: list = []
+    kwargs: dict = {}
+    for arg in ctx.args:
+        kwarg = arg.split("=")
+        kwargs[kwarg[0]] = kwarg[1]
+    try:
+        result = getattr(kraky_api_client, endpoint)(*args, **kwargs)
+        typer.echo(f"{result}")
+    except TypeError as err:
+        typer.echo(err)
+    except AttributeError:
+        typer.echo("Endpoint not supported!")
