2ec84017c9175c9465ec7eeb140ebe91bf19995f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 04:13:06 +0000

    Add package lca-third-party/0.2.0/lca_third_party-0.2.0.tar.gz

---
+++ packages/lca-third-party/lca_third_party-0.2.0.tar.gz/lca_third_party-0.2.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['lca_third_party']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'lca-third-party',
+    'version': '0.2.0',
+    'description': 'Exemplary third party library for LCA 2023',
+    'long_description': 'None',
+    'author': 'Michał Chałupczak',
+    'author_email': 'None',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
