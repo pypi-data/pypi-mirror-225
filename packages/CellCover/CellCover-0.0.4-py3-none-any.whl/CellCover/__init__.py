5f87da16b21d9fca68f113ea7b45e0d44ba8cdae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 04:12:45 +0000

    Add package CellCover/0.0.4/CellCover-0.0.4-py3-none-any.whl

---
+++ packages/CellCover/CellCover-0.0.4-py3-none-any.whl/CellCover/__init__.py
@@ -0,0 +1,5 @@
+from CellCover.covering import covering
+from CellCover.covering import getCoveringVariables
+from CellCover.covering import weight
+from CellCover.covering import binarization
+from CellCover.covering import SensList
