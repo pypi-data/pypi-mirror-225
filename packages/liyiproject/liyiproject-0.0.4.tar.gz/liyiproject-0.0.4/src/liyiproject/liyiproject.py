4ae493279d937346d8e6861faafa3a4dc488e604
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 14:10:10 +0000

    Add package liyiproject/0.0.4/liyiproject-0.0.4.tar.gz

---
+++ packages/liyiproject/liyiproject-0.0.4.tar.gz/liyiproject-0.0.4/src/liyiproject/liyiproject.py
@@ -0,0 +1,28 @@
+import os
+
+
+class Test_operation():
+    def Local_ip(self, ip_address):
+        print(__name__)
+        print(__name__ == "__main__")
+        return ip_address
+
+    def Local_hostname(self, hostname):
+        print(__name__)
+        print(__name__ == "__main__")
+        return hostname
+
+    def operation(self, statement):
+        info = os.popen(statement)
+        print(__name__)
+        print(__name__ == "__main__")
+        return info.read()
+
+    def test_mat(self, add_number):
+        print(__name__)
+        print(__name__ == "__main__")
+        return add_number + 1
+
+
+if __name__ != "__main__":
+    Test_operation
