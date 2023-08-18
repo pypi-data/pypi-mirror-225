9922e748f55fbab945d924286db65b500a598b14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 05:09:24 +0000

    Add package openllm/0.2.26/openllm-0.2.26-cp311-cp311-macosx_10_16_universal2.whl

---
+++ packages/openllm/openllm-0.2.26-cp311-cp311-macosx_10_16_universal2.whl/openllm/models/falcon/__init__.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+import sys, typing as t
+from openllm.exceptions import MissingDependencyError
+from openllm.utils import LazyModule, is_torch_available, is_vllm_available
+
+_import_structure: dict[str, list[str]] = {"configuration_falcon": ["FalconConfig", "START_FALCON_COMMAND_DOCSTRING", "DEFAULT_PROMPT_TEMPLATE"]}
+if t.TYPE_CHECKING:
+  from .configuration_falcon import (
+    DEFAULT_PROMPT_TEMPLATE as DEFAULT_PROMPT_TEMPLATE,
+    START_FALCON_COMMAND_DOCSTRING as START_FALCON_COMMAND_DOCSTRING,
+    FalconConfig as FalconConfig,
+  )
+try:
+  if not is_torch_available(): raise MissingDependencyError
+except MissingDependencyError: pass
+else:
+  _import_structure["modeling_falcon"] = ["Falcon"]
+  if t.TYPE_CHECKING: from .modeling_falcon import Falcon as Falcon
+try:
+  if not is_vllm_available(): raise MissingDependencyError
+except MissingDependencyError: pass
+else:
+  _import_structure["modeling_vllm_falcon"] = ["VLLMFalcon"]
+  if t.TYPE_CHECKING: from .modeling_vllm_falcon import VLLMFalcon as VLLMFalcon
+
+sys.modules[__name__] = LazyModule(__name__, globals()["__file__"], _import_structure)
