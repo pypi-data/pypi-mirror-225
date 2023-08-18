cf55bcc4de492315f34749f5960b04015be14c64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 15:09:34 +0000

    Add package hydrogibs/0.0.35/hydrogibs-0.0.35.tar.gz

---
+++ packages/hydrogibs/hydrogibs-0.0.35.tar.gz/hydrogibs-0.0.35/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="hydrogibs",
+    version="0.0.35",
+    description="A personal hydrology and hydraulics package"
+                " based on Christophe Ancey's teaching: "
+                "http://fr.ancey.ch/cours/masterGC/cours-hydraulique.pdf",
+    # package_dir={"": "hydrogibs"},
+    packages=find_packages(),
+    data_files=[
+        ('', ['hydrogibs/floods/GR4.csv',
+              'hydrogibs/floods/qdf-mean.csv',
+              'hydrogibs/floods/qdf-thres.csv']),
+        ('', ['hydrogibs/test/rain.csv'])
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="giboul",
+    author_email="axel.giboulot@epfl.ch",
+    license="MIT",
+    install_requires=["numpy", "scipy"],
+    python_requires=">=3",
+)
