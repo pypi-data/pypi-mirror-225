b071923182adb399d1db9bf5e69034cbc6ccf9d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 06:16:01 +0000

    Add package cellmaps-generate-hierarchy/0.1.0a9/cellmaps_generate_hierarchy-0.1.0a9-py2.py3-none-any.whl

---
+++ packages/cellmaps-generate-hierarchy/cellmaps_generate_hierarchy-0.1.0a9-py2.py3-none-any.whl/cellmaps_generate_hierarchy/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Cell Maps Generate Hierarchy."""
+
+__author__ = 'Ideker Lab Cell Maps team'
+__email__ = 'tools@cm4ai.org'
+__version__ = '0.1.0a9'
+__repo_url__ = 'https://github.com/idekerlab/cellmaps_generate_hierarchy'
+__description__ = 'A tool to generate hierarchies from protein to protein interaction networks'
