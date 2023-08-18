c7007fa4841f230cd9dd8545d707636d7acc3e88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 14:07:53 +0000

    Add package cvxrisk/0.1.4/cvxrisk-0.1.4.tar.gz

---
+++ packages/cvxrisk/cvxrisk-0.1.4.tar.gz/cvxrisk-0.1.4/cvx/risk/bounds.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""Bounds"""
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+import cvxpy as cp
+import numpy as np
+
+from cvx.risk import Model
+
+
+@dataclass
+class Bounds(Model):
+    m: int = 0
+    name: str = ""
+
+    def estimate(self, weights, **kwargs):
+        """No estimation for bounds"""
+        raise NotImplementedError("No estimation for bounds")
+
+    def _f(self, str):
+        return f"{str}_{self.name}"
+
+    def __post_init__(self):
+        self.parameter[self._f("lower")] = cp.Parameter(
+            shape=self.m,
+            name="lower bound",
+            value=np.zeros(self.m),
+        )
+        self.parameter[self._f("upper")] = cp.Parameter(
+            shape=self.m,
+            name="upper bound",
+            value=np.ones(self.m),
+        )
+
+    def update(self, **kwargs):
+        # lower = kwargs.get("lower", np.zeros(self.m))
+        lower = kwargs[self._f("lower")]
+        self.parameter[self._f("lower")].value = np.zeros(self.m)
+        self.parameter[self._f("lower")].value[: len(lower)] = lower
+
+        upper = kwargs[self._f("upper")]  # .get("upper", np.ones(self.m))
+        self.parameter[self._f("upper")].value = np.zeros(self.m)
+        self.parameter[self._f("upper")].value[
+            : len(upper)
+        ] = upper  # kwargs.get("upper", np.ones(m))
+
+    def constraints(self, weights, **kwargs):
+        return [
+            weights >= self.parameter[self._f("lower")],
+            weights <= self.parameter[self._f("upper")],
+        ]
