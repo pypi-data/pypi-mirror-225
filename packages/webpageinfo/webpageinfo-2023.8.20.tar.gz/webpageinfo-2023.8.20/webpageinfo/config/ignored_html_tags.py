b7c13d9d795d048b103b80518599108f8a361d18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 04:12:23 +0000

    Add package webpageinfo/2023.8.20/webpageinfo-2023.8.20.tar.gz

---
+++ packages/webpageinfo/webpageinfo-2023.8.20.tar.gz/webpageinfo-2023.8.20/webpageinfo/config/ignored_html_tags.py
@@ -0,0 +1,19 @@
+ignored_html_tags = {
+    "article",
+    "aside",
+    "figure",
+    "footer",
+    "frameset",
+    "frame",  # TODO choper le contenu
+    "header",
+    "iframe",  # TODO choper le contenu
+    "label",
+    "main",
+    "noframe",
+    "noscript",
+    "section",
+    "show",
+    "tbody",
+    "time",
+    "tt",
+}
