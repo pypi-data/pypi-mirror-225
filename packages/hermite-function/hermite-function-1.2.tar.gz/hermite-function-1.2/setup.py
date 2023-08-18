c6c1bfe487bf815f37dd1d4f7ad1adf07ba12b55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 05:12:22 +0000

    Add package hermite-function/1.2/hermite-function-1.2.tar.gz

---
+++ packages/hermite-function/hermite-function-1.2.tar.gz/hermite-function-1.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / 'README.md').read_text()
+  
+setup(
+    name = 'hermite-function',
+    version = '1.2',
+    description = 'A Hermite function series module.',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    
+    author = 'Sebastian Gössl',
+    author_email = 'goessl@student.tugraz.at',
+    license = 'MIT',
+    
+    py_modules = ['hermitefunction'],
+    url = 'https://github.com/goessl/hermite-function',
+    python_requires = '>=3.7',
+    install_requires = ['numpy', 'scipy'],
+    
+    classifiers = [
+      'Programming Language :: Python :: 3.7',
+      'License :: OSI Approved :: MIT License',
+      'Operating System :: OS Independent'
+    ]
+)
