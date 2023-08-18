0c041cdb7c2f033142bf56e9934ec5d1eacaff9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 05:08:47 +0000

    Add package flatpack/0.1/flatpack-0.1.tar.gz

---
+++ packages/flatpack/flatpack-0.1.tar.gz/flatpack-0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="flatpack",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # Any dependencies the package might have
+    ],
+    author="Romlin Group AB",
+    author_email="hello@romlin.com",
+    description="Train AI models - not your patience",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    entry_points={
+        'console_scripts': [
+            'flatpack=flatpack.main:test',
+        ],
+    },
+)
