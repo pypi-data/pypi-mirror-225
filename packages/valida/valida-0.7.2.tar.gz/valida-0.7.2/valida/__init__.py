36204892f481755376dd7efb980b430232b19524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 05:09:04 +0000

    Add package valida/0.7.2/valida-0.7.2.tar.gz

---
+++ packages/valida/valida-0.7.2.tar.gz/valida-0.7.2/valida/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.7.2"
+
+from valida.conditions import Value, Key, Index
+from valida.data import Data
+from valida.datapath import DataPath
+from valida.rules import Rule
+from valida.schema import Schema
