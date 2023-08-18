345fd519846f4642800ca9764182a53681ec2666
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 04:16:39 +0000

    Add package ethpm-types/0.5.4/ethpm-types-0.5.4.tar.gz

---
+++ packages/ethpm-types/ethpm-types-0.5.4.tar.gz/ethpm-types-0.5.4/tests/test_hexbytes.py
@@ -0,0 +1,80 @@
+from typing import Optional
+
+from hexbytes import HexBytes as OriginalHexBytes
+from pydantic import Field
+
+from ethpm_types import BaseModel, HexBytes
+
+TEST_MODEL_DICT = {
+    "byte_bytes": b"\x00",
+    "bytearray_bytes": bytearray([0]),
+    "str_bytes": "0x0",
+    "int_bytes": 0,
+    "bool_bytes": False,
+    "original_bytes": OriginalHexBytes("0"),
+    "optional_bytes": HexBytes(0),
+}
+EMPTY_BYTES = HexBytes("0x0000000000000000000000000000000000000000000000000000000000000000")
+
+
+class TestModel(BaseModel):
+    byte_bytes: HexBytes
+    bytearray_bytes: HexBytes
+    str_bytes: HexBytes
+    int_bytes: HexBytes
+    bool_bytes: HexBytes
+    original_bytes: HexBytes
+    optional_bytes: Optional[HexBytes] = None
+
+
+class Block(BaseModel):
+    gas_limit: int = Field(alias="gasLimit")
+    hash: Optional[HexBytes] = None
+    parent_hash: HexBytes = Field(EMPTY_BYTES, alias="parentHash")
+
+
+def test_hexbytes_dict():
+    test_model = TestModel.parse_obj(TEST_MODEL_DICT)
+
+    for _, item in test_model.__fields__.items():
+        assert item.type_ == HexBytes
+
+    test_dict = test_model.dict()
+    assert len(test_dict) == 7
+
+    for _, v in test_dict.items():
+        assert v == "0x00"
+
+
+def test_hexbytes_dict_exclude():
+    test_model = TestModel.parse_obj(TEST_MODEL_DICT)
+
+    test_dict = test_model.dict(exclude={"byte_bytes"})
+    assert len(test_dict) == 6
+    assert "byte_bytes" not in test_dict.keys()
+
+
+def test_hexbytes_json():
+    test_models = (TestModel.parse_obj(TEST_MODEL_DICT), TestModel(**TEST_MODEL_DICT))
+    for model in test_models:
+        test_json = model.json(exclude={"bool_bytes", "byte_bytes", "int_bytes"})
+        assert test_json == (
+            '{"bytearray_bytes":"0x00",'
+            '"optional_bytes":"0x00",'
+            '"original_bytes":"0x00",'
+            '"str_bytes":"0x00"}'
+        )
+
+
+def test_realistic_model():
+    block = Block(
+        gasLimit=123124,
+        hash=HexBytes("0x1be99d96b0b5784b07aea2750aee16a2efbe46cf271b246835bc101fd94bc992"),
+    )
+    actual = block.json(sort_keys=True)
+    expected = (
+        '{"gasLimit":123124,'
+        '"hash":"0x1be99d96b0b5784b07aea2750aee16a2efbe46cf271b246835bc101fd94bc992",'
+        f'"parentHash":"{EMPTY_BYTES.hex()}"}}'
+    )
+    assert actual == expected
---
+++ packages/ethpm-types/ethpm-types-0.5.4.tar.gz/ethpm-types-0.5.4/tests/test_package_manifest.py
@@ -0,0 +1,58 @@
+import os
+
+import github
+import pytest
+import requests
+from pydantic import ValidationError
+
+from ethpm_types import PackageManifest
+
+ETHPM_SPEC_REPO = github.Github(os.environ.get("GITHUB_ACCESS_TOKEN", None)).get_repo(
+    "ethpm/ethpm-spec"
+)
+
+EXAMPLES_RAW_URL = "https://raw.githubusercontent.com/ethpm/ethpm-spec/master/examples"
+
+
+def test_can_generate_schema():
+    PackageManifest.schema()
+
+
+@pytest.mark.parametrize(
+    "example_name",
+    [f.name for f in ETHPM_SPEC_REPO.get_contents("examples")],  # type: ignore[union-attr]
+)
+def test_examples(example_name):
+    example = requests.get(f"{EXAMPLES_RAW_URL}/{example_name}/v3.json")
+    example_json = example.json()
+
+    if "invalid" not in example_name:
+        package = PackageManifest.parse_obj(example_json)
+        assert package.dict() == example_json
+
+        # NOTE: Also make sure that the encoding is exactly the same (per EIP-2678)
+        assert package.json() == example.text
+
+        if package.sources:
+            for source_name, source in package.sources.items():
+                # NOTE: Per EIP-2678, "Checksum is only required if content is missing"
+                if not source.content:
+                    assert source.content_is_valid(), f"Invalid checksum for '{source_name}'"
+
+    else:
+        with pytest.raises(ValidationError):
+            PackageManifest.parse_obj(example_json).dict()
+
+
+def test_open_zeppelin_contracts(oz_package):
+    for source_name, source in oz_package.sources.items():
+        # NOTE: Per EIP-2678, "Checksum is only required if content is missing"
+        if not source.content:
+            assert source.content_is_valid(), f"Invalid checksum for '{source_name}'"
+
+
+def test_file_bases_dependency_url():
+    manifest = PackageManifest(
+        buildDependencies={"test-package": "file:///path/to/manifest/test-package.json"}
+    )
+    assert manifest.dependencies["test-package"] == "file:///path/to/manifest/test-package.json"
