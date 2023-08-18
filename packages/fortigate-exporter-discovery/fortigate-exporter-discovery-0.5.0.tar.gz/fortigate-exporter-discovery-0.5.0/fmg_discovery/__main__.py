5f4ddf66c785fabb664ea730354ce7002e552778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 03:10:25 +0000

    Add package fortigate-exporter-discovery/0.5.0/fortigate-exporter-discovery-0.5.0.tar.gz

---
+++ packages/fortigate-exporter-discovery/fortigate-exporter-discovery-0.5.0.tar.gz/fortigate-exporter-discovery-0.5.0/fmg_discovery/__main__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+    Copyright (C) 2023  Anders Håål
+
+    This file is part of fortigate-exporter-discovery.
+
+    fortigate-exporter-discovery is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    fortigate-exporter-discovery is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with fortigate-exporter-discovery.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+import argparse
+
+from fmg_discovery.file_service_discovery import file_service_discovery
+from fmg_discovery.http_service_discovery import http_service_discovery
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Prometheus Fortigate service discovery')
+    parser.add_argument('--server', action='store_true',
+                        help='Start in http service discovery mode',
+                        dest='server')
+    args = vars(parser.parse_args())
+
+    if args['server']:
+        http_service_discovery()
+    else:
+        file_service_discovery()
