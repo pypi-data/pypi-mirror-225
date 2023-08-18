2f3559bdac51c94d0ae402d56013b563a45bf6fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 05:10:01 +0000

    Add package vasp-manager/1.0.0/vasp_manager-1.0.0.tar.gz

---
+++ packages/vasp-manager/vasp_manager-1.0.0.tar.gz/vasp_manager-1.0.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="vasp_manager",
+    version="1.0.0",
+    description="A simple package to run and analyze VASP calculations",
+    packages=["vasp_manager"],
+    url="https://github.com/dgaines2/vasp_manager",
+    author="Dale Gaines II",
+    author_email="dalegainesii@gmail.com",
+    license="MIT",
+    include_package_data=True,
+    python_requires=">=3.10.0",
+    install_requires=[
+        "numpy>=1.22",
+        "pandas>=1.4",
+        "pymatgen>=2022.5",
+    ],
+    extras_require={"dev": ["black", "isort", "pre-commit", "pytest"]},
+)
