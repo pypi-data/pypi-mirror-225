a6b3467590b201cf546411c81de4d4281dc62fc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-18 14:09:35 +0000

    Add package johnson/0.0.1/johnson-0.0.1-py3-none-any.whl

---
+++ packages/johnson/johnson-0.0.1-py3-none-any.whl/johnson/cli.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from typing import Optional
+
+import typer
+from rich import print_json
+from rich.console import Console
+from typer import Typer
+from typing_extensions import Annotated
+
+from johnson.read_json import read_json_file
+
+console = Console()
+app = Typer()
+
+
+@app.command()
+def johnson(
+    file: Annotated[
+        Optional[Path], typer.Option('--file', '-f', help='path to json file')
+    ]
+):
+
+    if file is None:
+        console.print('No json file')
+        raise typer.Abort()
+    if file.is_file():
+        data = read_json_file(file)
+        console.print_json(data=data)
+    elif file.is_dir():
+        console.print('This is a directory')
+        raise typer.Abort()
+    elif not file.exists():
+        console.print("The file doesn't exist")
+        raise typer.Abort()
