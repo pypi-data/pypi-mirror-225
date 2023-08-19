db123659df79eced1465bb67d25324c49231b632
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 05:10:29 +0000

    Add package utilspkg/0.4.6/utilspkg-0.4.6.tar.gz

---
+++ packages/utilspkg/utilspkg-0.4.6.tar.gz/utilspkg-0.4.6/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='utilspkg',
+    version='0.4.6',
+    packages=find_packages(include=['utilspkg']),
+    install_requires=[
+        'airtable-python-wrapper>=0.15.3',
+        'openai>=0.27.7',
+        'protobuf>=4.24.1',
+        'python-dotenv>=1.0.0',
+        'pytz>=2023.3',
+        'slack-sdk>=3.21.3',
+        'tenacity>=8.2.2',
+        'PyYAML',
+        'google-cloud-logging'
+    ]
+)
