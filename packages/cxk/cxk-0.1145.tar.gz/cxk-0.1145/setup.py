db4d9abf6af63dde15aee95f468716fe04652055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 04:09:27 +0000

    Add package cxk/0.1145/cxk-0.1145.tar.gz

---
+++ packages/cxk/cxk-0.1145.tar.gz/cxk-0.1145/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+       name='cxk',
+       version='0.1145',
+       packages=find_packages(),
+       description='cxk',
+       author='hidrag',
+       author_email='114514@baidu.com',
+   )
