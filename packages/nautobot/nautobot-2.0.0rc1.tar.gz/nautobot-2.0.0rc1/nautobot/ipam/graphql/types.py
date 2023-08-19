753a28e3d8abcb9f6a45431969aa2041334695dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 05:09:08 +0000

    Add package nautobot/2.0.0rc1/nautobot-2.0.0rc1.tar.gz

---
+++ packages/nautobot/nautobot-2.0.0rc1.tar.gz/nautobot-2.0.0rc1/nautobot/ipam/graphql/types.py
@@ -0,0 +1,35 @@
+import graphene
+import graphene_django_optimizer as gql_optimizer
+
+from nautobot.extras.models import DynamicGroup
+from nautobot.ipam import models, filters
+
+
+class IPAddressType(gql_optimizer.OptimizedDjangoObjectType):
+    """Graphql Type Object for IPAddress model."""
+
+    address = graphene.String()
+    ip_version = graphene.Int()
+    dynamic_groups = graphene.List("nautobot.extras.graphql.types.DynamicGroupType")
+
+    class Meta:
+        model = models.IPAddress
+        filterset_class = filters.IPAddressFilterSet
+
+    def resolve_dynamic_groups(self, args):
+        return DynamicGroup.objects.get_for_object(self, use_cache=True)
+
+
+class PrefixType(gql_optimizer.OptimizedDjangoObjectType):
+    """Graphql Type Object for Prefix model."""
+
+    prefix = graphene.String()
+    ip_version = graphene.Int()
+    dynamic_groups = graphene.List("nautobot.extras.graphql.types.DynamicGroupType")
+
+    class Meta:
+        model = models.Prefix
+        filterset_class = filters.PrefixFilterSet
+
+    def resolve_dynamic_groups(self, args):
+        return DynamicGroup.objects.get_for_object(self, use_cache=True)
