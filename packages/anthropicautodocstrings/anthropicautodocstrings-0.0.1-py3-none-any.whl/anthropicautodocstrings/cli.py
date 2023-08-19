80054c25a9fbabdeb51db20f72ee55e0c7c6a9ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 04:09:28 +0000

    Add package anthropicautodocstrings/0.0.1/anthropicautodocstrings-0.0.1-py3-none-any.whl

---
+++ packages/anthropicautodocstrings/anthropicautodocstrings-0.0.1-py3-none-any.whl/anthropicautodocstrings/cli.py
@@ -0,0 +1,15 @@
+import typer
+
+
+def main():
+    """
+
+    def main() -> None:
+      'Prints a message to stdout'
+      typer.echo('Running AnthropicAutoDocstrings!')
+    """
+    typer.echo("Running AnthropiAutoDocstrings!")
+
+
+if __name__ == "__main__":
+    main()
