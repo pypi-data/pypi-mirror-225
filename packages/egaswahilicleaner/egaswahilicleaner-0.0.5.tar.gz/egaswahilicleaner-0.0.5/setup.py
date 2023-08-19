bdc031ea0f613b4c2df259cea78468ddee36d760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 04:11:05 +0000

    Add package egaswahilicleaner/0.0.5/egaswahilicleaner-0.0.5.tar.gz

---
+++ packages/egaswahilicleaner/egaswahilicleaner-0.0.5.tar.gz/egaswahilicleaner-0.0.5/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Intended Audience :: Developers',
+    'Operating System :: Microsoft :: Windows',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Operating System :: Unix',
+    'Operating System :: iOS',
+]
+
+setup(
+    name='egaswahilicleaner',
+    version='0.0.5',
+    description='Swahili text cleaning library',
+   # long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='',
+    author='Shadrack Kajigili',
+    author_email='shadrackkajigili4@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='swahili_text_cleaner',
+    packages=find_packages(),
+    install_requires=['nltk']
+)
