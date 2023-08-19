5b869a2a28683e61dce23790877745ff2913313d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 03:12:34 +0000

    Add package mikan-card-downloader/2.1.1/mikan_card_downloader-2.1.1.tar.gz

---
+++ packages/mikan-card-downloader/mikan_card_downloader-2.1.1.tar.gz/mikan_card_downloader-2.1.1/src/mikan/downloader.py
@@ -0,0 +1,100 @@
+# Copyright (C) 2022-2023 DemonicSavage
+# This file is part of Mikan.
+
+# Mikan is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+
+# Mikan is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+
+from __future__ import annotations
+
+from pathlib import Path
+from typing import Any, Coroutine
+
+import aiohttp
+from tqdm.asyncio import tqdm
+
+import mikan.html_parser as parser
+from mikan import config, json_utils
+from mikan.classes import CardType
+
+
+class Downloader:
+    def __init__(
+        self,
+        data_path: Path,
+        config_path: Path,
+        img_type: CardType,
+        cfg: config.Config,
+    ):
+        self.base_path = data_path.expanduser()
+        self.path = self.base_path / img_type.results_dir
+        self.objs: dict[str, dict[str, list[str]]] = {}
+
+        self.config_path = config_path
+
+        self.img_type = img_type
+        self.objs[self.img_type.results_dir] = {}
+
+        self.session = aiohttp.ClientSession(
+            connector=aiohttp.TCPConnector(limit=cfg.max_conn, limit_per_host=cfg.max_conn),
+            timeout=aiohttp.ClientTimeout(total=None),
+            cookies={"sessionid": cfg.cookie},
+        )
+
+        json_utils.load_cards(self.objs, self.config_path)
+
+        self.parser = parser.Parser(self.objs, self.img_type, self.session)
+
+    async def __aenter__(self) -> Downloader:
+        return self
+
+    async def __aexit__(self, ext_type: None, value: None, trace: None) -> None:
+        await self.session.close()
+
+    async def download_file(self, item: str) -> None:
+        try:
+            res = await self.session.get(f"https:{item}")
+            if res.status == 200:
+                self.path.mkdir(exist_ok=True, parents=True)
+
+                with open(self.path / self.get_card_image_name(item), "wb") as file:
+                    async for chunk in res.content.iter_any():
+                        file.write(chunk)
+
+                message = f"Downloaded item {self.get_card_image_name(item)}."
+
+        except aiohttp.ClientError as e:
+            message = f"Couldn't download item {item}: {e}"
+
+        tqdm.write(message)
+
+    def get_card_image_name(self, url: str) -> str:
+        return url.split("/")[-1]
+
+    async def get(self) -> None:
+        tasks: list[Coroutine[Any, Any, None]] = []
+
+        for item in self.objs[self.img_type.results_dir].values():
+            tasks.extend(
+                [self.download_file(card) for card in item if not (self.path / self.get_card_image_name(card)).exists()]
+            )
+
+        await tqdm.gather(*tasks, disable=len(tasks) == 0)
+
+    async def update(self) -> None:
+        print("Searching for new or missing items...")
+        await self.parser.get_cards_from_pages()
+
+        self.update_json_file()
+        print("Updated items database.")
+
+    def update_json_file(self) -> None:
+        self.objs[self.img_type.results_dir] = dict(sorted(self.objs[self.img_type.results_dir].items(), reverse=True))
+        json_utils.dump_to_file(self.objs, self.config_path)
