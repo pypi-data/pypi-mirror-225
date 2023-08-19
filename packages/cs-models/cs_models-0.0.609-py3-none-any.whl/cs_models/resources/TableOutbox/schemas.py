5f572ca666c5664887dea900cea26dacecedb869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 03:11:43 +0000

    Add package cs-models/0.0.609/cs_models-0.0.609-py3-none-any.whl

---
+++ packages/cs-models/cs_models-0.0.609-py3-none-any.whl/cs_models/resources/TableOutbox/schemas.py
@@ -0,0 +1,16 @@
+from marshmallow import (
+    Schema,
+    fields,
+    validate,
+)
+
+
+class TableOutboxResourceSchema(Schema):
+    not_blank = validate.Length(min=1, error='Field cannot be blank')
+
+    id = fields.Integer(dump_only=True)
+    sec_filing_id = fields.Integer(required=True)
+    table_number = fields.Integer(required=True)
+    table_html = fields.String(required=True)
+    table_preceding_info = fields.String(required=True)
+    updated_at = fields.DateTime()
