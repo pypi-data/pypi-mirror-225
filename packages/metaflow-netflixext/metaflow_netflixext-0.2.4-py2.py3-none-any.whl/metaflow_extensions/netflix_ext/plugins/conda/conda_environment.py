089f886aa8dbf60e6e742f3ebbeb57424f32661f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 03:09:08 +0000

    Add package metaflow-netflixext/0.2.4/metaflow_netflixext-0.2.4-py2.py3-none-any.whl

---
+++ packages/metaflow-netflixext/metaflow_netflixext-0.2.4-py2.py3-none-any.whl/metaflow_extensions/netflix_ext/plugins/conda/conda_environment.py
@@ -0,0 +1,256 @@
+# pyright: strict, reportTypeCommentUsage=false, reportMissingTypeStubs=false
+
+import json
+import os
+import tarfile
+
+from io import BytesIO
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    List,
+    Optional,
+    Set,
+    Tuple,
+    Union,
+    cast,
+)
+
+from metaflow.plugins.datastores.local_storage import LocalStorage
+from metaflow.flowspec import FlowSpec
+
+from metaflow.exception import MetaflowException
+
+from metaflow.metaflow_config import CONDA_MAGIC_FILE_V2
+
+from metaflow.metaflow_environment import MetaflowEnvironment
+
+
+from .envsresolver import EnvsResolver
+from .utils import get_conda_manifest_path
+
+from .env_descr import CachedEnvironmentInfo, EnvID, ResolvedEnvironment
+from .conda import Conda
+from .conda_step_decorator import get_conda_decorator
+
+
+class CondaEnvironment(MetaflowEnvironment):
+    TYPE = "conda"
+    _filecache = None
+    _conda = None
+
+    def __init__(self, flow: FlowSpec):
+        self._flow = flow
+
+        self._conda = None  # type: Optional[Conda]
+
+        # A conda environment sits on top of whatever default environment
+        # the user has so we get that environment to be able to forward
+        # any calls we don't handle specifically to that one.
+        from metaflow.plugins import ENVIRONMENTS
+        from metaflow.metaflow_config import DEFAULT_ENVIRONMENT
+
+        if DEFAULT_ENVIRONMENT == self.TYPE:
+            # If the default environment is Conda itself then fallback on
+            # the default 'default environment'
+            self.base_env = MetaflowEnvironment(self._flow)
+        else:
+            self.base_env = [
+                e
+                for e in ENVIRONMENTS + [MetaflowEnvironment]
+                if e.TYPE == DEFAULT_ENVIRONMENT
+            ][0](self._flow)
+
+    def init_environment(self, echo: Callable[..., None]):
+        # Print a message for now
+        echo("Bootstrapping Conda environment... (this could take a few minutes)")
+
+        self._conda = cast(Conda, self._conda)
+        resolver = EnvsResolver(self._conda)
+
+        for step in self._flow:
+            # Figure out the environments that we need to resolve for all steps
+            # We will resolve all unique environments in parallel
+            step_conda_dec = get_conda_decorator(self._flow, step.name)
+            if step_conda_dec.is_enabled() and not step_conda_dec.is_fetch_at_exec():
+                resolver.add_environment_for_step(step.name, step_conda_dec)
+
+        resolver.resolve_environments(echo)
+
+        update_envs = []  # type: List[ResolvedEnvironment]
+        if self._datastore_type != "local":
+            # We may need to update caches
+            # Note that it is possible that something we needed to resolve, we don't need
+            # to cache (if we resolved to something already cached).
+            formats = set()  # type: Set[str]
+            for _, resolved_env, f, _ in resolver.need_caching_environments():
+                update_envs.append(resolved_env)
+                formats.update(f)
+
+            self._conda.cache_environments(update_envs, {"conda": list(formats)})
+        else:
+            update_envs = [
+                resolved_env for _, resolved_env, _ in resolver.new_environments()
+            ]
+
+        self._conda.add_environments(update_envs)
+
+        # Update the default environment
+        for env_id, resolved_env, _ in resolver.all_environments():
+            if resolved_env and env_id.full_id == "_default":
+                self._conda.set_default_environment(resolved_env.env_id)
+
+        # We are done -- write back out the environments.
+        # TODO: Not great that this is manual
+        self._conda.write_out_environments()
+
+        # Delegate to whatever the base environment needs to do.
+        self.base_env.init_environment(echo)
+
+    def validate_environment(
+        self, echo: Callable[..., Any], datastore_type: str
+    ) -> None:
+        self._local_root = cast(str, LocalStorage.get_datastore_root_from_config(echo))
+        self._datastore_type = datastore_type
+        self._conda = Conda(echo, datastore_type)
+
+        return self.base_env.validate_environment(echo, datastore_type)
+
+    def decospecs(self) -> Tuple[str, ...]:
+        # Apply conda and pip decorator and base environment's decorators to all steps.
+        # We will later resolve which to keep.
+        return ("conda", "pip") + self.base_env.decospecs()
+
+    def _get_env_id(self, step_name: str) -> Optional[Union[str, EnvID]]:
+        conda_decorator = get_conda_decorator(self._flow, step_name)
+        if conda_decorator.is_enabled():
+            if not conda_decorator.is_fetch_at_exec():
+                resolved_env = cast(Conda, self._conda).environment(
+                    conda_decorator.env_id
+                )
+                if resolved_env:
+                    return resolved_env.env_id
+                else:
+                    raise MetaflowException(
+                        "Cannot find environment for step '%s'" % step_name
+                    )
+            else:
+                resolved_env_id = os.environ.get("_METAFLOW_CONDA_ENV")
+                if resolved_env_id:
+                    return EnvID(*json.loads(resolved_env_id))
+                # Here we will return the name of the environment
+                return conda_decorator.from_env_name_unresolved
+        return None
+
+    def _get_executable(self, step_name: str) -> Optional[str]:
+        env_id = self._get_env_id(step_name)
+        if env_id is not None:
+            # The create method in Conda() sets up this symlink when creating the
+            # environment.
+            return os.path.join(".", "__conda_python")
+        return None
+
+    def bootstrap_commands(self, step_name: str, datastore_type: str) -> List[str]:
+        # Bootstrap conda and execution environment for step
+        env_id = self._get_env_id(step_name)
+        if env_id is not None:
+            if isinstance(env_id, EnvID):
+                arg1 = env_id.req_id
+                arg2 = env_id.full_id
+            else:
+                arg1 = env_id
+                arg2 = "_fetch_exec"
+            return [
+                "export CONDA_START=$(date +%s)",
+                "echo 'Bootstrapping environment ...'",
+                'python -m %s.remote_bootstrap "%s" "%s" %s %s %s'
+                % (
+                    "metaflow_extensions.netflix_ext.plugins.conda",
+                    self._flow.name,
+                    step_name,
+                    arg1,
+                    arg2,
+                    datastore_type,
+                ),
+                "export _METAFLOW_CONDA_ENV=$(cat _env_id)",
+                "export PYTHONPATH=$(pwd)/_escape_trampolines:$(printenv PYTHONPATH)",
+                "echo 'Environment bootstrapped.'",
+                "export CONDA_END=$(date +%s)",
+            ]
+        return []
+
+    def add_to_package(self) -> List[Tuple[str, str]]:
+        # TODO: Improve this to only extract the environments that we care about
+        # The issue is that we need to pass a path which is a bit annoying since
+        # we then can't clean it up easily.
+        files = self.base_env.add_to_package()
+        # Add conda manifest file to job package at the top level.
+
+        # In the case of a resume, we still "package" the code but don't send it
+        # anywhere -- if we are resuming on a remote node, this file may not exist
+        # so we skip
+        path = get_conda_manifest_path(self._local_root)
+        if path and os.path.exists(path):
+            files.append((path, os.path.basename(path)))
+        return files
+
+    def pylint_config(self) -> List[str]:
+        config = self.base_env.pylint_config()
+        # Disable (import-error) in pylint
+        config.append("--disable=F0401")
+        return config
+
+    def executable(self, step_name: str, default: Optional[str] = None) -> str:
+        # Get relevant python interpreter for step
+        executable = self._get_executable(step_name)
+        if executable is not None:
+            return executable
+        return self.base_env.executable(step_name, default)
+
+    @classmethod
+    def get_client_info(
+        cls, flow_name: str, metadata: Dict[str, str]
+    ) -> Dict[str, Any]:
+        # TODO: FIX THIS: this doesn't work with the new format.
+        if cls._filecache is None:
+            from metaflow.client.filecache import FileCache
+
+            cls._filecache = FileCache()
+        info = metadata.get("code-package")
+        env_id = json.loads(metadata.get("conda_env_id", "[]"))
+        if info is None or not env_id:
+            return {"type": "conda"}
+        info = json.loads(info)
+        _, blobdata = cls._filecache.get_data(
+            info["ds_type"], flow_name, info["location"], info["sha"]
+        )
+        with tarfile.open(fileobj=BytesIO(blobdata), mode="r:gz") as tar:
+            conda_file = tar.extractfile(CONDA_MAGIC_FILE_V2)
+        if conda_file is None:
+            return {"type": "conda"}
+        cached_env_info = CachedEnvironmentInfo.from_dict(
+            json.loads(conda_file.read().decode("utf-8"))
+        )
+        resolved_env = cached_env_info.env_for(env_id[0], env_id[1])
+        if resolved_env:
+            new_info = {
+                "type": "conda",
+                "req_id": env_id[0],
+                "full_id": env_id[1],
+                "user_deps": "; ".join(map(str, resolved_env.deps)),
+                "all_packages": "; ".join(
+                    [
+                        p.filename
+                        for p in sorted(resolved_env.packages, key=lambda p: p.filename)
+                    ]
+                ),
+            }
+            return new_info
+        return {"type": "conda"}
+
+    def get_package_commands(self, code_package_url: str, datastore_type: str):
+        return self.base_env.get_package_commands(code_package_url, datastore_type)
+
+    def get_environment_info(self, include_ext_info=False):
+        return self.base_env.get_environment_info(include_ext_info)
---
+++ packages/metaflow-netflixext/metaflow_netflixext-0.2.4-py2.py3-none-any.whl/metaflow_extensions/netflix_ext/plugins/conda/conda_flow_decorator.py
@@ -0,0 +1,159 @@
+from metaflow.decorators import FlowDecorator
+from metaflow.metaflow_environment import InvalidEnvironmentException
+
+
+class CondaFlowDecorator(FlowDecorator):
+    """
+    Specifies the Conda environment for all steps of the flow.
+
+    Use `@conda_base` to set common libraries required by all
+    steps and use `@conda` to specify step-specific additions.
+
+    Parameters
+    ----------
+    name : Optional[str]
+        If specified, can refer to a named environment. The environment referred to
+        here will be the one used as a base environment for all steps.
+        If specified, nothing else can be specified in this decorator.
+    pathspec : Optional[str]
+        If specified, can refer to the pathspec of an existing step. The environment
+        of this referred step will be used as a base environment for all steps.
+        If specified, nothing else can be specified in this decorator.
+    libraries : Optional[Dict[str, str]]
+        Libraries to use for this step. The key is the name of the package
+        and the value is the version to use (default: `{}`). Note that versions can
+        be specified either as a specific version or as a comma separated string
+        of constraints like "<2.0,>=1.5".
+    channels : Optional[List[str]]
+        Additional channels to search
+    pip_packages : Optional[Dict[str, str]]
+        Same as libraries but for pip packages.
+    pip_sources : Optional[List[str]]
+        Same as channels but for pip sources.
+    python : Optional[str]
+        Version of Python to use, e.g. '3.7.4'. If not specified, the current Python
+        version will be used.
+    fetch_at_exec : bool, default False
+        If set to True, the environment will be fetched when the task is
+        executing as opposed to at the beginning of the flow (or at deploy time if
+        deploying to a scheduler). This option requires name or pathspec to be
+        specified. This is useful, for example, if you want this step to always use
+        the latest named environment when it runs as opposed to the latest when it
+        is deployed.
+    disabled : bool, default False
+        If set to True, disables Conda
+    """
+
+    name = "conda_base"
+    defaults = {
+        "name": None,
+        "pathspec": None,
+        "libraries": {},
+        "channels": [],
+        "pip_packages": {},
+        "pip_sources": [],
+        "python": None,
+        "fetch_at_exec": None,
+        "disabled": None,
+    }
+
+    def flow_init(
+        self, flow, graph, environment, flow_datastore, metadata, logger, echo, options
+    ):
+        if "pip_base" in flow._flow_decorators:
+            raise InvalidEnvironmentException(
+                "conda_base decorator is not compatible with pip_base. "
+                "Please specify only one of them."
+            )
+        if environment.TYPE != "conda":
+            raise InvalidEnvironmentException(
+                "The *@conda_base* decorator requires " "--environment=conda"
+            )
+
+        if (self.attributes["name"] or self.attributes["pathspec"]) and len(
+            [
+                k
+                for k, v in self.attributes.items()
+                if v and k not in ("name", "pathspec")
+            ]
+        ):
+            raise InvalidEnvironmentException(
+                "You cannot specify `name` or `pathspec` along with other attributes in @%s"
+                % self.name
+            )
+
+
+class PipFlowDecorator(FlowDecorator):
+    """
+    Specifies the Pip environment for all steps of the flow.
+
+    Use `@pip_base` to set common libraries required by all
+    steps and use `@pip` to specify step-specific additions.
+
+    Parameters
+    ----------
+    name : Optional[str]
+        If specified, can refer to a named environment. The environment referred to
+        here will be the one used as a base environment for all steps.
+        If specified, nothing else can be specified in this decorator.
+    pathspec : Optional[str]
+        If specified, can refer to the pathspec of an existing step. The environment
+        of this referred step will be used as a base environment for all steps.
+        If specified, nothing else can be specified in this decorator.
+    packages : Optional[Dict[str, str]]
+        Packages to use for this step. The key is the name of the package
+        and the value is the version to use (default: `{}`). Note that versions can
+        be specified either as a specific version or as a comma separated string
+        of constraints like "<2.0,>=1.5".
+    sources : Optional[List[str]]
+        Additional channels to search for
+    python : Optional[str]
+        Version of Python to use, e.g. '3.7.4'. If not specified, the current version
+        will be used.
+    fetch_at_exec : bool, default False
+        If set to True, the environment will be fetched when the task is
+        executing as opposed to at the beginning of the flow (or at deploy time if
+        deploying to a scheduler). This option requires name or pathspec to be
+        specified. This is useful, for example, if you want this step to always use
+        the latest named environment when it runs as opposed to the latest when it
+        is deployed.
+    disabled : bool, default False
+        If set to True, disables Pip
+    """
+
+    name = "pip_base"
+
+    defaults = {
+        "name": None,
+        "pathspec": None,
+        "packages": {},
+        "sources": [],
+        "python": None,
+        "fetch_at_exec": None,
+        "disabled": None,
+    }
+
+    def flow_init(
+        self, flow, graph, environment, flow_datastore, metadata, logger, echo, options
+    ):
+        if "conda_base" in flow._flow_decorators:
+            raise InvalidEnvironmentException(
+                "pip_base decorator is not compatible with conda_base. "
+                "Please specify only one of them."
+            )
+        if environment.TYPE != "conda":
+            raise InvalidEnvironmentException(
+                "The *@pip_base* decorator requires --environment=conda"
+            )
+
+        if (self.attributes["name"] or self.attributes["pathspec"]) and len(
+            [
+                k
+                for k, v in self.attributes.items()
+                if v and k not in ("name", "pathspec")
+            ]
+        ):
+            raise InvalidEnvironmentException(
+                "You cannot specify `name` or `pathspec` along with other attributes in @%s"
+                % self.name
+            )
