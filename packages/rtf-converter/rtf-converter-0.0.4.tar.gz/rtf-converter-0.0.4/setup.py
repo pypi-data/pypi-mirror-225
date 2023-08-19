4a90bcfaf9104a6733b72a566db1401d07c10f90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 05:09:13 +0000

    Add package rtf-converter/0.0.4/rtf-converter-0.0.4.tar.gz

---
+++ packages/rtf-converter/rtf-converter-0.0.4.tar.gz/rtf-converter-0.0.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+# Read the content of the README.md file
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='rtf-converter',
+    version='0.0.4',
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown', 
+)
