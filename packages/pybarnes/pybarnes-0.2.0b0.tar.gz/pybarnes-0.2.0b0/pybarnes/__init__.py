38bdf4526f82360c106ed27966dbb48d5c2c023a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 03:11:38 +0000

    Add package pybarnes/0.2.0b0/pybarnes-0.2.0b0.tar.gz

---
+++ packages/pybarnes/pybarnes-0.2.0b0.tar.gz/pybarnes-0.2.0b0/pybarnes/__init__.py
@@ -0,0 +1,3 @@
+from .barnes_filter import BarnesFilter, BarnesFilter4Scatter
+
+__version__ = "0.2.0-beta"
---
+++ packages/pybarnes/pybarnes-0.2.0b0.tar.gz/pybarnes-0.2.0b0/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+setup(name='pybarnes',version='0.2.0-beta',author='Lin Ouyang',
+    packages=["pybarnes"],
+    include_package_data=False,
+    install_requires=["metpy"]
+)
