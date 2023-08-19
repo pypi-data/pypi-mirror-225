4f9e95c425aa65e013ede008d2bdb125cd195d10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 04:11:45 +0000

    Add package PyUPC-EAN/2.9.6/PyUPC-EAN-2.9.6.tar.gz

---
+++ packages/PyUPC-EAN/PyUPC-EAN-2.9.6.tar.gz/PyUPC-EAN-2.9.6/upcean/encode/getsfname.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+'''
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the Revised BSD License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Revised BSD License for more details.
+
+    Copyright 2011-2023 Game Maker 2k - https://github.com/GameMaker2k
+    Copyright 2011-2023 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
+
+    $FileInfo: getsfname.py - Last Update: 4/14/2023 Ver. 2.9.5 RC 1 - Author: cooldude2k $
+'''
+
+from __future__ import absolute_import, division, print_function, unicode_literals;
+import upcean.support;
+pilsupport = upcean.support.check_for_pil();
+cairosupport = upcean.support.check_for_cairo();
+if(pilsupport or cairosupport):
+ from upcean.encode.predraw import get_save_filename;
