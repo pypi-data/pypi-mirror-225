436514bfcf0568cfd3f1e91ac42599757fe238d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 05:09:36 +0000

    Add package emlabpkg/1.5/emlabpkg-1.5.tar.gz

---
+++ packages/emlabpkg/emlabpkg-1.5.tar.gz/emlabpkg-1.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="emlabpkg",
+    version="1.5",
+    summary="Univeristy of Cincinnati, Physics Department, Dr. Mikheev's Lab Package. Contains driver for NF-ZM2376 and edited version of sweep",
+    author="Sushant Padhye",
+    author_email="padhyesm@mail.uc.edu",
+    packages=find_packages(),
+    install_requires=["qcodes"],
+)
