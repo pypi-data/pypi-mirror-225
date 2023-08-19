495a7963fd48ce66bff415612b54528f281ba1e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 04:09:27 +0000

    Add package pysyncq/0.0.0/pysyncq-0.0.0.tar.gz

---
+++ packages/pysyncq/pysyncq-0.0.0.tar.gz/pysyncq-0.0.0/setup.py
@@ -0,0 +1,16 @@
+
+'''
+Install the Python Synchronisation Queue - pysyncqpip
+'''
+
+from setuptools import setup , find_packages
+
+setup(  name = 'pysyncq' ,
+     version = '0.0.0' ,
+ description = 'Python based time/data synchronisation queue' ,
+         url = 'https://github.com/jsdpag/pysyncq' ,
+      author = 'Jackson Smith' ,
+     license = 'GPL' ,
+   packages = find_packages( ) ,
+   zip_safe = False )
+
