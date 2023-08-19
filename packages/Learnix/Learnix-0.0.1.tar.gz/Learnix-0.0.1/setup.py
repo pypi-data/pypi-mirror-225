78af07aa3f1546b19777c1823e35fa6b767e0ee0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 03:11:05 +0000

    Add package Learnix/0.0.1/Learnix-0.0.1.tar.gz

---
+++ packages/Learnix/Learnix-0.0.1.tar.gz/Learnix-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='Learnix',
+    version='0.0.1',
+    description='Python library for scientific computing',
+    author='Khalid Walid Aghrini',
+    author_email='learnix.team@gmail.com',
+    install_requires=[
+        'numpy',  # Add any required dependencies here
+    ],
+    keywords=['scientific', 'computing', 'machine learning', 'library', 'math'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
