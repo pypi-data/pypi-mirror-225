de24b265709095e317187ce4fc3444a4ab807f5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 04:11:28 +0000

    Add package pmidcite/0.0.45/pmidcite-0.0.45.tar.gz

---
+++ packages/pmidcite/pmidcite-0.0.45.tar.gz/pmidcite-0.0.45/src/pmidcite/__version__.py
@@ -0,0 +1,7 @@
+"""Version of pmidcite project"""
+
+__copyright__ = 'Copyright (C) 2019-present, DV Klopfenstein, PhD, PhD. All rights reserved'
+__author__ = ' DV Klopfenstein, PhD, PhD'
+__version__ = '0.0.45'
+
+# Copyright (C) 2019-present, DV Klopfenstein, PhD, PhD. All rights reserved
