18ade25be8b1593f59b15351e50aff4b4e49b6af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 04:12:07 +0000

    Add package compose-stubs/1.0.0/compose-stubs-1.0.0.tar.gz

---
+++ packages/compose-stubs/compose-stubs-1.0.0.tar.gz/compose-stubs-1.0.0/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+project_directory = os.path.abspath(os.path.dirname(__file__))
+readme_path = os.path.join(project_directory, 'README.rst')
+
+with open(readme_path) as readme_file:
+    long_description = readme_file.read()
+
+
+setup(
+    name='compose-stubs',
+    version='1.0.0',
+    description='Type stubs for compose',
+    long_description=long_description,
+    license='0BSD',
+    url='https://github.com/mentalisttraceur/python-compose-stubs',
+    author='Alexander Kozhevnikov',
+    author_email='mentalisttraceur@gmail.com',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    packages=["compose-stubs"],
+    package_data={'compose-stubs': ['__init__.pyi', 'compose.pyi']},
+)
