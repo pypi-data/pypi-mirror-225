b43a05f605a34741591edb28b7db5cfc9a44f242
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 03:10:21 +0000

    Add package skrit-r0073rr0r/0.2.5/skrit_r0073rr0r-0.2.5-py3-none-any.whl

---
+++ packages/skrit-r0073rr0r/skrit_r0073rr0r-0.2.5-py3-none-any.whl/skrit_r0073rr0r/litrovacki.py
@@ -0,0 +1,8 @@
+"""Python Skrit Litrovacki"""
+from skrit_r0073rr0r.skrit import Skrit
+
+class Litrovacki(Skrit):
+    """Python Skrit Satrovacki INIT"""
+    def __init__(self):
+        super().__init__()
+        print('Litrovacki')
