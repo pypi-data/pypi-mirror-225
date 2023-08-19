1737665dc603531ddcde7127df6b3af063b9d502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 05:10:39 +0000

    Add package pixelpay-sdk/2.1.4/pixelpay_sdk-2.1.4-py3-none-any.whl

---
+++ packages/pixelpay-sdk/pixelpay_sdk-2.1.4-py3-none-any.whl/pixelpay/exceptions/FailedEncryptionException.py
@@ -0,0 +1,2 @@
+class FailedEncryptionException(Exception):
+    pass
---
+++ packages/pixelpay-sdk/pixelpay_sdk-2.1.4-py3-none-any.whl/pixelpay/exceptions/__init__.py
@@ -0,0 +1,9 @@
+from .FailedEncryptionException import FailedEncryptionException
+from .InvalidCredentialsException import InvalidCredentialsException
+from .InvalidTransactionTypeException import InvalidTransactionTypeException
+
+__all__ = [
+    "FailedEncryptionException",
+    "InvalidCredentialsException",
+    "InvalidTransactionTypeException",
+]
