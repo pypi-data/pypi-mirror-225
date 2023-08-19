37626b88dbc3e99a487705335376aa4bdbbda897
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 04:11:51 +0000

    Add package exampl/0.0.1/exampl-0.0.1.tar.gz

---
+++ packages/exampl/exampl-0.0.1.tar.gz/exampl-0.0.1/example.py
@@ -0,0 +1,12 @@
+class algori:
+    def recurs(self):
+        i = 0
+        while i < 9:
+            if i == 2:
+                break
+            i += 2
+    def circu(self):
+        i = 0
+        while i < 9:
+            i += 2
+            if i == 2:
---
+++ packages/exampl/exampl-0.0.1.tar.gz/exampl-0.0.1/setup.py
@@ -0,0 +1,38 @@
+#!python
+# -*- coding:utf-8 -*-
+from __future__ import print_function
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name="exampl",
+    version='0.0.1',
+    author="xiaofengkz",
+    author_email="xiaofengkz@163.com",
+    description="a template for pgzero",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/zhec5hl01/python-games",
+    py_modules=['example'],
+    install_requires=[
+        "pgzero <= 1.2.1",
+        "pygame <= 2.1.2"
+        ],
+    classifiers=[
+        "Topic :: Games/Entertainment ",
+        'Topic :: Games/Entertainment :: Puzzle Games',
+        'Topic :: Games/Entertainment :: Board Games',
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+)
+
