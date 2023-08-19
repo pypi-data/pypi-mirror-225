fe533d67becbc9c544dc48c9f041db8c027d10f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 03:11:04 +0000

    Add package capfinder/0.1.2/capfinder-0.1.2.tar.gz

---
+++ packages/capfinder/capfinder-0.1.2.tar.gz/capfinder-0.1.2/src/capfinder/add.py
@@ -0,0 +1,13 @@
+def add(a: int, y: int, z: int) -> int:
+    """
+    Add three numbers together
+    param a: first number
+    param y: second number
+    param z: third number
+    return: sum of a, y, and z
+    """
+    return a + y + z
+
+
+if __name__ == "__main__":
+    print(add(2, 2, 3))
