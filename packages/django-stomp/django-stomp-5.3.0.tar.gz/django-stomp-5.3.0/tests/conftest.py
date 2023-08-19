e32e1fca852f25d3f7a1c2c33f026d5ea28efb5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 04:11:57 +0000

    Add package django-stomp/5.3.0/django-stomp-5.3.0.tar.gz

---
+++ packages/django-stomp/django-stomp-5.3.0.tar.gz/django-stomp-5.3.0/tests/conftest.py
@@ -0,0 +1,43 @@
+import os
+import uuid
+
+from django.conf import settings
+
+
+def pytest_assertrepr_compare(config, op, left, right):
+    """
+    More details at: https://stackoverflow.com/a/50625086/3899136
+    """
+    if op in ("==", "!="):
+        return ["{0} {1} {2}".format(left, op, right)]
+
+
+def pytest_configure():
+    settings.configure(
+        INSTALLED_APPS=["django_stomp", "tests.support"],
+        STOMP_SERVER_HOST=os.getenv("STOMP_SERVER_HOST"),
+        STOMP_SERVER_PORT=os.getenv("STOMP_SERVER_PORT"),
+        STOMP_SERVER_STANDBY_HOST=os.getenv("STOMP_SERVER_STANDBY_HOST"),
+        STOMP_SERVER_STANDBY_PORT=os.getenv("STOMP_SERVER_STANDBY_PORT"),
+        STOMP_SERVER_USER=os.getenv("STOMP_SERVER_USER"),
+        STOMP_SERVER_PASSWORD=os.getenv("STOMP_SERVER_PASSWORD"),
+        STOMP_USE_SSL=os.getenv("STOMP_USE_SSL"),
+        STOMP_LISTENER_CLIENT_ID=os.getenv("STOMP_LISTENER_CLIENT_ID"),
+        STOMP_CORRELATION_ID_REQUIRED=os.getenv("STOMP_CORRELATION_ID_REQUIRED"),
+        STOMP_PROCESS_MSG_ON_BACKGROUND=os.getenv("STOMP_PROCESS_MSG_ON_BACKGROUND"),
+        STOMP_OUTGOING_HEARTBEAT=os.getenv("STOMP_OUTGOING_HEARTBEAT"),
+        STOMP_INCOMING_HEARTBEAT=os.getenv("STOMP_INCOMING_HEARTBEAT"),
+        STOMP_SERVER_VHOST=os.getenv("STOMP_SERVER_VHOST"),
+        STOMP_DEFAULT_EXCLUSIVE_QUEUE=os.getenv("STOMP_DEFAULT_EXCLUSIVE_QUEUE"),
+        DATABASES={
+            "default": {
+                "ENGINE": os.getenv("DB_ENGINE", "django.db.backends.sqlite3"),
+                "NAME": os.getenv("DB_DATABASE", f"test_db-{uuid.uuid4()}"),
+                "USER": os.getenv("DB_USER"),
+                "HOST": os.getenv("DB_HOST"),
+                "PORT": os.getenv("DB_PORT"),
+                "PASSWORD": os.getenv("DB_PASSWORD"),
+                "TEST": {"NAME": os.getenv("DB_DATABASE", f"test_db-{uuid.uuid4()}")},
+            }
+        },
+    )
---
+++ packages/django-stomp/django-stomp-5.3.0.tar.gz/django-stomp-5.3.0/tests/unit/services/test_consumer.py
@@ -0,0 +1,121 @@
+import json
+from uuid import uuid4
+
+import pytest
+
+from django_stomp import builder
+from django_stomp.services.consumer import StompFrame
+from django_stomp.services.consumer import build_listener
+from tests.support.helpers import get_active_threads_name_with_prefix
+
+
+def test_should_create_at_most_the_defined_number_of_workers(mocker):
+    mocker.patch("django_stomp.services.consumer.STOMP_PROCESS_MSG_WORKERS", 3)
+
+    listener = build_listener(f"some-destination-{uuid4()}", should_process_msg_on_background=True)
+
+    fake_frame = StompFrame(cmd="FAKE_CMD", headers={"message-id": "123"}, body=json.dumps({"someKey": 1}))
+
+    listener.on_message(fake_frame)
+    listener.on_message(fake_frame)
+    listener.on_message(fake_frame)
+    listener.on_message(fake_frame)
+
+    workers_threads = get_active_threads_name_with_prefix(listener._subscription_id)
+    assert len(workers_threads) == 3  # 3 workers
+    listener.shutdown_worker_pool()
+
+
+def test_should_clean_up_worker_pool():
+    listener = build_listener(f"some-destination-{uuid4()}", should_process_msg_on_background=True)
+
+    fake_frame = StompFrame(cmd="FAKE_CMD", headers={"message-id": "123"}, body=json.dumps({"someKey": 1}))
+
+    listener.on_message(fake_frame)
+
+    workers_threads_before_pool_shutdown = get_active_threads_name_with_prefix(listener._subscription_id)
+    assert len(workers_threads_before_pool_shutdown) == 1  # only one worker
+
+    listener.shutdown_worker_pool()
+
+    workers_threads_after_pool_shutdown = get_active_threads_name_with_prefix(listener._subscription_id)
+    assert len(workers_threads_after_pool_shutdown) == 0  # no active worker thread
+
+
+def test_should_still_process_message_if_worker_pool_was_explicitly_shutdown():
+    listener = build_listener(f"some-destination-{uuid4()}", should_process_msg_on_background=True)
+
+    fake_frame = StompFrame(cmd="FAKE_CMD", headers={"message-id": "123"}, body=json.dumps({"someKey": 1}))
+
+    listener.on_message(fake_frame)
+
+    workers_threads_before_pool_shutdown = get_active_threads_name_with_prefix(listener._subscription_id)
+    assert len(workers_threads_before_pool_shutdown) == 1  # only one worker
+    listener.shutdown_worker_pool()
+
+    workers_threads_after_pool_shutdown = get_active_threads_name_with_prefix(listener._subscription_id)
+    assert len(workers_threads_after_pool_shutdown) == 0  # no active worker thread
+
+    listener.on_message(fake_frame)
+
+    workers_threads_before_pool_shutdown = get_active_threads_name_with_prefix(listener._subscription_id)
+    assert len(workers_threads_before_pool_shutdown) == 1  # only one worker
+
+    listener.shutdown_worker_pool()
+
+
+def test_should_have_only_one_django_stomp_listener_even_if_set_listener_is_called_multiple_times():
+    # Arrange - build listener to some arbirtrary queue
+    django_stomp_listener = builder.build_listener(f"some-destination-{uuid4()}")
+
+    # Act - in sucession, multiples _set_listener call to trigger _connection.set_listener
+    django_stomp_listener._set_listener()
+    django_stomp_listener._set_listener()
+    django_stomp_listener._set_listener()
+    django_stomp_listener._set_listener()
+    django_stomp_listener._set_listener()
+
+    # Assert - it has two listeners (protocol-listener, stomp.py defined and the one defined by django-stomp)
+    assert len(django_stomp_listener._connection.transport.listeners) == 2
+    assert django_stomp_listener._connection.get_listener("protocol-listener") is not None
+
+    django_stomp_listener_id = django_stomp_listener._listener_id
+    assert django_stomp_listener._connection.get_listener(django_stomp_listener_id) is not None
+
+
+def test_listener_connection_configuration_must_have_headers_properly_configured():
+    # Arrange - defines the expected headers setup
+    headers_setup_mapping = {
+        "client-id": str,
+        "activemq.prefetchSize": str,
+        "prefetch-count": str,
+        "x-dead-letter-routing-key": str,
+        "x-dead-letter-exchange": str,
+        "exclusive": bool,
+    }
+
+    # Arrange - build listener to some arbirtrary queue and get its connection configuration headers
+    django_stomp_listener = builder.build_listener(f"some-destination-{uuid4()}")
+    created_header_setup = django_stomp_listener._connection_configuration["headers"]
+
+    assert headers_setup_mapping.keys() == created_header_setup.keys(), "headers do not have all required keys"
+
+    for key, value in headers_setup_mapping.items():
+        assert isinstance(created_header_setup[key], value), "header has wrong type"
+
+    assert int(created_header_setup["prefetch-count"]), "prefetch-count must be a number"
+
+
+@pytest.mark.parametrize("setting_value,expected", [(True, True), (False, False)])
+def test_should_have_a_listener_with_param_exclusive_in_connection_configuration_headers(
+    mocker, setting_value, expected
+):
+    # Arrange - mock the stomp default exclusive queue setting
+    mocker.patch("django_stomp.services.consumer.STOMP_DEFAULT_EXCLUSIVE_QUEUE", setting_value)
+
+    # Arrange - build listener to some arbirtrary queue
+    django_stomp_listener = builder.build_listener(f"some-destination-{uuid4()}")
+
+    assert (
+        django_stomp_listener._connection_configuration["headers"]["exclusive"] is expected
+    ), "The stomp default exclusive queue parameter was not configured correctly."
