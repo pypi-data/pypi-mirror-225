80371759171ca5b7c6f3d4f697e0112af4af3971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 05:09:44 +0000

    Add package pyward/2.2/pyward-2.2.tar.gz

---
+++ packages/pyward/pyward-2.2.tar.gz/pyward-2.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyward',
+    version='2.2',  # Update the version number to 2.2
+    description='Stabilizes Code and Makes It Secure',
+    author='Rachel Anthony',
+    author_email='dev@pyward.com',
+    url='https://github.com/pyward/pyward',
+    packages=find_packages(),
+    install_requires=[
+        'requests',  # Add 'requests' as a dependency
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
