fff4427d1e5627dfd34ba3927ffd46f3ed0e3a29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 04:11:08 +0000

    Add package setuptools-github/0.3.10b98/setuptools_github-0.3.10b98-py3-none-any.whl

---
+++ packages/setuptools-github/setuptools_github-0.3.10b98-py3-none-any.whl/setuptools_github/_build.py
@@ -0,0 +1,8 @@
+# autogenerate build file
+branch = 'beta/0.3.10'
+build = '98'
+current = '0.3.10b98'
+hash = '507c657056d1a66520ec6b219a64706e70b0ff15'
+runid = '5904313530'
+version = '0.3.10b98'
+workflow = 'beta'
