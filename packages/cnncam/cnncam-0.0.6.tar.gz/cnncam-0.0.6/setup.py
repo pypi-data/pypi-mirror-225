4858e6018a705a1d11818763a7d851578e41257f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 03:12:41 +0000

    Add package cnncam/0.0.6/cnncam-0.0.6.tar.gz

---
+++ packages/cnncam/cnncam-0.0.6.tar.gz/cnncam-0.0.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+with open("lib/README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+    name="cnncam",
+    package_dir={'':'lib'},
+    packages=find_packages(where='lib'),
+    # packages=['cnncam'],
+    long_description=long_description, 
+    long_description_content_type = 'text/markdown',
+    version='0.0.6',
+    description='Gradient Based Class Activation Maps for TensorFlow models.',
+    py_modules=["cnncam"],
+    install_requires= ["tensorflow>=2.0.0",
+                       "keras>=2.12.0",
+                       "numpy>=1.23",
+                       "opencv-python>=4.7.0",
+                       "matplotlib>=3.7.0"],
+    extras_require = {
+        "dev" : ["twine>=4.0.2"]
+    }
+)
