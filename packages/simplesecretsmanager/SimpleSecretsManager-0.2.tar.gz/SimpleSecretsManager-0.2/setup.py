19867fa13d39be0313af6c70287b8a8793c20466
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 05:09:36 +0000

    Add package simplesecretsmanager/0.2/SimpleSecretsManager-0.2.tar.gz

---
+++ packages/simplesecretsmanager/SimpleSecretsManager-0.2.tar.gz/SimpleSecretsManager-0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SimpleSecretsManager",
+    version="0.2",
+    packages=["SimpleSecretsManager"],
+    install_requires=[
+        "cryptography",
+    ],
+    license="MIT",
+    description="A simple secrets manager",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/noneabove1182/SimpleSecretsManager",
+)
