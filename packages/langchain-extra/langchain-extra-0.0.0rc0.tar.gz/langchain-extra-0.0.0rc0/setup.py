785d63d173e9e971781f067a9040e6a10c989aa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 04:11:40 +0000

    Add package langchain-extra/0.0.0rc0/langchain-extra-0.0.0rc0.tar.gz

---
+++ packages/langchain-extra/langchain-extra-0.0.0rc0.tar.gz/langchain-extra-0.0.0rc0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='langchain-extra',
+    version='0.0.0rc0',
+    packages=find_packages(),
+    license="MIT",
+    description="Use langchain-extras"
+)
