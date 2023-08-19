12691021e58b1a88998c9f1e148dab7d87201b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 03:11:02 +0000

    Add package profcomff-definitions/2023.8.18/profcomff_definitions-2023.8.18.tar.gz

---
+++ packages/profcomff-definitions/profcomff_definitions-2023.8.18.tar.gz/profcomff_definitions-2023.8.18/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+
+with open("README.md", "r", encoding="utf-8") as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name="profcomff_definitions",
+    version="2023.08.18",
+    author="Stanislav Roslavtsev",
+    url="https://github.com/profcomff/dwh-definitions",
+    description="Data warehouse definitions and schemas",
+    license='BDS',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=["SQLAlchemy", "alembic", "setuptools"],
+    author_email="roslavtzev.stanislaw@yandex.ru",
+    zip_safe=False,
+    classifiers=["Programming Language :: Python :: 3.11", "License :: OSI Approved :: BSD License"],
+)
