fcf0f3b8ff58ab53562552e55bd326e2f606179e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 03:10:53 +0000

    Add package girona-donostia/0.1.3/girona_donostia-0.1.3.tar.gz

---
+++ packages/girona-donostia/girona_donostia-0.1.3.tar.gz/girona_donostia-0.1.3/girona_donostia/__init__.py
@@ -0,0 +1,11 @@
+import numpy as np
+from itertools import product
+import ast
+import os
+import copy 
+import datetime as dt
+
+from girona_donostia import functions_for_library
+from girona_donostia import  objects_for_library
+from girona_donostia.objects_for_library import *
+from girona_donostia.functions_for_library import *
---
+++ packages/girona-donostia/girona_donostia-0.1.3.tar.gz/girona_donostia-0.1.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='girona_donostia',
+    version='0.1.3',    
+    description='A package to help facilitate studying of Nonlinear optical propreties',
+    url='https://github.com/Petru-Milev/Girona_Donostia',
+    author='Petru Milev',
+    author_email='petia.md36@gmail.com',
+    license='-',
+    packages = find_packages(include=['girona_donostia', 'girona_donostia.*']),
+    install_requires=['numpy',                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
