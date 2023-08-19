d9241a195e7f268c0e708b644ab5f2565de6bd0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 05:09:16 +0000

    Add package swiftly-sys/0.0.21/swiftly-sys-0.0.21.tar.gz

---
+++ packages/swiftly-sys/swiftly-sys-0.0.21.tar.gz/swiftly-sys-0.0.21/scripts/unix/swiftly-utils.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+read_cli_result() {
+    # Get the system's temporary directory using Python
+    local temp_dir=$(python3 -c "import tempfile; print(tempfile.gettempdir())")
+
+    local result_file_path="${temp_dir}/swiftly_cli_result.txt"
+    
+    # Check if the result file exists
+    if [[ ! -f "$result_file_path" ]]; then
+        echo "Error: Result file not found!"
+        return 1
+    fi
+    
+    # Read the result
+    local result=$(cat "$result_file_path")
+    
+    # Remove the temporary file
+    rm "$result_file_path"
+    
+    # Return the result
+    echo "$result"
+}
+
+is_sourced() {
+    # If the script's parent process is the same as the current shell, then it's being sourced
+    # $0 is the name of the script, and $BASH_SOURCE is the source of the current script
+    [[ "$0" != "$BASH_SOURCE" ]]
+}
+
+ensure_sourced(){
+    # Check if the script is being sourced
+    if ! is_sourced; then
+        echo "Please run this script using 'source' or '.'"
+        return 1 2>/dev/null || exit 1  # If sourced, use 'return'. If run as a script, use 'exit'.
+    fi
+}
