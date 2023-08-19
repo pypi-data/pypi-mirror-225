dd3e20b666aac855a9c521abd09efa516a3d1a86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 04:11:47 +0000

    Add package geeeks/0.1.1/geeeks-0.1.1.tar.gz

---
+++ packages/geeeks/geeeks-0.1.1.tar.gz/geeeks-0.1.1/geeeks/StringFunctions.py
@@ -0,0 +1,8 @@
+class StringFunctions:
+    def __init__(self):
+        pass
+    def reverse(data):
+        return "".join(list(data)[::-1])
+    
+    def make_link(line):
+        return "-".join(line.lower().split(" "))
