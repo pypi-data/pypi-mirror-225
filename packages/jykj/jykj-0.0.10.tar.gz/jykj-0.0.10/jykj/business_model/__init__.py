4741088d70af37d5ba94f56a2fa2cdb360eb9ae6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 03:09:22 +0000

    Add package jykj/0.0.10/jykj-0.0.10.tar.gz

---
+++ packages/jykj/jykj-0.0.10.tar.gz/jykj-0.0.10/jykj/business_model/__init__.py
@@ -0,0 +1,4 @@
+from jykj.business_model.false_alarm_detection import FalseAlarmDetection
+from jykj.business_model.video_cap_base64 import VideoCapBase64
+
+__all__ = ["VideoCapBase64", "FalseAlarmDetection"]
