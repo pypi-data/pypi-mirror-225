f553681aec4d936de0eafbd2dcd3f564a375d49b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 03:12:41 +0000

    Add package streamlit-dynamic-filters/0.1.2/streamlit_dynamic_filters-0.1.2.tar.gz

---
+++ packages/streamlit-dynamic-filters/streamlit_dynamic_filters-0.1.2.tar.gz/streamlit_dynamic_filters-0.1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.2'
+DESCRIPTION = "Dynamic multiselect filters for Streamlit"
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="streamlit_dynamic_filters",
+    version=VERSION,
+    author="Oleksandr Arsentiev",
+    author_email="<arsentiev9393@gmail.com>",
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=['streamlit'],
+    keywords=['streamlit', 'custom', 'component'],
+    license="MIT",
+    url="https://github.com/arsentievalex/streamlit-dynamic-filters",
+)
