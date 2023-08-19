c1ebf228939b50efad6f25ad8b0bf61e594442dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 04:09:34 +0000

    Add package utilspkg/0.4.0/utilspkg-0.4.0.tar.gz

---
+++ packages/utilspkg/utilspkg-0.4.0.tar.gz/utilspkg-0.4.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='utilspkg',
+    version='0.4.0',
+    # this tells it to just use the utils/ folder
+    packages=find_packages(include=['utilspkg']),
+    # packages=find_packages(),
+    # packages=find_packages(include=['utils_pkg', 'utils_pkg.utils']),
+    requires=['airtable_python_wrapper', 'openai', 'python_dotenv', 'pytz', 'PyYAML', 'slack_sdk', 'tenacity']
+    
+)
