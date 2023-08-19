edc2020d8ad595a8aa7ab5d985800f178cf495a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 03:10:54 +0000

    Add package laminci/0.8.0/laminci-0.8.0-py2.py3-none-any.whl

---
+++ packages/laminci/laminci-0.8.0-py2.py3-none-any.whl/laminci/nox.py
@@ -0,0 +1,9 @@
+from ._nox import (  # noqa
+    build_docs,
+    login_laminapp_admin,
+    login_testuser1,
+    login_testuser2,
+    run_pre_commit,
+    run_pytest,
+    setup_test_instances_from_main_branch,
+)
