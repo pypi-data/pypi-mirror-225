77f152fad1555ee4260c688503aa3bfdd3daeea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 03:09:12 +0000

    Add package django-df-api-drf/0.0.8/django_df_api_drf-0.0.8-py3-none-any.whl

---
+++ packages/django-df-api-drf/django_df_api_drf-0.0.8-py3-none-any.whl/df_api_drf/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+
+class ErrorSerializer(serializers.Serializer):
+    message = serializers.CharField(required=True)
+    code = serializers.CharField(required=True)
+    field = serializers.CharField(required=False)
+
+
+class ErrorResponseSerializer(serializers.Serializer):
+    errors = ErrorSerializer(many=True, required=True)
