ef745f7af6e4200a937e784105dfc178848f6530
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 03:11:04 +0000

    Add package pypomes-core/0.2.8/pypomes_core-0.2.8-py3-none-any.whl

---
+++ packages/pypomes-core/pypomes_core-0.2.8-py3-none-any.whl/pypomes_core/file_pomes.py
@@ -0,0 +1,37 @@
+from flask import Request
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from werkzeug.datastructures import FileStorage
+
+
+def file_from_request(request: Request, file_name: str = None, file_seq: int = 0) -> bytes:
+    """
+    Retrieve and return the contents of the file returned in the response to a request.
+
+    The file may be referred to by its name (*file_name*), or if no name is specified,
+    by its sequence number (*file_seq*).
+
+    :param request: the request
+    :param file_name: optional name for the file
+    :param file_seq:  sequence number for the file, defaults to the first file
+    :return: the contents retrieved from the file
+    """
+    # inicialize the return variable
+    result: bytes | None = None
+
+    count: int = len(request.files)
+    # has a file been found ?
+    if count > 0:
+        # yes, retrieve it
+        file: FileStorage | None = None
+        if isinstance(file_name, str):
+            file = request.files.get(file_name)
+        elif isinstance(file_seq, int) and file_seq >= 0:
+            file_in: str = list(request.files)[file_seq]
+            file = request.files[file_in]
+
+        if file is not None:
+            result: bytes = file.stream.read()
+
+    return result
