3e8ce7820e762188033ba7cff779fd552c9823d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 03:08:54 +0000

    Add package hollihop-api-client/0.0.12/hollihop_api_client-0.0.12.tar.gz

---
+++ packages/hollihop-api-client/hollihop_api_client-0.0.12.tar.gz/hollihop_api_client-0.0.12/src/hollihop_api_client/tests/test_offices.py
@@ -0,0 +1,15 @@
+def test_get_offices_by_location_id(hh_client, test_data):
+    response = hh_client.offices.get_offices(location_id=test_data.test_city_id)
+    for office in response:
+        assert office.location == test_data.test_city_name
+
+
+def test_get_offices_by_location_name(hh_client, test_data):
+    response = hh_client.offices.get_offices(name=test_data.test_city_name)
+    for office in response:
+        assert office.location == test_data.test_city_name
+
+
+def test_get_office_by_id(hh_client, test_data):
+    response = hh_client.offices.get_offices(id=test_data.test_office_id)
+    assert response[-1].name == test_data.test_office_name
