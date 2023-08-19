3ea0f5c7293466a63a8b22aa0941b8d5e7027b9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 03:10:11 +0000

    Add package pdm-click/0.1.2/pdm_click-0.1.2.tar.gz

---
+++ packages/pdm-click/pdm_click-0.1.2.tar.gz/pdm_click-0.1.2/README.md
@@ -0,0 +1,1 @@
+# pdm-click
---
+++ packages/pdm-click/pdm_click-0.1.2.tar.gz/pdm_click-0.1.2/src/pdm_click/build_hooks.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+# import subprocess
+from typing import TYPE_CHECKING, Final
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+    from pdm.backend.hooks import Context
+
+
+class ClickBuildHook:
+    DEFAULT_TARGET_DIR = "completions"
+    SHELLS: Final[list[str]] = ["bash", "zsh", "fish"]
+
+    def pdm_build_initialize(self, context: Context):
+        pass
+
+    def pdm_build_update_files(self, context: Context, files: dict[str, Path]):
+        metadata = context.config.metadata
+        print(metadata)
+        # for shell in ClickBuildHook.SHELLS:
+        #     subprocess.run(["pdm", "run", ])
