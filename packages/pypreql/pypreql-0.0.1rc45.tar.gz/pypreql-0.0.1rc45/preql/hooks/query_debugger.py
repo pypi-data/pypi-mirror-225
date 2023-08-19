c0adb16595dd5b06fbcd597fdf65cca3fe7c302d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 04:11:46 +0000

    Add package pypreql/0.0.1rc45/pypreql-0.0.1rc45.tar.gz

---
+++ packages/pypreql/pypreql-0.0.1rc45.tar.gz/pypreql-0.0.1rc45/preql/hooks/query_debugger.py
@@ -0,0 +1,84 @@
+from typing import Union
+from preql.core.models import QueryDatasource, CTE, Datasource, Select
+
+from preql.hooks.base_hook import BaseHook
+from preql.constants import logger
+from logging import StreamHandler, DEBUG
+from preql.core.processing.concept_strategies_v2 import StrategyNode
+
+
+from preql.dialect.bigquery import BigqueryDialect
+
+renderer = BigqueryDialect()
+
+
+def print_recursive_resolved(input: Union[QueryDatasource, Datasource], depth: int = 0):
+    display = [
+        (
+            "  " * depth,
+            input.full_name,
+            "->",
+            input.group_required,
+            "->",
+            [c.address for c in input.output_concepts],
+        )
+    ]
+    if isinstance(input, QueryDatasource):
+        for child in input.datasources:
+            display += print_recursive_resolved(child, depth + 1)
+    return display
+
+
+def print_recursive_nodes(input: StrategyNode, depth: int = 0):
+    resolved = input.resolve()
+    display = [
+        (
+            "  " * depth,
+            input,
+            "->",
+            resolved.grain,
+            "->",
+            [c.address for c in resolved.output_concepts],
+        )
+    ]
+    for child in input.parents:
+        display += print_recursive_nodes(child, depth + 1)
+    return display
+
+
+def print_recursive_ctes(input: CTE, depth: int = 0, max_depth: int | None = None):
+    if max_depth and depth > max_depth:
+        return
+    select_statement = [c.address for c in input.output_columns]
+    print("  " * depth, input.name, "->", input.group_to_grain, "->", select_statement)
+    sql = renderer.render_cte(input).statement
+    for line in sql.split("\n"):
+        logger.debug("  " * (depth) + line)
+    print("-----")
+    if isinstance(input, CTE):
+        for child in input.parent_ctes:
+            print_recursive_ctes(child, depth + 1)
+
+
+class DebuggingHook(BaseHook):
+    def __init__(self, level=DEBUG, max_depth: int | None = None):
+        if not any([isinstance(x, StreamHandler) for x in logger.handlers]):
+            logger.addHandler(StreamHandler())
+        logger.setLevel(level)
+        self.max_depth = max_depth
+
+    def process_select_info(self, select: Select):
+        print(f"grain: {str(select.grain)}")
+
+    def process_root_datasource(self, datasource: QueryDatasource):
+        printed = print_recursive_resolved(datasource)
+        for row in printed:
+            print("".join([str(v) for v in row]))
+
+    def process_root_cte(self, cte: CTE):
+        print_recursive_ctes(cte, max_depth=self.max_depth)
+
+    def process_root_strategy_node(self, node: StrategyNode):
+        printed = print_recursive_nodes(node)
+        for row in printed:
+            print("".join([str(v) for v in row]))
