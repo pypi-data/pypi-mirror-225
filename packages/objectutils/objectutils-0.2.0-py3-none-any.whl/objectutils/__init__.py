2b887b4fee90ee8c309d2d98adc9b16ced002562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 04:11:38 +0000

    Add package objectutils/0.2.0/objectutils-0.2.0-py3-none-any.whl

---
+++ packages/objectutils/objectutils-0.2.0-py3-none-any.whl/objectutils/__init__.py
@@ -0,0 +1,13 @@
+from .main import recursion_level, flatten, zip_dicts
+from .single_dispatch import deep_traverse, PathGroup
+from .class_wrapper import T
+
+
+__all__ = [
+    "recursion_level",
+    "flatten",
+    "zip_dicts",
+    "deep_traverse",
+    "T",
+    "PathGroup"
+]
