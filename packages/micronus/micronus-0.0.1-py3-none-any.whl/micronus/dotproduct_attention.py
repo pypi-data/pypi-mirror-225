a5e7c41efea7288449cf2755b11db69f59dd6708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 05:09:46 +0000

    Add package micronus/0.0.1/micronus-0.0.1-py3-none-any.whl

---
+++ packages/micronus/micronus-0.0.1-py3-none-any.whl/micronus/dotproduct_attention.py
@@ -0,0 +1,39 @@
+from tensorflow import matmul, math, cast, float32
+from tensorflow.keras.layers import Layer
+from tensorflow.keras.backend import softmax
+
+class DotProductAttention(Layer):
+    """class for scaled dot product attention"""
+
+    def __init__(self, **kwargs):
+        super(DotProductAttention, self).__init__(**kwargs)
+
+    def call(self, queries, keys, values, d_k, mask=None):
+        """method for scaled dot product attention
+
+        Parameters
+        ----------
+        queries :
+            
+        keys :
+            
+        values :
+            
+        d_k :
+            
+        mask :
+             (Default value = None)
+
+        Returns
+        -------
+
+        """
+
+        scores = matmul(queries, keys, transpose_b=True) / math.sqrt(cast(d_k, float32))
+
+        if mask is not None:
+            scores += -1e9 * mask
+        
+        weights = softmax(scores)
+
+        return matmul(weights, values)
