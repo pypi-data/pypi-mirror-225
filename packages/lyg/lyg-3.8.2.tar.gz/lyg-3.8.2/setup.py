81b5c13d7c5f0c7442de33f9e358c165c3d456ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-19 04:11:43 +0000

    Add package lyg/3.8.2/lyg-3.8.2.tar.gz

---
+++ packages/lyg/lyg-3.8.2.tar.gz/lyg-3.8.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lyg',
+    version='3.8.2',  # 递增的版本号
+    packages=find_packages(),
+    package_data={
+        'lyg': ['*.so', '*.pyd']
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows"
+    ],
+)
+
