66b48bed10a45cf50e596e8fcd5a3024260a57e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 11:52:52 +0000

    Add package minecraft-object-utils/0.0.1/minecraft_object_utils-0.0.1.tar.gz

---
+++ packages/minecraft-object-utils/minecraft_object_utils-0.0.1.tar.gz/minecraft_object_utils-0.0.1/minecraft_object_utils/ModInfo.py
@@ -0,0 +1,25 @@
+import os.path
+
+VANILLA_NAMESPACE = "minecraft"
+DEFAULT_DATA_DIRECTORY = os.path.join(os.path.dirname(__file__), "data")
+
+
+class ModInfo:
+    namespace: str
+    version: str
+    directory: str
+
+    @property
+    def versioned_name(self) -> str:
+        return f"{self.namespace}-{self.version}"
+
+    def __init__(self, namespace: str, version: str = "", directory: str = "") -> None:
+        self.namespace = namespace
+        self.version = version
+        if directory == "":
+            self.directory = DEFAULT_DATA_DIRECTORY
+        else:
+            self.directory = directory
+
+
+VANILLA_JAVA_LATEST = ModInfo(VANILLA_NAMESPACE, "1.20")
